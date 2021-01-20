51f01bc48e418aa1d4832d01f19edb6a2228c762
Matthew Carr <matt.carr@madetech.com>
2021-01-20 22:09:03 +0000

    Adds support for js/jsx files in jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
+// See: https://jestjs.io/docs/en/configuration for a full list of configuration options
 module.exports = {
   roots: ["<rootDir>"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   transform: {
     ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
-    "^.+\\.(ts|tsx)?$": "babel-jest",
+    "^.+\\.(ts|tsx|js|jsx)?$": "babel-jest",
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
   testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
 };
