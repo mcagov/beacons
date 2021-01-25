a4ed670ad675f228e163a4f2c653df9e8b5574f0
Matthew Carr <matt.carr@madetech.com>
2021-01-25 11:58:43 +0000

    Specify terraform aws version

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   shared_credentials_file = "$HOME/.aws/credentials"
   profile                 = "default"
