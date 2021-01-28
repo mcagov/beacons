b3c78476af908cc0edeb4a3fd8e9409aecf6f01c
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-01-28 13:52:57 +0000

    Add showcase links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,28 @@ const Home: FunctionComponent = () => {
         <p className="govuk-body">Use this service to:</p>
 
         <ul className="govuk-list govuk-list--bullet">
-          <li>showcase your continuous integration and deployment pipeline</li>
           <li>
-            talk about reusable React components that will speed up future
-            sprints
+            <a href={"https://github.com/madetech/mca-beacons-webapp/actions"}>
+              showcase
+            </a>{" "}
+            your{" "}
+            <a href={"https://github.com/madetech/mca-beacons-integration"}>
+              continuous integration
+            </a>{" "}
+            and{" "}
+            <a href={"https://console.aws.amazon.com"}>deployment pipeline</a>
+          </li>
+          <li>
+            talk about reusable{" "}
+            <a href={"https://design-system.service.gov.uk/"}>
+              GOV.UK design system
+            </a>{" "}
+            components that will speed up future sprints
+          </li>
+          <li>
+            outline the{" "}
+            <a href={"https://miro.com/app/board/o9J_lZRBWY4=/"}>data model</a>{" "}
+            as we currently understand it
           </li>
         </ul>
         <a
