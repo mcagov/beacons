090e91b6ac84ce745925bb780dcd526ce96e3deb
Matthew Carr <matt.carr@madetech.com>
2021-01-28 11:40:43 +0000

    Add README for terraform config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 
-  # Requires bucket/Dynamo DB to be configured, please see README.md
+  # Requires S3 bucket & Dynamo DB to be configured, please see README.md
   backend "s3" {
     bucket = "mca-beacons-state"
     key    = "state"
