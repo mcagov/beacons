ee8da3b757ba47035f2e6fdd67e6b4fe39b562c4
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-01-28 13:17:57 +0000

    Add govuk-width-container to PhaseBanner so it renders correctly

--- src/components/PhaseBanner.tsx
+++ src/components/PhaseBanner.tsx
@@ -9,7 +9,7 @@ export const PhaseBanner: React.FC<PhaseBannerProps> = ({
   phase,
   bannerHtml,
 }: PhaseBannerProps): JSX.Element => (
-  <div className="govuk-phase-banner">
+  <div className="govuk-phase-banner govuk-width-container">
     <p className="govuk-phase-banner__content">
       <strong className="govuk-tag govuk-phase-banner__content__tag">
         {phase}
