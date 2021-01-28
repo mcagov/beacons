f0ded4100623f31a081de0e15e8e53b439e5603e
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-01-28 15:58:12 +0000

    Remove button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,26 +33,6 @@ const Home: FunctionComponent = () => {
             as we currently understand it
           </li>
         </ul>
-        <a
-          href="#"
-          role="button"
-          draggable="false"
-          className="govuk-button govuk-!-margin-top-2 govuk-!-margin-bottom-8 govuk-button--start"
-          data-module="govuk-button"
-        >
-          Start now
-          <svg
-            className="govuk-button__start-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            width="17.5"
-            height="19"
-            viewBox="0 0 33 40"
-            aria-hidden="true"
-            focusable="false"
-          >
-            <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
-          </svg>
-        </a>
       </main>
     </div>
   );
