fab6986bf6a4c1707113497a84a60035bd76a0f0
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-01-29 09:48:53 +0000

    Include global GOV.UK header tags in _app Head
    (With exception of IE8/9 conditional targeting comments, see issue #30)
    
    Global metadata from GOV.UK template, including viewport, charset, lang,
    favicon etc.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useEffect } from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.scss";
+import Head from "next/Head";
 
 const MyApp: FunctionComponent<AppProps> = ({
   Component,
@@ -17,7 +18,57 @@ const MyApp: FunctionComponent<AppProps> = ({
     GOVUKFrontend.initAll();
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#0b0c0c" />
+
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+        <link
+          rel="shortcut icon"
+          sizes="16x16 32x32 48x48"
+          href="/assets/images/favicon.ico"
+          type="image/x-icon"
+        />
+        <link
+          rel="mask-icon"
+          href="/assets/images/govuk-mask-icon.svg"
+          color="#0b0c0c"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/images/govuk-apple-touch-icon-180x180.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="167x167"
+          href="/assets/images/govuk-apple-touch-icon-167x167.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/assets/images/govuk-apple-touch-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/assets/images/govuk-apple-touch-icon.png"
+        />
+
+        <meta
+          property="og:image"
+          content="/assets/images/govuk-opengraph-image.png"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default MyApp;
