69911a8307781beda010f38ef1185a902ab61184
Matthew Carr <matt.carr@madetech.com>
2021-02-02 12:51:15 +0000

    Add how long it takes and before you start sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,6 @@ const ServiceStartPage: FunctionComponent = () => (
       <title>Beacon Registration Service - Register a new beacon</title>
     </Head>
 
-    <Heading />
-
     <AboutTheService />
 
     <HowLongItTakes />
@@ -19,20 +17,32 @@ const ServiceStartPage: FunctionComponent = () => (
 
 export default ServiceStartPage;
 
-const Heading: FunctionComponent = () => (
-  <h1 className="govuk-heading-l">Register or update a 406 MHz beacon</h1>
-);
-
 const AboutTheService: FunctionComponent = () => (
-  <p className="govuk-body">
-    Use this service to register or update an existing 406MHz distress beacon.
-  </p>
+  <>
+    <h1 className="govuk-heading-l">Register or update a 406 MHz beacon</h1>
+
+    <p className="govuk-body">
+      Use this service to register or update an existing 406MHz distress beacon.
+    </p>
+  </>
 );
 
 const HowLongItTakes: FunctionComponent = () => (
-  <h2 className="govuk-heading-m">How long it takes</h2>
+  <>
+    <h2 className="govuk-heading-m">How long it takes</h2>
+
+    <p className="govuk-body">
+      TO UPDATE: It takes up to xxx weeks to register your beacon online.
+    </p>
+  </>
 );
 
 const BeforeYouStart: FunctionComponent = () => (
-  <h2 className="govuk-heading-m">Before you start</h2>
+  <>
+    <h2 className="govuk-heading-m">Before you start</h2>
+
+    <p className="govuk-body">
+      TO UPDATE: : You'll need certain information before you start.
+    </p>
+  </>
 );
