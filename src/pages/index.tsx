fa14a799e3d4978dd9116fccf51c6e2d697a8570
Stuart Mindt <stuart.mindt@madetech.com>
2021-02-03 16:26:24 +0000

    Added content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent, FunctionComponentElement } from "react";
+import React, { FunctionComponent } from "react";
 import Head from "next/head";
 import Aside from "../components/Aside";
 import { StartButton } from "../components/Button";
 import { Grid } from "../components/Grid";
+import { InsetText } from "../components/InsetText";
 
 const ServiceStartPage: FunctionComponent = () => (
   <>
@@ -17,7 +18,6 @@ const ServiceStartPage: FunctionComponent = () => (
         <>
           <PageHeading />
           <AboutTheService />
-          <StartButton />
           <OtherWaysToAccessTheService />
         </>
       }
@@ -32,12 +32,32 @@ const RelatedContent: FunctionComponent = () => (
       <ul className="govuk-list govuk-!-font-size-16">
         <li>
           <a className="govuk-link" href="#">
-            Related link
+            Find your beacons HEX ID
           </a>
         </li>
         <li>
-          <a className="govuk-link govuk-!-font-size-16" href="#">
-            Related link
+          <a className="govuk-link" href="#">
+            Emergency and life-saving equipment on ships
+          </a>
+        </li>
+        <li>
+          <a className="govuk-link" href="#">
+            UK Ship Register
+          </a>
+        </li>
+        <li>
+          <a className="govuk-link" href="#">
+            Aircraft registrations (G-INFO)
+          </a>
+        </li>
+        <li>
+          <a className="govuk-link" href="#">
+            Non 406MHz beacons
+          </a>
+        </li>
+        <li>
+          <a className="govuk-link" href="#">
+            Non UK encoded beacons
           </a>
         </li>
       </ul>
@@ -51,21 +71,45 @@ const PageHeading: FunctionComponent = () => (
 
 const AboutTheService: FunctionComponent = () => (
   <>
-    <p className="govuk-body">
-      Use this service to register or update an existing 406MHz distress beacon.
-    </p>
+    <p className="govuk-body">Use this service to:</p>
 
-    <h2 className="govuk-heading-m">How long it takes</h2>
-
-    <p className="govuk-body">
-      TO UPDATE: It takes up to xxx weeks to register your beacon online.
-    </p>
+    <ul className="govuk-list govuk-list--bullet">
+      <li>Register a new 406 Megahertz (MHz) beacon</li>
+      <li>Update your name, address or other details</li>
+      <li>Tell us about a change in beacon ownership</li>
+      <li>Inform us of a beacon disposal</li>
+    </ul>
+    <InsetText>
+      <>
+        This service is only for UK encoded 406MHz beacons. You can{" "}
+        <a className="govuk-link" href="#">
+          contact the UK Beacon Registry
+        </a>{" "}
+        if you are not sure if your beacon is 406MHz or not.
+      </>
+    </InsetText>
 
     <h2 className="govuk-heading-m">Before you start</h2>
 
-    <p className="govuk-body">
-      TO UPDATE: You will need certain information before you start.
-    </p>
+    <ul className="govuk-list govuk-list--bullet">
+      <li>
+        You will need to know the beacon HEX ID, manufacturer serial number and
+        model
+      </li>
+      <li>
+        If you have a vessel, you will need your vessel name, number, call sign
+        and MMSI number
+      </li>
+      <li>
+        If you have an aircraft, you will need the aircraft registration mark,
+        manufacturer and type and 24 bit HEX
+      </li>
+      <li>
+        You will also need emergency contact details for Search and Rescue
+      </li>
+    </ul>
+
+    <StartButton />
   </>
 );
 
@@ -74,8 +118,36 @@ const OtherWaysToAccessTheService: FunctionComponent = () => (
     <h2 className="govuk-heading-m">Other ways to apply</h2>
 
     <p className="govuk-body">
-      TO UPDATE: You can request postal forms by emailing.
+      You can also{" "}
+      <a className="govuk-link" href="#">
+        register by post
+      </a>
+      .
     </p>
+
+    <p className="govuk-body">
+      Or contact the UK Beacon Registry for help with registering.
+    </p>
+
+    <InsetText>
+      <>
+        <ul className="govuk-list">
+          <li className="govuk-!-font-weight-bold">The UK Beacon Registry</li>
+          <li>
+            <a className="govuk-link" href="mailto:ukbeacons@mcga.gov.uk">
+              ukbeacons@mcga.gov.uk
+            </a>
+          </li>
+          <li>Telephone: 01326 211569</li>
+          <li>Fax: 01326 319264</li>
+          <li>
+            <a className="govuk-link" href="#">
+              Find out about call charges
+            </a>
+          </li>
+        </ul>
+      </>
+    </InsetText>
   </>
 );
 
