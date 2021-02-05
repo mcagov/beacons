769796084aa178f9ea0302b49033347fcf6538ce
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-02-05 19:56:21 +0000

    Extract composite components of form groups

--- src/pages/register-a-beacon/check-beacon-details.tsx
+++ src/pages/register-a-beacon/check-beacon-details.tsx
@@ -8,9 +8,14 @@ import {
   FormFieldset,
   Input,
   FormLegend,
+  FormLabel,
+  FormGroup,
   Select,
   SelectOption,
+  FormHint,
 } from "../../components/Form";
+import { Details } from "../../components/Details";
+import { A } from "../../components/Typography";
 
 const CheckBeaconDetails: FunctionComponent = () => (
   <>
@@ -27,31 +32,91 @@ const CheckBeaconDetails: FunctionComponent = () => (
                   service.
                 </InsetText>
 
-                <Select label="Beacon manufacturer" name="beaconManufacturer">
-                  <SelectOption value="Raleigh">Raleigh</SelectOption>
-                  <SelectOption value="Giant">Giant</SelectOption>
-                  <SelectOption value="Trek">Trek</SelectOption>
-                </Select>
+                <BeaconManufacturerSelect />
 
-                <Select label="Beacon model" name="beaconModel">
-                  <SelectOption value="Chopper">Chopper</SelectOption>
-                  <SelectOption value="TCR">TCR</SelectOption>
-                  <SelectOption value="Madone">Madone</SelectOption>
-                </Select>
+                <BeaconModelSelect />
 
-                <Input
-                  label="15 digit beacon HEX ID"
-                  name="beaconHexId"
-                  spellCheck={false}
-                />
+                <BeaconHexIdInput />
               </FormFieldset>
               <Button buttonText="Submit" />
             </Form>
+            <IfYouNeedHelp />
           </>
         }
       />
     </Layout>
   </>
+);
+
+const BeaconManufacturerSelect: FunctionComponent = (): JSX.Element => (
+  <FormGroup>
+    <FormLabel htmlFor="beaconManufacturer">
+      Select your beacon manufacturer
+    </FormLabel>
+    <Select name="beaconManufacturer" defaultValue="default">
+      <option hidden disabled value="default">
+        Beacon manufacturer
+      </option>
+      <SelectOption value="Raleigh">Raleigh</SelectOption>
+      <SelectOption value="Giant">Giant</SelectOption>
+      <SelectOption value="Trek">Trek</SelectOption>
+    </Select>
+  </FormGroup>
+);
+
+const BeaconModelSelect: FunctionComponent = (): JSX.Element => (
+  <FormGroup>
+    <FormLabel htmlFor="beaconModel">Select your beacon model</FormLabel>
+    <Select name="beaconModel" defaultValue="default">
+      <option hidden disabled value="default">
+        Beacon model
+      </option>
+      <SelectOption value="Chopper">Chopper</SelectOption>
+      <SelectOption value="TCR">TCR</SelectOption>
+      <SelectOption value="Madone">Madone</SelectOption>
+    </Select>
+  </FormGroup>
+);
+
+const BeaconHexIdInput: FunctionComponent = (): JSX.Element => (
+  <FormGroup>
+    <FormLabel htmlFor="beaconHexId">
+      Enter the 15 digit beacon HEX ID
+    </FormLabel>
+    <FormHint id="beaconHexId">
+      This will be on your beacon. It must be 15 characters long and use
+      characters 0-9, A-F
+    </FormHint>
+    <Input name="beaconHexId" spellCheck={false} />
+    <br />
+    <br />
+    <Details summaryText="What does the 15 digit beacon HEX ID look like?">
+      TODO: Image of a beacon showing hex ID
+    </Details>
+  </FormGroup>
+);
+
+const IfYouNeedHelp: FunctionComponent = (): JSX.Element => (
+  <Details summaryText="If you need help completing this form">
+    <p>
+      <b>The UK Beacon Registry</b>
+      <br />
+      <A href="mailto:ukbeacons@mcga.gov.uk">ukbeacons@mcga.gov.uk</A>
+      <br />
+      Telephone: 01326 211569 <br />
+      Fax: 01326 319264 <br />
+      Monday to Friday, 9am to 5pm (except public holidays)
+      <br />
+    </p>
+    <p>
+      <A href="https://www.gov.uk/call-charges">Find out about call charges</A>
+    </p>
+    <p>
+      In an emergency in the UK, dial 999 and ask for the Coastguard. If you are
+      at sea, use GMDSS systems to make a distress or urgency alert:Emergency
+      Contact - Dial 999 and ask for the Coastguard
+    </p>
+  </Details>
 );
 
 export default CheckBeaconDetails;
