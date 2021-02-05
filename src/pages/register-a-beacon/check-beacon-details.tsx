399af809bdfc8539b05caf4f78b57e6851637908
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-02-05 11:48:13 +0000

    Check-beacon-details form POSTs data to check-beacon-summary page

--- src/pages/register-a-beacon/check-beacon-details.tsx
+++ src/pages/register-a-beacon/check-beacon-details.tsx
@@ -2,6 +2,8 @@ import React, { FunctionComponent } from "react";
 import Head from "next/head";
 import { Grid } from "../../components/Grid";
 import { InsetText } from "../../components/InsetText";
+import { Layout } from "../../components/Layout";
+import { BreadcrumbList, BreadcrumListItem } from "../../components/Breadcrumb";
 
 const CheckBeaconDetails: FunctionComponent = () => (
   <>
@@ -10,72 +12,84 @@ const CheckBeaconDetails: FunctionComponent = () => (
         Beacon Registration Service - Register a new 406 MHz distress beacon
       </title>
     </Head>
+    <Layout breadcrumbs={<Breadcrumbs />}>
+      <Grid
+        mainContent={
+          <>
+            <form
+              action="/register-a-beacon/check-beacon-summary"
+              method="POST"
+            >
+              <fieldset className="govuk-fieldset">
+                <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
+                  <h1 className="govuk-fieldset__heading">
+                    Check beacon details
+                  </h1>
+                  <InsetText>
+                    The details of your beacon must be checked to ensure they
+                    have a UK encoding and if they are already registered with
+                    this service.
+                  </InsetText>
+                </legend>
 
-    <Grid
-      mainContent={
-        <>
-          <form action="/register-a-beacon/check-beacon-summary" method="POST">
-            <fieldset className="govuk-fieldset">
-              <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
-                <h1 className="govuk-fieldset__heading">
-                  Check beacon details
-                </h1>
-                <InsetText>
-                  The details of your beacon must be checked to ensure they have
-                  a UK encoding and if they are already registered with this
-                  service.
-                </InsetText>
-              </legend>
+                <div className="govuk-form-group">
+                  <label className="govuk-label" htmlFor="beaconManufacturer">
+                    Beacon manufacturer
+                  </label>
+                  <select
+                    className="govuk-select"
+                    id="beaconManufacturer"
+                    name="beaconManufacturer"
+                  >
+                    <option value="Raleigh">Raleigh</option>
+                    <option value="Giant">Giant</option>
+                    <option value="Trek">Trek</option>
+                  </select>
+                </div>
 
-              <div className="govuk-form-group">
-                <label className="govuk-label" htmlFor="beacon-manufacturer">
-                  Beacon manufacturer
-                </label>
-                <select
-                  className="govuk-select"
-                  id="beacon-manufacturer"
-                  name="beacon-manufacturer"
-                >
-                  <option value="manufacturer_1">Raleigh</option>
-                  <option value="manufacturer_2">Giant</option>
-                  <option value="manufacturer_3">Trek</option>
-                </select>
-              </div>
+                <div className="govuk-form-group">
+                  <label className="govuk-label" htmlFor="beaconModel">
+                    Beacon model
+                  </label>
+                  <select
+                    className="govuk-select"
+                    id="beaconModel"
+                    name="beaconModel"
+                  >
+                    <option value="Chopper">Chopper</option>
+                    <option value="TCR">TCR</option>
+                    <option value="Madone">Madone</option>
+                  </select>
+                </div>
 
-              <div className="govuk-form-group">
-                <label className="govuk-label" htmlFor="beacon-model">
-                  Beacon model
-                </label>
-                <select
-                  className="govuk-select"
-                  id="beacon-model"
-                  name="beacon-manufacturer"
-                >
-                  <option value="model_1">Chopper</option>
-                  <option value="model_2">TCR</option>
-                  <option value="model_3">Madone</option>
-                </select>
-              </div>
-
-              <div className="govuk-form-group">
-                <label className="govuk-label" htmlFor="beacon-model">
-                  15 digit beacon HEX ID
-                </label>
-                <input
-                  className="govuk-input"
-                  id="beacon-hex-id"
-                  name="beacon-hex-id"
-                  type="text"
-                  spellCheck="false"
-                />
-              </div>
-            </fieldset>
-            <button>Submit</button>
-          </form>
-        </>
-      }
-    />
+                <div className="govuk-form-group">
+                  <label className="govuk-label" htmlFor="beaconHexId">
+                    15 digit beacon HEX ID
+                  </label>
+                  <input
+                    className="govuk-input"
+                    id="beaconHexId"
+                    name="beaconHexId"
+                    type="text"
+                    spellCheck="false"
+                  />
+                </div>
+              </fieldset>
+              <button>Submit</button>
+            </form>
+          </>
+        }
+      />
+    </Layout>
   </>
+);
+
+const Breadcrumbs: FunctionComponent = () => (
+  <BreadcrumbList>
+    <BreadcrumListItem>Home</BreadcrumListItem>
+    <BreadcrumListItem>Section</BreadcrumListItem>
+    <BreadcrumListItem>Subsection</BreadcrumListItem>
+  </BreadcrumbList>
 );
 
 export default CheckBeaconDetails;
--- src/pages/register-a-beacon/check-beacon-summary.tsx
+++ src/pages/register-a-beacon/check-beacon-summary.tsx
@@ -1,23 +1,58 @@
 import React, { FunctionComponent } from "react";
-import Head from "next/head";
 import { Grid } from "../../components/Grid";
+import { Layout } from "../../components/Layout";
+import { BreadcrumbList, BreadcrumListItem } from "../../components/Breadcrumb";
 
-const CheckBeaconDetails: FunctionComponent = () => (
-  <>
-    <Head>
-      <title>
-        Beacon Registration Service - Register a new 406 MHz distress beacon
-      </title>
-    </Head>
+import parse from "urlencoded-body-parser";
+import { GetServerSideProps } from "next";
 
-    <Grid
-      mainContent={
-        <>
-          <p>Form data going to appear here</p>
-        </>
-      }
-    />
-  </>
+interface CheckBeaconDetailsProps {
+  beaconManufacturer: string;
+  beaconModel: string;
+  beaconHexId: string;
+}
+
+const CheckBeaconDetails: FunctionComponent<CheckBeaconDetailsProps> = ({
+  beaconManufacturer,
+  beaconModel,
+  beaconHexId,
+}: CheckBeaconDetailsProps): JSX.Element => {
+  return (
+    <>
+      <Layout breadcrumbs={<Breadcrumbs />}>
+        <Grid
+          mainContent={
+            <>
+              <p>{beaconManufacturer}</p>
+              <p>{beaconModel}</p>
+              <p>{beaconHexId}</p>
+            </>
+          }
+        />
+      </Layout>
+    </>
+  );
+};
+
+const Breadcrumbs: FunctionComponent = () => (
+  <BreadcrumbList>
+    <BreadcrumListItem>Home</BreadcrumListItem>
+    <BreadcrumListItem>Section</BreadcrumListItem>
+    <BreadcrumListItem>Subsection</BreadcrumListItem>
+  </BreadcrumbList>
 );
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  if (context.req.method === "POST") {
+    const previousFormPageData = await parse(context.req);
+    return {
+      props: previousFormPageData,
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
 
 export default CheckBeaconDetails;
