fd93105afee8089e3aeb5e75dd5d621c18be73df
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-02-08 17:30:42 +0000

    Create GOV.UK typography components

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,15 +1,37 @@
 import React, { FunctionComponent, ReactNode } from "react";
 
-interface AProps {
+interface AnchorLinkProps {
   href: string;
   children: ReactNode;
 }
 
-export const A: FunctionComponent<AProps> = ({
+interface GovUKBodyProps {
+  children: ReactNode;
+}
+
+interface GovUKListProps {
+  children: ReactNode;
+}
+
+export const AnchorLink: FunctionComponent<AnchorLinkProps> = ({
   href,
   children,
-}: AProps): JSX.Element => (
+}: AnchorLinkProps): JSX.Element => (
   <a href={href} className="govuk-link">
     {children}
   </a>
+);
+
+export const GovUKBody: FunctionComponent<GovUKBodyProps> = ({
+  children,
+}: GovUKBodyProps): JSX.Element => <p className="govuk-body">{children}</p>;
+
+export const GovUKList: FunctionComponent<GovUKListProps> = ({
+  children,
+}: GovUKListProps): JSX.Element => <ul className="govuk-list">{children}</ul>;
+
+export const GovUKBulletedList: FunctionComponent<GovUKListProps> = ({
+  children,
+}: GovUKListProps): JSX.Element => (
+  <ul className="govuk-list govuk-list--bullet">{children}</ul>
 );
