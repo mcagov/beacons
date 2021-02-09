5e9af8ff38293ee93b7ebb64252f64de19d738da
Matthew Carr <matt.carr@madetech.com>
2021-02-09 14:35:09 +0000

    Tidy up tests

--- test/lib/middleware.test.ts
+++ test/lib/middleware.test.ts
@@ -4,46 +4,51 @@ import { cookieSessionId } from "../../src/lib/types";
 describe("Middleware Functions", () => {
   describe("cookeRedirect", () => {
     let context;
+    let writeHeadFunction;
     let endFunction;
 
     beforeEach(() => {
+      writeHeadFunction = jest.fn();
+      endFunction = jest.fn();
+
       context = {
         res: {
-          writeHead: jest.fn(),
+          writeHead: writeHeadFunction,
         },
         req: { cookies: {} },
       };
-
-      endFunction = jest.fn();
 
       context.res.writeHead.mockReturnValueOnce({ end: endFunction });
     });
 
     const assertRedirected = () => {
-      expect(context.res.writeHead).toHaveBeenCalledWith(307, {
+      cookieRedirect(context);
+
+      expect(writeHeadFunction).toHaveBeenCalledWith(307, {
         Location: "/",
       });
       expect(endFunction).toHaveBeenCalledTimes(1);
     };
 
-    it("should redirect if there are no cookies", () => {
+    const assertNotRedirected = () => {
       cookieRedirect(context);
 
+      expect(writeHeadFunction).not.toHaveBeenCalled();
+      expect(endFunction).not.toHaveBeenCalled();
+    };
+
+    it("should redirect if there are no cookies", () => {
       assertRedirected();
     });
 
     it("should not redirect if the cookie id is set ", () => {
       context.req.cookies = { [cookieSessionId]: "1" };
 
-      cookieRedirect(context);
-
-      expect(context.res.writeHead).not.toHaveBeenCalled();
+      assertNotRedirected();
     });
 
     it("should redirect if the cookie session id is not set", () => {
       context.req.cookies = { "beacons-session": "1" };
-
-      cookieRedirect(context);
 
       assertRedirected();
     });
@@ -51,15 +56,11 @@ describe("Middleware Functions", () => {
     it("should redirect if the cookie session id is set to null", () => {
       context.req.cookies = { [cookieSessionId]: null };
 
-      cookieRedirect(context);
-
       assertRedirected();
     });
 
     it("should redirect if the cookie session id is set to undefined", () => {
       context.req.cookies = { [cookieSessionId]: void 0 };
-
-      cookieRedirect(context);
 
       assertRedirected();
     });
