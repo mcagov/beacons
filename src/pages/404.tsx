cc6a20d42877076b3778f64d6d704de20e38db64
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-02-11 14:50:11 +0000

    Add basic 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "next/link";
+import { Grid } from "../components/Grid";
+import { Layout } from "../components/Layout";
+import { BeaconRegistryContactInfo } from "../components/Mca";
+
+const FourOhFour = () => {
+  return (
+    <Layout>
+      <Grid
+        mainContent={
+          <>
+            <h1 className="govuk-heading-l">Page not found</h1>
+            <p className="govuk-body">
+              If you typed the web address, check it is correct.
+            </p>
+            <p className="govuk-body">
+              If you pasted the web address, check you copied the entire
+              address.
+            </p>
+            <p className="govuk-body">
+              If the web address is correct or you selected a link or button,
+              use the details below if you need to speak to someone about your
+              beacon.
+            </p>
+            <h2 className="govuk-heading-m">Contact the UK Beacon Registry</h2>
+            <BeaconRegistryContactInfo />
+          </>
+        }
+      />
+    </Layout>
+  );
+};
+
+export default FourOhFour;
