1550eceaac5297ba5bd22263849629806bcaac62
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-02-18 15:21:31 +0000

    Unskip check-beacon-details test

--- test/pages/register-a-beacon/check-beacon-details.test.tsx
+++ test/pages/register-a-beacon/check-beacon-details.test.tsx
@@ -2,14 +2,9 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import CheckBeaconDetails from "../../../src/pages/register-a-beacon/check-beacon-details";
 
-xdescribe("CheckBeaconDetails", () => {
+describe("CheckBeaconDetails", () => {
   it("should have a back button which directs the user to the service start page", () => {
-    const formData = {
-      manufacturer: "",
-      beacon: "",
-      hexId: "",
-    };
-    render(<CheckBeaconDetails formData={formData} needsValidation={false} />);
+    render(<CheckBeaconDetails formData={{}} needsValidation={false} />);
 
     expect(screen.getByText("Back", { exact: true })).toHaveAttribute(
       "href",
