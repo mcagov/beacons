2ef55b02edd29b1637852cb856f7915419ffd710
Matthew Carr <matt.carr@madetech.com>
2021-02-19 12:12:17 +0000

    Add input test file

---
+++ test/components/Input.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { Input } from "../../src/components/Input";
+
+describe("Input", () => {
+  let id: string;
+  let label: string;
+  let hintText: string;
+
+  beforeEach(() => {
+    id = "hexId";
+    label = "Beacon Hex ID";
+    hintText = "Please enter a value Hex ID";
+  });
+
+  it("should render the input with the correct label without the hint text", () => {
+    render(<Input id={id} label={label} />);
+
+    expect(screen.getByLabelText(label)).toBeDefined();
+    expect(screen.queryByText(hintText)).toBeNull();
+  });
+
+  it("should render the hint text if provided", () => {
+    render(<Input id={id} label={label} hintText={hintText} />);
+
+    expect(screen.getByText(hintText)).toBeDefined();
+  });
+});
