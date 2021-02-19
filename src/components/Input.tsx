d42072773f52b154ac24b2c6d52c39b382781730
Matthew Carr <matt.carr@madetech.com>
2021-02-19 10:59:09 +0000

    Refactor input component into own file

---
+++ src/components/Input.tsx
@@ -0,0 +1,55 @@
+import React, {
+  FunctionComponent,
+  InputHTMLAttributes,
+  ReactNode,
+} from "react";
+import { FormHint, FormLabel } from "./Form";
+
+interface InputProps {
+  id: string;
+  label: string;
+  name?: string;
+  hintText?: string;
+  type?: string;
+  htmlAttributes?: InputHTMLAttributes<Element>;
+  defaultValue?: string;
+  numOfChars?: 2 | 3 | 4 | 5 | 10 | 20;
+}
+
+export const Input: FunctionComponent<InputProps> = ({
+  id,
+  name,
+  label,
+  hintText = null,
+  type = "text",
+  htmlAttributes = {},
+  defaultValue = "",
+  numOfChars = null,
+}: InputProps): JSX.Element => {
+  const inputWidthClass: string = numOfChars
+    ? `govuk-input--width-${numOfChars}`
+    : "";
+
+  name = name ? name : id;
+
+  let hintComponent: ReactNode;
+  if (hintText) {
+    hintComponent = <FormHint forId={id}>{hintText}</FormHint>;
+  }
+
+  return (
+    <>
+      <FormLabel htmlFor={id}>{label}</FormLabel>
+      {hintComponent}
+
+      <input
+        className={`govuk-input ${inputWidthClass}`}
+        id={id}
+        name={name}
+        type={type}
+        defaultValue={defaultValue}
+        {...htmlAttributes}
+      />
+    </>
+  );
+};
