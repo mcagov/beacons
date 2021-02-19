86a24c727b60c52a908c40356822359326882dc9
Matthew Carr <matt.carr@madetech.com>
2021-02-19 12:07:12 +0000

    Add hint text test for Input

---
+++ test/components/Form.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import { FormLabel } from "../../src/components/Form";
+
+describe("Form Components", () => {
+  describe("FormLabel", () => {
+    it("should render the label with the correct text", () => {
+      let label = "Beacon HexId";
+      let id = "hex-id";
+
+      render(
+        <>
+          <FormLabel htmlFor={id}>{label}</FormLabel>{" "}
+          <input id={id} type="text" />
+        </>
+      );
+      expect(screen.getByLabelText(label)).toBeDefined();
+    });
+  });
+});
--- test/components/Index.test.tsx
+++ test/components/Index.test.tsx
@@ -5,16 +5,24 @@ import { Input } from "../../src/components/Input";
 describe("Input", () => {
   let id: string;
   let label: string;
+  let hintText: string;
 
   beforeEach(() => {
     id = "hexId";
     label = "Beacon Hex ID";
+    hintText = "Please enter a value Hex ID";
   });
-  it("should render the input component with the same name", () => {
+
+  it("should render the input with the correct label without the hint text", () => {
     render(<Input id={id} label={label} />);
 
     expect(screen.getByLabelText(label)).toBeDefined();
+    expect(screen.queryByText(hintText)).toBeNull();
   });
 
-  it("should render the input component with a different name", () => {});
+  it("should render the hint text if provided", () => {
+    render(<Input id={id} label={label} hintText={hintText} />);
+
+    expect(screen.getByText(hintText)).toBeDefined();
+  });
 });
