4b26da12c4a6c6c9e00c7a4d6c1284f6704fc694
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-02-23 14:37:01 +0000

    Move handleFormSubmission() tests from page to utils.test

--- test/pages/register-a-beacon/primary-beacon-use.test.tsx
+++ test/pages/register-a-beacon/primary-beacon-use.test.tsx
@@ -1,26 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { GetServerSidePropsContext } from "next";
 import React from "react";
-import { formSubmissionCookieId } from "../../../src/lib/types";
+import { handleFormSubmission } from "../../../src/lib/utils";
 import PrimaryBeaconUse, {
   getServerSideProps,
 } from "../../../src/pages/register-a-beacon/primary-beacon-use";
 
-// Mock module dependencies in getServerSideProps
-jest.mock("../../../src/lib/middleware", () => ({
+jest.mock("../../../src/lib/utils", () => ({
   __esModule: true,
-  updateFormCache: jest.fn().mockReturnValue({}),
-  withCookieRedirect: jest.fn().mockImplementation((callback) => {
-    return async (context) => {
-      return callback(context);
-    };
-  }),
-}));
-jest.mock("../../../src/lib/formValidator", () => ({
-  __esModule: true,
-  FormValidator: {
-    hasErrors: jest.fn().mockReturnValue(false),
-  },
+  handleFormSubmission: jest.fn().mockImplementation(() => jest.fn()),
 }));
 
 describe("PrimaryBeaconUse", () => {
@@ -43,39 +31,11 @@ describe("PrimaryBeaconUse", () => {
   });
 
   it("should redirect to about-the-vessel page on valid form submission", async () => {
-    const mockUserSubmittedFormContext = {
-      req: {
-        method: "POST",
-      },
-    };
+    const dummyContext = {};
+    await getServerSideProps(dummyContext as GetServerSidePropsContext);
 
-    const response = await getServerSideProps(
-      mockUserSubmittedFormContext as GetServerSidePropsContext
+    expect(handleFormSubmission).toHaveBeenCalledWith(
+      "/register-a-beacon/about-the-vessel"
     );
-
-    expect(response).toStrictEqual({
-      redirect: {
-        statusCode: 303,
-        destination: "/register-a-beacon/about-the-vessel",
-      },
-    });
-  });
-
-  xdescribe("getServerSideProps()", () => {
-    let context;
-    beforeEach(() => {
-      context = {
-        req: {
-          cookies: {
-            [formSubmissionCookieId]: "1",
-          },
-        },
-      };
-    });
-
-    it("should return an empty props object", async () => {
-      const expectedProps = await getServerSideProps(context);
-      expect(expectedProps).toStrictEqual({ props: {} });
-    });
   });
 });
