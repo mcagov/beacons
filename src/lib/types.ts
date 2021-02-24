2dbad77627abbd1688ac9d0f64f62afa4503b550
Ninamma Rai <ninamma.rai@outlook.com>
2021-02-24 17:38:49 +0000

    Add fields to Vessel from about-the-vessel page

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -29,6 +29,11 @@ export interface Beacon {
 }
 
 export interface Vessel {
+  maxCapacity: string;
+  vesselName: string;
+  homeport: string;
+  areaOfOperation: string;
+  beaconLocation: string;
   moreVesselDetails: string;
 }
 
