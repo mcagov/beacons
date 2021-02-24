fc4206a46a78a92b986b239f951798785de3e30b
Ninamma Rai <ninamma.rai@outlook.com>
2021-02-24 18:07:25 +0000

    Add WIP field validators for about-the-vessel page and tests - needs refactoring

--- src/lib/fieldValidators.ts
+++ src/lib/fieldValidators.ts
@@ -94,9 +94,53 @@ export class MoreVesselDetailsValidator extends FieldValidator {
   }
 }
 
+export class VesselMaxCapacityValidator extends FieldValidator {
+  constructor() {
+    super();
+    this._rules = [
+      {
+        errorMessage: "Maximum number of persons onboard is a required field",
+        predicateFn: (value) => value.length === 0,
+      },
+      {
+        errorMessage:
+          "Maximum number of persons onboard must be a whole number",
+        predicateFn: (value) => value.match(/\D+/) !== null,
+      },
+    ];
+  }
+}
+
+export class VesselAreaOfOperationValidator extends FieldValidator {
+  constructor() {
+    super();
+    this._rules = [
+      {
+        errorMessage: "Typical area of operation has too many characters",
+        predicateFn: (value) => value.length > 250,
+      },
+    ];
+  }
+}
+
+export class VesselBeaconLocationValidator extends FieldValidator {
+  constructor() {
+    super();
+    this._rules = [
+      {
+        errorMessage: "Where the beacon is kept has too many characters",
+        predicateFn: (value) => value.length > 100,
+      },
+    ];
+  }
+}
+
 export const fieldValidatorLookup = {
   manufacturer: new BeaconManufacturerValidator(),
   model: new BeaconModelValidator(),
   hexId: new BeaconHexIdValidator(),
+  maxCapacity: new VesselMaxCapacityValidator(),
+  areaOfOperation: new VesselAreaOfOperationValidator(),
+  beaconLocation: new VesselBeaconLocationValidator(),
   moreVesselDetails: new MoreVesselDetailsValidator(),
 };
