7d8798d43ee6ebcff151951b8d066d6736cf7114
Ninamma Rai <ninamma.rai@outlook.com>
2021-02-25 12:55:55 +0000

    Add test for POST and GET urls for about-the-vessel page

--- test/pages/register-a-beacon/about-the-vessel.test.tsx
+++ test/pages/register-a-beacon/about-the-vessel.test.tsx
@@ -1,6 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { GetServerSidePropsContext } from "next";
 import React from "react";
-import AboutTheVessel from "../../../src/pages/register-a-beacon/about-the-vessel";
+import { handlePageRequest } from "../../../src/lib/handlePageRequest";
+import AboutTheVessel, {
+  getServerSideProps,
+} from "../../../src/pages/register-a-beacon/about-the-vessel";
+
+jest.mock("../../../src/lib/handlePageRequest", () => ({
+  __esModule: true,
+  handlePageRequest: jest.fn().mockImplementation(() => jest.fn()),
+}));
 
 describe("AboutTheVessel", () => {
   it("should have a back button which directs the user to the primary beacon use page", () => {
@@ -9,6 +18,26 @@ describe("AboutTheVessel", () => {
     expect(screen.getByText("Back", { exact: true })).toHaveAttribute(
       "href",
       "/register-a-beacon/primary-beacon-use"
+    );
+  });
+
+  it("should POST its form submission to itself for redirection via getServerSideProps()", () => {
+    const { container } = render(
+      <AboutTheVessel formData={{}} needsValidation={false} />
+    );
+    const ownPath = "/register-a-beacon/about-the-vessel";
+
+    const form = container.querySelector("form");
+
+    expect(form).toHaveAttribute("action", ownPath);
+  });
+
+  it("should redirect to vessel-communications page on valid form submission", async () => {
+    const context = {};
+    await getServerSideProps(context as GetServerSidePropsContext);
+
+    expect(handlePageRequest).toHaveBeenCalledWith(
+      "/register-a-beacon/vessel-communications"
     );
   });
 });
