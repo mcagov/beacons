981eac65c0b9d1b7b5e85610fc2b4142fcf439da
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-02-25 14:21:55 +0000

    Extract FormInputProps interface to Input.tsx for reuse

--- src/pages/register-a-beacon/check-beacon-details.tsx
+++ src/pages/register-a-beacon/check-beacon-details.tsx
@@ -15,7 +15,7 @@ import {
   FormLegendPageHeading,
 } from "../../components/Form";
 import { Grid } from "../../components/Grid";
-import { Input } from "../../components/Input";
+import { FormInputProps, Input } from "../../components/Input";
 import { InsetText } from "../../components/InsetText";
 import { Layout } from "../../components/Layout";
 import { IfYouNeedHelp } from "../../components/Mca";
@@ -32,12 +32,6 @@ import { ensureFormDataHasKeys } from "../../lib/utils";
 interface CheckBeaconDetailsProps {
   formData: CacheEntry;
   needsValidation?: boolean;
-}
-
-interface FormInputProps {
-  value: string;
-  errorMessages: string[];
-  showErrors: boolean;
 }
 
 const CheckBeaconDetails: FunctionComponent<CheckBeaconDetailsProps> = ({
