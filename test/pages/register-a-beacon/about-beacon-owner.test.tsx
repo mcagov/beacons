41cc2fef67e91aad4cfd7ef82dd85d41961c4853
Matthew Carr <matt.carr@madetech.com>
2021-02-26 19:36:59 +0000

    Fix up FormGroup tests

--- test/pages/register-a-beacon/about-beacon-owner.test.tsx
+++ test/pages/register-a-beacon/about-beacon-owner.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { GetServerSidePropsContext } from "next";
 import React from "react";
-import { formSubmissionCookieId } from "../../../src/lib/types";
+import { handlePageRequest } from "../../../src/lib/handlePageRequest";
 import AboutBeaconOwner, {
   getServerSideProps,
 } from "../../../src/pages/register-a-beacon/about-beacon-owner";
+
+jest.mock("../../../src/lib/handlePageRequest", () => ({
+  __esModule: true,
+  handlePageRequest: jest.fn().mockImplementation(() => jest.fn()),
+}));
 
 describe("AboutBeaconOwner", () => {
   it("should have a back button which directs the user to the primary beacon use page", () => {
@@ -16,22 +22,14 @@ describe("AboutBeaconOwner", () => {
   });
 
   describe("getServerSideProps()", () => {
-    let context;
-    beforeEach(() => {
-      context = {
-        req: {
-          cookies: {
-            [formSubmissionCookieId]: "1",
-          },
-        },
-      };
-    });
-
     it("should return a largely empty props object", async () => {
-      const expectedProps = await getServerSideProps(context);
-      expect(expectedProps).toStrictEqual({
-        props: { formData: {}, needsValidation: false },
-      });
+      const context = {};
+      await getServerSideProps(context as GetServerSidePropsContext);
+
+      expect(handlePageRequest).toHaveBeenCalledWith(
+        "/register-a-beacon/beacon-owner-address",
+        expect.anything()
+      );
     });
   });
 });
