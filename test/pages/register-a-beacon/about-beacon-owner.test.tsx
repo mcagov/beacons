e1fb86f28da4c553d800d0cb9a2eb06684805d72
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-02-26 09:19:55 +0000

    Test about-b-o page re-directs to b-o-address on valid form submission

--- test/pages/register-a-beacon/about-beacon-owner.test.tsx
+++ test/pages/register-a-beacon/about-beacon-owner.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { GetServerSidePropsContext } from "next";
 import React from "react";
-import { formSubmissionCookieId } from "../../../src/lib/types";
+import { handlePageRequest } from "../../../src/lib/handlePageRequest";
 import AboutBeaconOwner, {
   getServerSideProps,
 } from "../../../src/pages/register-a-beacon/about-beacon-owner";
+
+jest.mock("../../../src/lib/handlePageRequest", () => ({
+  __esModule: true,
+  handlePageRequest: jest.fn().mockImplementation(() => jest.fn()),
+}));
 
 describe("AboutBeaconOwner", () => {
   it("should have a back button which directs the user to the primary beacon use page", () => {
@@ -15,23 +21,23 @@ describe("AboutBeaconOwner", () => {
     );
   });
 
-  describe("getServerSideProps()", () => {
-    let context;
-    beforeEach(() => {
-      context = {
-        req: {
-          cookies: {
-            [formSubmissionCookieId]: "1",
-          },
-        },
-      };
-    });
+  it("should POST its form submission to itself for redirection via getServerSideProps()", () => {
+    const { container } = render(
+      <AboutBeaconOwner formData={{}} needsValidation={false} />
+    );
+    const ownPath = "/register-a-beacon/about-beacon-owner";
 
-    it("should return a largely empty props object", async () => {
-      const expectedProps = await getServerSideProps(context);
-      expect(expectedProps).toStrictEqual({
-        props: { formData: {}, needsValidation: false },
-      });
-    });
+    const form = container.querySelector("form");
+
+    expect(form).toHaveAttribute("action", ownPath);
+  });
+
+  it("should redirect to the beacon-owner-address page on valid form submission", async () => {
+    const context = {};
+    await getServerSideProps(context as GetServerSidePropsContext);
+
+    const startURL = "/register-a-beacon/beacon-owner-address";
+
+    expect(handlePageRequest).toHaveBeenCalledWith(startURL);
   });
 });
