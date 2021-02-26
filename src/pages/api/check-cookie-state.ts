d36f8827e68f09ebeb9d3665709d8da1cea586c9
Matthew Carr <matt.carr@madetech.com>
2021-02-26 11:57:13 +0000

    Add tests for check-cookie-state function

--- src/pages/api/check-cookie-state.ts
+++ src/pages/api/check-cookie-state.ts
@@ -5,7 +5,7 @@ import { acceptRejectCookieId } from "../../lib/types";
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
   const cookies: NextApiRequestCookies = req.cookies;
-  const oneYearFromNow = new Date();
+  const oneYearFromNow: Date = new Date();
   oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
 
   if (!cookies || !cookies[acceptRejectCookieId]) {
