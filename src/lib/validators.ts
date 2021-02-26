87d7014573f56f092d68802f4cda249e340684ab
Matthew Carr <matt.carr@madetech.com>
2021-02-26 09:52:53 +0000

    Initial commit for form validatino refactor

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -7,7 +7,7 @@ export class Validators {
   /**
    * Validator that requires the value to be non-empty.
    *
-   * @param errorMessage {string}            The error message to return if the rule is violated
+   * @param errorMessage {string}            An error message if the rule is violated
    * @returns            {IFieldValidator}   The field validator instance
    */
   public static required(errorMessage: string): IFieldValidator {
@@ -17,11 +17,25 @@ export class Validators {
   /**
    * Validator that requires the value to be less than or equal to the provided number.
    *
-   * @param errorMessage {string}            The error message to return if the rule is violated
+   * @param errorMessage {string}            An error message if the rule is violated
    * @param max          {number}            The max number of characters allowed
-   * @returns            {IFieldValidator}   The field validator instance
+   * @returns            {IFieldValidator}   A validator instance
    */
   public static max(errorMessage: string, max: number): IFieldValidator {
+    return null;
+  }
+
+  /**
+   * Validator that requires the value to match a regular expression pattern provided.
+   *
+   * @param errorMessage {string}            An error message if the rule is violated
+   * @param pattern      {RegExp}            A regular expression to be used to test the value
+   * @returns            {IFieldValidator}   A validator instance
+   */
+  public static pattern(
+    errorMessage: string,
+    pattern: RegExp
+  ): IFieldValidator {
     return null;
   }
 }
