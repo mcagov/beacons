92279d91ecfaa869aebc6ab09dd7383c17299b11
Matthew Carr <matt.carr@madetech.com>
2021-03-02 08:51:49 +0000

    Move methods into field input class

---
+++ src/lib/form/abstractFormNode.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FieldManager } from "./fieldManager";
+import { ValidationRule } from "./validators";
+
+type ControlValue = string | Record<string, AbstractFormNode>;
+
+/**
+ * This is the base class for `FieldInput`, and `FieldManager`.  It represents a node in the form tree of depth 1.
+ */
+export abstract class AbstractFormNode {
+  protected _value: any;
+
+  /**
+   * A node is `pristine` if the user has not edited this node within the form.
+   *
+   * This is the case if the user has not submitted form data to the server to be validated.
+   */
+  protected pristine = true;
+
+  private _parent: FieldManager = null;
+
+  constructor(
+    value: ControlValue,
+    public readonly validators: ValidationRule[]
+  ) {
+    this._value = value;
+  }
+
+  /**
+   * @param parent {FieldManager}   Sets the parent form group control
+   */
+  setParent(parent: FieldManager): void {
+    this._parent = parent;
+  }
+
+  /**
+   * Getter for the parent control.
+   */
+  public get parent(): FieldManager {
+    return this._parent;
+  }
+
+  /**
+   * Marks the control as `dirty`.
+   */
+  public markAsDirty(): void {
+    this.pristine = false;
+  }
+}
