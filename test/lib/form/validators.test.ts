5c0bb17d5c7495467fd1b3fb500251d982f3b40d
Matthew Carr <matt.carr@madetech.com>
2021-03-04 12:16:23 +0000

    Add tests

--- test/lib/form/validators.test.ts
+++ test/lib/form/validators.test.ts
@@ -85,6 +85,58 @@ describe("Form Validators", () => {
     });
   });
 
+  describe("isValidDate", () => {
+    beforeEach(() => {
+      ({ errorMessage, applies } = Validators.isValidDate(
+        expectedErrorMessage
+      ));
+    });
+
+    it("should have an error if the value is null", () => {
+      expect(applies(null)).toBe(true);
+    });
+
+    it("should have an error if the not a date string", () => {
+      expect(applies("beacon information")).toBe(true);
+    });
+
+    it("should not have an error if the value is a valid date string", () => {
+      expect(applies(new Date().toISOString())).toBe(false);
+    });
+  });
+
+  describe("isInThePast", () => {
+    beforeEach(() => {
+      ({ errorMessage, applies } = Validators.isInThePast(
+        expectedErrorMessage
+      ));
+    });
+
+    it("should not have an error if the value is null", () => {
+      expect(applies(null)).toBe(false);
+    });
+
+    it("should not have an error if the not a date string", () => {
+      expect(applies("beacon information")).toBe(false);
+    });
+
+    it("should not have an error if the date is now", () => {
+      expect(applies(new Date().toISOString())).toBe(false);
+    });
+
+    it("should have an error if the date is in the past", () => {
+      const date = new Date();
+      date.setDate(date.getDate() - 1);
+      expect(applies(new Date().toISOString())).toBe(false);
+    });
+
+    it("should have an error if the date is in the future", () => {
+      const date = new Date();
+      date.setDate(date.getDate() + 1);
+      expect(applies(date.toISOString())).toBe(true);
+    });
+  });
+
   describe("hexId", () => {
     beforeEach(() => {
       ({ errorMessage, applies } = Validators.hexId(expectedErrorMessage));
