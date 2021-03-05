edd66ab6bb028a3df0b622f3899a47352e40e9c2
Matthew Carr <matt.carr@madetech.com>
2021-03-05 14:31:25 +0000

    Remove the getServerSideProps test from 404 page

--- test/pages/404.test.tsx
+++ test/pages/404.test.tsx
@@ -1,40 +1,9 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import { acceptRejectCookieId } from "../../src/lib/types";
-import FourOhFour, { getServerSideProps } from "../../src/pages/404";
+import FourOhFour from "../../src/pages/404";
 
 describe("404 Page", () => {
   it("should render the page", () => {
-    render(<FourOhFour showCookieBanner={false} />);
-  });
-
-  describe("getServerSideProps()", () => {
-    let context;
-
-    beforeEach(() => {
-      context = {
-        req: {
-          cookies: {},
-        },
-      };
-    });
-    it("should set showCookieBanner to true if the user has not accepted the cookie policy", async () => {
-      const props = await getServerSideProps(context);
-      expect(props).toStrictEqual({
-        props: {
-          showCookieBanner: true,
-        },
-      });
-    });
-
-    it("should set showCookieBanner to false if the user has accepted the cookie policy", async () => {
-      context.req.cookies[acceptRejectCookieId] = "id";
-      const props = await getServerSideProps(context);
-      expect(props).toStrictEqual({
-        props: {
-          showCookieBanner: false,
-        },
-      });
-    });
+    render(<FourOhFour />);
   });
 });
