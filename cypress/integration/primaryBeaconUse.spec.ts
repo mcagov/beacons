31dcf3219cfb6c3eff3fa92c53e7f7713ce2529e
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-03-15 16:51:56 +0000

    Update primary-beacon-use page tests

--- cypress/integration/primaryBeaconUse.spec.ts
+++ cypress/integration/primaryBeaconUse.spec.ts
@@ -1,22 +1,43 @@
 import {
+  givenIAmAt,
   givenIHaveSelected,
   requiredFieldErrorMessage,
   thenIShouldSeeAnErrorMessageThatContains,
+  thenIShouldSeeAnErrorSummaryLinkThatContains,
+  thenMyFocusMovesTo,
   thenTheUrlShouldContain,
   whenIClickContinue,
+  whenIClickOnTheErrorSummaryLinkContaining,
   whenIType,
 } from "./common.spec";
 
 describe("As a beacon owner, I want to submit the primary use for my beacon", () => {
-  const pageLocation = "/register-a-beacon/primary-beacon-use";
+  const pageUrl = "/register-a-beacon/primary-beacon-use";
 
   beforeEach(() => {
-    givenIAmOnThePrimaryBeaconUsePage();
+    givenIAmAt(pageUrl);
   });
 
   it("displays an error if no primary beacon use is selected", () => {
     whenIClickContinue();
     thenIShouldSeeAnErrorMessageThatContains(requiredFieldErrorMessage);
+    thenIShouldSeeAnErrorSummaryLinkThatContains(
+      "Maritime pleasure use",
+      requiredFieldErrorMessage
+    );
+  });
+
+  xit("focuses me on the first radio button if there is an error", () => {
+    // TODO issue #148 (https://github.com/mcagov/beacons-webapp/issues/148)
+    whenIClickContinue();
+    thenIShouldSeeAnErrorMessageThatContains(requiredFieldErrorMessage);
+    thenIShouldSeeAnErrorSummaryLinkThatContains(
+      "Maritime pleasure use",
+      requiredFieldErrorMessage
+    );
+
+    whenIClickOnTheErrorSummaryLinkContaining(requiredFieldErrorMessage);
+    thenMyFocusMovesTo("#motor-vessel");
   });
 
   it("routes to the next page if there are no errors with the selected primary beacon use", () => {
@@ -29,7 +50,15 @@ describe("As a beacon owner, I want to submit the primary use for my beacon", ()
   it("displays an error if 'Other pleasure vessel' is selected, but no text is provided", () => {
     givenIHaveSelected("#other-pleasure-vessel");
     whenIClickContinue();
-    thenIShouldSeeAnErrorMessageThatContains(requiredFieldErrorMessage);
+    thenIShouldSeeAnErrorMessageThatContains(
+      "Other pleasure vessel",
+      requiredFieldErrorMessage
+    );
+    whenIClickOnTheErrorSummaryLinkContaining(
+      "Other pleasure vessel",
+      requiredFieldErrorMessage
+    );
+    thenMyFocusMovesTo("#otherPleasureVesselText");
   });
 
   it("routes to the next page if there are no errors with Other pleasure vessel selected", () => {
@@ -39,9 +68,4 @@ describe("As a beacon owner, I want to submit the primary use for my beacon", ()
 
     thenTheUrlShouldContain("/register-a-beacon/about-the-vessel");
   });
-
-  const givenIAmOnThePrimaryBeaconUsePage = () => {
-    cy.visit("/");
-    cy.visit(pageLocation);
-  };
 });
