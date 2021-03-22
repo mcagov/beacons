5d56544cb777085c8b24244079ad33162de1347c
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2021-03-22 15:01:07 +0000

    Create gov notify gateway API

---
+++ src/gateways/gov-notify-api-gateway.ts
@@ -0,0 +1,26 @@
+import { NotifyClient } from "notifications-node-client";
+
+export class GovNotifyGateway {
+  api;
+  constructor() {
+    this.api = new NotifyClient(process.env.GOV_NOTIFY_API_KEY);
+  }
+
+  public sendEmail(emailTemplateId: string, email: string, personalisation={}): boolean {
+    for (const key in personalisation) {
+      if (typeof personalisation[key] == 'undefined') personalisation[key] = "";
+    }
+
+    try {
+      this.api.sendEmail(emailTemplateId, email, {
+        personalisation: personalisation,
+        reference: personalisation["reference"]
+      }).catch(err => {""});
+
+      return true;
+    }
+    catch(error) {
+      return false;
+    }
+  }
+}
