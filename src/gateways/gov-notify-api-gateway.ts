9a061aff73147167617f3899aa90343504c7fe9b
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2021-03-22 15:05:29 +0000

    Refactor: formatting

--- src/gateways/gov-notify-api-gateway.ts
+++ src/gateways/gov-notify-api-gateway.ts
@@ -6,20 +6,27 @@ export class GovNotifyGateway {
     this.api = new NotifyClient(process.env.GOV_NOTIFY_API_KEY);
   }
 
-  public sendEmail(emailTemplateId: string, email: string, personalisation={}): boolean {
+  public sendEmail(
+    emailTemplateId: string,
+    email: string,
+    personalisation = {}
+  ): boolean {
     for (const key in personalisation) {
-      if (typeof personalisation[key] == 'undefined') personalisation[key] = "";
+      if (typeof personalisation[key] == "undefined") personalisation[key] = "";
     }
 
     try {
-      this.api.sendEmail(emailTemplateId, email, {
-        personalisation: personalisation,
-        reference: personalisation["reference"]
-      }).catch(err => {""});
+      this.api
+        .sendEmail(emailTemplateId, email, {
+          personalisation: personalisation,
+          reference: personalisation["reference"],
+        })
+        .catch((err) => {
+          return err;
+        });
 
       return true;
-    }
-    catch(error) {
+    } catch (error) {
       return false;
     }
   }
--- src/pages/register-a-beacon/application-complete.tsx
+++ src/pages/register-a-beacon/application-complete.tsx
@@ -1,34 +1,87 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import React, { FunctionComponent } from "react";
 import { Grid } from "../../components/Grid";
 import { Layout } from "../../components/Layout";
 import { Panel } from "../../components/Panel";
 import { GovUKBody } from "../../components/Typography";
 import { WarningText } from "../../components/WarningText";
-import { withCookieRedirect } from "../../lib/middleware";
+import { GovNotifyGateway } from "../../gateways/gov-notify-api-gateway";
+import { CacheEntry } from "../../lib/formCache";
+import { getCache, withCookieRedirect } from "../../lib/middleware";
+import { joinStrings, referenceNumber } from "../../lib/utils";
 
-interface ApplicationCompletePageProps {
-  showCookieBanner: boolean;
+interface ApplicationCompleteProps {
+  formData: CacheEntry;
 }
 
-const ApplicationCompletePage: FunctionComponent<ApplicationCompletePageProps> = ({
-  showCookieBanner,
-}: ApplicationCompletePageProps): JSX.Element => {
+const ApplicationCompletePage: FunctionComponent<ApplicationCompleteProps> = ({
+  formData,
+}: ApplicationCompleteProps): JSX.Element => {
   const pageHeading = "Application Complete";
+  let pageSubHeading =
+    process.env.NEXT_PUBLIC_GOV_NOTIFY_CUSTOMER_EMAIL_TEMPLATE;
+  if (process.env.NEXT_PUBLIC_GOV_NOTIFY_CUSTOMER_EMAIL_TEMPLATE) {
+    const govNotifyGateway = new GovNotifyGateway();
+
+    govNotifyGateway.sendEmail(
+      process.env.NEXT_PUBLIC_GOV_NOTIFY_CUSTOMER_EMAIL_TEMPLATE,
+      formData.beaconOwnerEmail,
+      {
+        owner_name: formData.beaconOwnerFullName,
+        reference: referenceNumber("A#", 7),
+        beacon_information: joinStrings([
+          formData.manufacturer,
+          formData.model,
+          formData.hexId,
+        ]),
+        owner_details: joinStrings([
+          formData.beaconOwnerFullName,
+          formData.beaconOwnerTelephoneNumber,
+          formData.beaconOwnerAlternativeTelephoneNumber,
+          formData.beaconOwnerEmail,
+        ]),
+        owner_address: joinStrings([
+          formData.beaconOwnerAddressLine1,
+          formData.beaconOwnerAddressLine2,
+          formData.beaconOwnerTownOrCity,
+          formData.beaconOwnerPostcode,
+        ]),
+        emergency_contact_1_name: formData.emergencyContact1FullName,
+        emergency_contact_1_telephone_number:
+          formData.emergencyContact1TelephoneNumber,
+        emergency_contact_1_alternative_telephone_number:
+          formData.emergencyContact1AlternativeTelephoneNumber,
+        emergency_contact_2_name: formData.emergencyContact2FullName,
+        emergency_contact_2_telephone_number:
+          formData.emergencyContact2TelephoneNumber,
+        emergency_contact_2_alternative_telephone_number:
+          formData.emergencyContact2AlternativeTelephoneNumber,
+        emergency_contact_3_name: formData.emergencyContact3FullName,
+        emergency_contact_3_telephone_number:
+          formData.emergencyContact3TelephoneNumber,
+        emergency_contact_3_alternative_telephone_number:
+          formData.emergencyContact3AlternativeTelephoneNumber,
+      }
+    );
+
+    pageSubHeading = "We have sent you a confirmation email.";
+  }
+
+  if (!pageSubHeading) {
+    //pageSubHeading += "We could not send you a confirmation email.";
+  }
 
   return (
     <>
       <Layout
         title={pageHeading}
         pageHasErrors={false}
-        showCookieBanner={showCookieBanner}
+        showCookieBanner={false}
       >
         <Grid
           mainContent={
             <>
-              <Panel title={pageHeading}>
-                We have sent you a confirmation email.
-              </Panel>
+              <Panel title={pageHeading}>{pageSubHeading}</Panel>
               <ApplicationCompleteWhatNext />
               <WarningText>
                 <GovUKBody className="govuk-!-font-weight-bold">
@@ -64,9 +117,12 @@ const ApplicationCompleteWhatNext: FunctionComponent = (): JSX.Element => (
 );
 
 export const getServerSideProps: GetServerSideProps = withCookieRedirect(
-  async () => {
+  async (context: GetServerSidePropsContext) => {
+    const formData: CacheEntry = getCache(context.req.cookies);
+    // TODO: State persistence stuff to go her
+
     return {
-      props: {},
+      props: { formData },
     };
   }
 );
