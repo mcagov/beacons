6c4543b4d497909649d872b4122fe448774fd9b8
Matthew Carr <matt.carr@madetech.com>
2021-03-31 07:46:04 +0100

    Add custom 500 page for unhandled errors

---
+++ src/pages/500.tsx
@@ -0,0 +1,41 @@
+import React, { FunctionComponent } from "react";
+import { Grid } from "../components/Grid";
+import { Layout } from "../components/Layout";
+import { BeaconRegistryContactInfo } from "../components/Mca";
+import {
+  GovUKBody,
+  PageHeading,
+  SectionHeading,
+} from "../components/Typography";
+
+const Custom500: FunctionComponent = () => {
+  const pageHeading = "Sorry, there is a problem with the service";
+
+  return (
+    <Layout title={pageHeading} pageHasErrors={false} showCookieBanner={false}>
+      <Grid
+        mainContent={
+          <>
+            <PageHeading>{pageHeading}</PageHeading>
+            <GovUKBody>
+              If you typed the web address, check it is correct.
+            </GovUKBody>
+            <GovUKBody>
+              If you pasted the web address, check you copied the entire
+              address.
+            </GovUKBody>
+            <GovUKBody>
+              If the web address is correct or you selected a link or button,
+              use the details below if you need to speak to someone about your
+              beacon.
+            </GovUKBody>
+            <SectionHeading>Contact the UK Beacon Registry</SectionHeading>
+            <BeaconRegistryContactInfo />
+          </>
+        }
+      />
+    </Layout>
+  );
+};
+
+export default Custom500;
---
+++ test/pages/500.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import Custom500 from "../../src/pages/500";
+
+describe("Custom 500 page", () => {
+  it("should render the page", () => {
+    render(<Custom500 />);
+  });
+});
