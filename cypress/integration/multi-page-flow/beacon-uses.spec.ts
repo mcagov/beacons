e75b559e2101ed5ca579970f6aff24101b2ba0ff
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-03-31 16:17:56 +0100

    Failing page flow test: Maritime environment -> Commercial purpose

---
+++ cypress/integration/multi-page-flow/beacon-uses.spec.ts
@@ -0,0 +1,55 @@
+import {
+  andIClickContinue,
+  givenIAmAt,
+  givenIHaveSelected,
+  iCanSeeAHeadingThatContains,
+  thenTheUrlShouldContain,
+} from "../common.spec";
+
+describe("As a beacon owner in the maritime environment,", () => {
+  const environmentUrl = "/register-a-beacon/beacon-use";
+  const purposeUrl = "/register-a-beacon/purpose";
+  const activityUrl = "/register-a-beacon/activity";
+
+  const aboutTheVesselUrl = "/register-a-beacon/about-the-vessel";
+
+  describe("adding a beacon use directs me along a page flow relevant to me", () => {
+    it("directs me to the correct Purpose and Activity pages", () => {
+      givenIAmAt(environmentUrl);
+      givenIHaveSelected("#maritime");
+      andIClickContinue();
+      thenTheUrlShouldContain(purposeUrl);
+      iCanSeeAHeadingThatContains("maritime");
+
+      givenIHaveSelected("#pleasure");
+      andIClickContinue();
+      thenTheUrlShouldContain(activityUrl);
+      iCanSeeAHeadingThatContains("maritime");
+      iCanSeeAHeadingThatContains("pleasure");
+
+      givenIHaveSelected("#rowing-vessel");
+      andIClickContinue();
+      thenTheUrlShouldContain(aboutTheVesselUrl);
+      iCanSeeAHeadingThatContains("vessel");
+    });
+
+    it("directs me to the correct Purpose and Activity pages", () => {
+      givenIAmAt(environmentUrl);
+      givenIHaveSelected("#maritime");
+      andIClickContinue();
+      thenTheUrlShouldContain(purposeUrl);
+      iCanSeeAHeadingThatContains("maritime");
+
+      givenIHaveSelected("#commercial");
+      andIClickContinue();
+      thenTheUrlShouldContain(activityUrl);
+      iCanSeeAHeadingThatContains("maritime");
+      iCanSeeAHeadingThatContains("commercial");
+
+      givenIHaveSelected("#sailing");
+      andIClickContinue();
+      thenTheUrlShouldContain(aboutTheVesselUrl);
+      iCanSeeAHeadingThatContains("vessel");
+    });
+  });
+});
