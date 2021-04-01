0c6cf975033646d20bfa3e40131089543faba224
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-01 09:48:30 +0100

    ✅ Back btn more-details -> <environment>-communications

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -1,2 +1,8 @@
 export const ofcomLicenseUrl =
   "https://www.ofcom.org.uk/manage-your-licence/radiocommunication-licences/ships-radio";
+
+export enum PageURLs {
+  vessel_communications = "/register-a-beacon/vessel-communnications",
+  aircraft_communications = "/register-a-beacon/aircraft-communications",
+  land_other_communications = "/register-a-beacon/land-other-communications",
+}
--- src/pages/register-a-beacon/more-details.tsx
+++ src/pages/register-a-beacon/more-details.tsx
@@ -17,6 +17,8 @@ import { FormManager } from "../../lib/form/formManager";
 import { Validators } from "../../lib/form/validators";
 import { FormSubmission } from "../../lib/formCache";
 import { FormPageProps, handlePageRequest } from "../../lib/handlePageRequest";
+import { Environment } from "../../lib/registration/types";
+import { PageURLs } from "../../lib/urls";
 
 interface MoreDetailsTextAreaProps {
   id: string;
@@ -39,15 +41,22 @@ const definePageForm = ({ moreDetails }: FormSubmission): FormManager => {
 const MoreDetails: FunctionComponent<FormPageProps> = ({
   form,
   showCookieBanner,
+  flattenedRegistration,
 }: FormPageProps): JSX.Element => {
+  const environment = flattenedRegistration.environment;
+  const previousPageUrlMap = {
+    [Environment.MARITIME]: PageURLs.vessel_communications,
+    [Environment.AVIATION]: PageURLs.aircraft_communications,
+    [Environment.LAND]: PageURLs.land_other_communications,
+    [Environment.OTHER]: PageURLs.land_other_communications,
+  };
+
   const pageHeading = "Provide more details that could help in a search";
 
   return (
     <>
       <Layout
-        navigation={
-          <BackButton href="/register-a-beacon/vessel-communications" />
-        }
+        navigation={<BackButton href={previousPageUrlMap[environment]} />}
         title={pageHeading}
         pageHasErrors={form.hasErrors}
         showCookieBanner={showCookieBanner}
