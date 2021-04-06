5a5f6364329c60cf4047809ca433824dd05aaf9a
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-06 16:17:36 +0100

    Fix linting

--- test/lib/form/validators/mmsiNumberValidator.test.ts
+++ test/lib/form/validators/mmsiNumberValidator.test.ts
@@ -2,11 +2,10 @@ import { ValidatorFn, Validators } from "../../../../src/lib/form/validators";
 
 describe("isLength validator", () => {
   const expectedErrorMessage = "is a validated field";
-  let errorMessage: string;
   let applies: ValidatorFn;
 
   beforeEach(() => {
-    ({ errorMessage, applies } = Validators.mmsiNumber(expectedErrorMessage));
+    ({ applies } = Validators.mmsiNumber(expectedErrorMessage));
   });
 
   it("should require numbers only", () => {
