ec58a95245cef86aa786f38096816109fbdbe42c
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-06 10:58:40 +0100

    Remove back/forward flow tests from purpose page
    (Covered in environments-and-purposes.spec.tsx)

--- cypress/integration/purpose.spec.ts
+++ cypress/integration/purpose.spec.ts
@@ -1,41 +1,33 @@
+import { PageURLs } from "../../src/lib/urls";
 import {
+  andIClickContinue,
+  andIHaveEnteredNoInformation,
+  andIHaveSelected,
   givenIHaveACookieSetAndIVisit,
-  givenIHaveSelected,
-  iCanClickTheBackLinkToGoToPreviousPage,
   requiredFieldErrorMessage,
   thenIShouldSeeAnErrorMessageThatContains,
-  thenMyFocusMovesTo,
-  thenTheUrlShouldContain,
   whenIClickContinue,
-  whenIClickOnTheErrorSummaryLinkContaining,
 } from "./common.spec";
 
 describe("As a beacon owner, I want to submit the purpose for my beacon", () => {
-  const thisPageUrl = "/register-a-beacon/purpose";
-  const previousPageUrl = "/register-a-beacon/beacon-use";
-
-  beforeEach(() => {
-    givenIHaveACookieSetAndIVisit(thisPageUrl);
-  });
-
-  xit("allows me to go back a page by following the 'back' button", () => {
-    iCanClickTheBackLinkToGoToPreviousPage(previousPageUrl);
-  });
-
-  xit("displays an error if no beacon use purpose is selected", () => {
+  it("displays an error if no beacon use purpose is selected", () => {
+    givenIAmAnAviationBeaconOwner();
+    andIHaveEnteredNoInformation();
     whenIClickContinue();
     thenIShouldSeeAnErrorMessageThatContains(requiredFieldErrorMessage);
-    whenIClickOnTheErrorSummaryLinkContaining(
-      "purpose",
-      requiredFieldErrorMessage
-    );
-    thenMyFocusMovesTo("#pleasure");
-  });
-
-  xit("routes to the next page if there are no errors with the selected primary beacon use", () => {
-    givenIHaveSelected("#pleasure");
-    whenIClickContinue();
-
-    thenTheUrlShouldContain("/register-a-beacon/activity");
+    // TODO: Uncomment below assertions when issue #148 fixed.  Error link
+    // should move focus to first radio button.
+    //
+    // whenIClickOnTheErrorSummaryLinkContaining(
+    //   "purpose",
+    //   requiredFieldErrorMessage
+    // );
+    // thenMyFocusMovesTo("#pleasure");
   });
 });
+
+const givenIAmAnAviationBeaconOwner = () => {
+  givenIHaveACookieSetAndIVisit(PageURLs.environment);
+  andIHaveSelected("#aviation");
+  andIClickContinue();
+};
