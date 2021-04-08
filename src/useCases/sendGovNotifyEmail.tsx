106be03dcf09f83698e92871810e54a6b3e4700b
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2021-04-08 16:24:40 +0100

    Stop bombarding Marten with all kinds of emails

--- src/useCases/sendGovNotifyEmail.tsx
+++ src/useCases/sendGovNotifyEmail.tsx
@@ -12,48 +12,42 @@ export class SendGovNotifyEmail {
     const templateId = process.env.GOV_NOTIFY_CUSTOMER_EMAIL_TEMPLATE;
 
     if (templateId) {
-      return this.gateway.sendEmail(
-        templateId,
-        registration.ownerEmail
-          ? registration.ownerEmail
-          : "marten.wetterberg@madetech.com",
-        {
-          owner_name: registration.ownerFullName,
-          reference: registration.reference,
-          beacon_information: joinStrings([
-            registration.manufacturer,
-            registration.model,
-            registration.hexId,
-          ]),
-          owner_details: joinStrings([
-            registration.ownerFullName,
-            registration.ownerTelephoneNumber,
-            registration.ownerAlternativeTelephoneNumber,
-            registration.ownerEmail,
-          ]),
-          owner_address: joinStrings([
-            registration.ownerAddressLine1,
-            registration.ownerAddressLine2,
-            registration.ownerTownOrCity,
-            registration.ownerPostcode,
-          ]),
-          emergency_contact_1_name: registration.emergencyContact1FullName,
-          emergency_contact_1_telephone_number:
-            registration.emergencyContact1TelephoneNumber,
-          emergency_contact_1_alternative_telephone_number:
-            registration.emergencyContact1AlternativeTelephoneNumber,
-          emergency_contact_2_name: registration.emergencyContact2FullName,
-          emergency_contact_2_telephone_number:
-            registration.emergencyContact2TelephoneNumber,
-          emergency_contact_2_alternative_telephone_number:
-            registration.emergencyContact2AlternativeTelephoneNumber,
-          emergency_contact_3_name: registration.emergencyContact3FullName,
-          emergency_contact_3_telephone_number:
-            registration.emergencyContact3TelephoneNumber,
-          emergency_contact_3_alternative_telephone_number:
-            registration.emergencyContact3AlternativeTelephoneNumber,
-        }
-      );
+      return this.gateway.sendEmail(templateId, registration.ownerEmail, {
+        owner_name: registration.ownerFullName,
+        reference: registration.reference,
+        beacon_information: joinStrings([
+          registration.manufacturer,
+          registration.model,
+          registration.hexId,
+        ]),
+        owner_details: joinStrings([
+          registration.ownerFullName,
+          registration.ownerTelephoneNumber,
+          registration.ownerAlternativeTelephoneNumber,
+          registration.ownerEmail,
+        ]),
+        owner_address: joinStrings([
+          registration.ownerAddressLine1,
+          registration.ownerAddressLine2,
+          registration.ownerTownOrCity,
+          registration.ownerPostcode,
+        ]),
+        emergency_contact_1_name: registration.emergencyContact1FullName,
+        emergency_contact_1_telephone_number:
+          registration.emergencyContact1TelephoneNumber,
+        emergency_contact_1_alternative_telephone_number:
+          registration.emergencyContact1AlternativeTelephoneNumber,
+        emergency_contact_2_name: registration.emergencyContact2FullName,
+        emergency_contact_2_telephone_number:
+          registration.emergencyContact2TelephoneNumber,
+        emergency_contact_2_alternative_telephone_number:
+          registration.emergencyContact2AlternativeTelephoneNumber,
+        emergency_contact_3_name: registration.emergencyContact3FullName,
+        emergency_contact_3_telephone_number:
+          registration.emergencyContact3TelephoneNumber,
+        emergency_contact_3_alternative_telephone_number:
+          registration.emergencyContact3AlternativeTelephoneNumber,
+      });
     }
 
     return false;
