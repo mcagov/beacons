675e69cfa4cdd0c5ee5d2c72a65b3fa0ff117cf3
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-09 11:03:55 +0100

    Complete testing of Aviation single-beacon owner path ✅

--- cypress/integration/i-can-see-previously-entered-information.spec.ts
+++ cypress/integration/i-can-see-previously-entered-information.spec.ts
@@ -1,4 +1,4 @@
-import { testAviationPleasureUse, testData } from "./happy-path-test-data.spec";
+import { testData } from "./happy-path-test-data.spec";
 
 export const iCanSeeMyBeaconDetails = (): void =>
   Object.values(testData.beaconDetails).forEach((value) => cy.contains(value));
@@ -7,30 +7,6 @@ export const iCanSeeMyAdditionalBeaconInformation = (): void =>
   Object.values(testData.additionalBeaconInformation).forEach((value) =>
     cy.contains(value)
   );
-
-export const iCanSeeMyAviationPleasureUse = (): void => {
-  Object.values(testAviationPleasureUse.type).forEach((value) => {
-    cy.get("main").contains(value);
-  });
-  Object.values(testAviationPleasureUse.aircraft).forEach((value) => {
-    cy.get("main").contains(value);
-  });
-  cy.get("main").contains(
-    testAviationPleasureUse.communications.satelliteTelephone
-  );
-  cy.get("main").contains(
-    testAviationPleasureUse.communications.mobileTelephone1
-  );
-  cy.get("main").contains(
-    testAviationPleasureUse.communications.mobileTelephone2
-  );
-  cy.get("main").contains(
-    testAviationPleasureUse.communications.otherCommunication
-  );
-  cy.get("main").contains(testAviationPleasureUse.moreDetails);
-  // TODO: Test that user's dongle choice is played back on check-your-answers
-  // cy.get("main").contains("Dongle")
-};
 
 export const iCanSeeMyPersonalDetails = (): void =>
   Object.values(testData.ownerDetails).forEach((value) =>
--- cypress/integration/single-beacon-owner/i-can-register-an-aviation-beacon.spec.ts
+++ cypress/integration/single-beacon-owner/i-can-register-an-aviation-beacon.spec.ts
@@ -1,34 +1,31 @@
+import { Environment, Purpose } from "../../../src/lib/registration/types";
 import { PageURLs } from "../../../src/lib/urls";
-import { testAviationPleasureUse } from "../happy-path-test-data.spec";
 import {
-  iCanEditMyActivity,
+  testAviationCommercialUse,
+  testAviationPleasureUse,
+  testAviationUse,
+} from "../happy-path-test-data.spec";
+import {
   iCanEditMyAdditionalBeaconInformation,
   iCanEditMyAdditionalUseInformation,
   iCanEditMyAddressDetails,
-  iCanEditMyAircraftCommunications,
-  iCanEditMyAircraftDetails,
   iCanEditMyBeaconDetails,
   iCanEditMyEmergencyContactDetails,
-  iCanEditMyEnvironment,
   iCanEditMyPersonalDetails,
-  iCanEditMyPurpose,
 } from "../i-can-edit-previously-entered-information.spec";
 import {
+  iCanSeeMyAdditionalBeaconInformation,
   iCanSeeMyAddressDetails,
-  iCanSeeMyAviationPleasureUse,
   iCanSeeMyBeaconDetails,
   iCanSeeMyEmergencyContactDetails,
   iCanSeeMyPersonalDetails,
 } from "../i-can-see-previously-entered-information.spec";
 import {
   andIClickContinue,
+  givenIAmAt,
   givenIHaveSelected,
-  givenIHaveTyped,
   iAmAt,
   iCanSeeAHeadingThatContains,
-  thenTheCheckboxShouldBeChecked,
-  thenTheInputShouldContain,
-  thenTheRadioButtonShouldBeSelected,
   thenTheUrlShouldContain,
   whenIClickBack,
 } from "../selectors-and-assertions.spec";
@@ -46,7 +43,7 @@ import {
 describe("As an aviation beacon owner,", () => {
   it("I can register my beacon for pleasure purposes", () => {
     givenIHaveEnteredMyBeaconDetails();
-    givenIHaveEnteredMyAviationPleasureUse();
+    givenIHaveEnteredMyAviationUse(Purpose.PLEASURE);
     andIHaveNoFurtherUses();
 
     givenIHaveEnteredMyPersonalDetails();
@@ -55,108 +52,59 @@ describe("As an aviation beacon owner,", () => {
 
     thenTheUrlShouldContain(PageURLs.checkYourAnswers);
     iCanSeeMyBeaconDetails();
-    iCanSeeMyAviationPleasureUse();
+    iCanSeeMyAdditionalBeaconInformation();
+    iCanSeeMyAviationUse(Purpose.PLEASURE);
     iCanSeeMyPersonalDetails();
     iCanSeeMyAddressDetails();
     iCanSeeMyEmergencyContactDetails();
-
-    whenIClickBack();
-    iCanEditMyEmergencyContactDetails();
-    whenIClickBack();
-    iCanEditMyAddressDetails();
-    whenIClickBack();
-    iCanEditMyPersonalDetails();
-    whenIClickBack();
-    iCanEditMyAdditionalUseInformation();
-    whenIClickBack();
-    iCanEditMyAircraftCommunications();
-    whenIClickBack();
-    iCanEditMyAircraftDetails();
-    whenIClickBack();
-    iCanEditMyActivity();
-    whenIClickBack();
-    iCanEditMyPurpose();
-    whenIClickBack();
-    iCanEditMyEnvironment();
-    whenIClickBack();
-    iCanEditMyAdditionalBeaconInformation();
-    whenIClickBack();
-    iCanEditMyBeaconDetails();
-    whenIClickBack();
-    iAmAt(PageURLs.start);
+    iCanGoBackAndEditMyAviationUse(Purpose.PLEASURE);
   });
 
   it("I can register my beacon for commercial purposes", () => {
     givenIHaveEnteredMyBeaconDetails();
+    givenIHaveEnteredMyAviationUse(Purpose.COMMERCIAL);
+    andIHaveNoFurtherUses();
 
-    thenTheUrlShouldContain(PageURLs.environment);
-    givenIHaveSelected("#aviation");
-    andIClickContinue();
+    givenIHaveEnteredMyPersonalDetails();
+    givenIHaveEnteredMyAddressDetails();
+    givenIHaveEnteredMyEmergencyContactDetails();
 
-    thenTheUrlShouldContain(PageURLs.purpose);
-    iCanSeeAHeadingThatContains("aviation");
-    givenIHaveSelected("#commercial");
-    andIClickContinue();
-
-    thenTheUrlShouldContain(PageURLs.activity);
-    iCanSeeAHeadingThatContains("aviation");
-    iCanSeeAHeadingThatContains("commercial");
-    givenIHaveSelected("#passenger-plane");
-    andIClickContinue();
-
-    thenTheUrlShouldContain(PageURLs.aboutTheAircraft);
-    iCanSeeAHeadingThatContains("aircraft");
-    givenIHaveTyped("15", "#maxCapacity");
-    andIClickContinue();
-
-    thenTheUrlShouldContain(PageURLs.aircraftCommunications);
-    givenIHaveSelected("#satelliteTelephone");
-    givenIHaveTyped("+881612345678", "#satelliteTelephoneInput");
-    andIClickContinue();
-
-    thenTheUrlShouldContain(PageURLs.moreDetails);
-    whenIClickBack();
-
-    thenTheUrlShouldContain(PageURLs.aircraftCommunications);
-    thenTheCheckboxShouldBeChecked("#satelliteTelephone");
-    thenTheInputShouldContain("+881612345678", "#satelliteTelephoneInput");
-    whenIClickBack();
-
-    thenTheUrlShouldContain(PageURLs.aboutTheAircraft);
-    iCanSeeAHeadingThatContains("aircraft");
-    thenTheInputShouldContain("15", "#maxCapacity");
-    whenIClickBack();
-
-    thenTheUrlShouldContain(PageURLs.activity);
-    iCanSeeAHeadingThatContains("aviation");
-    iCanSeeAHeadingThatContains("commercial");
-    thenTheRadioButtonShouldBeSelected("#passenger-plane");
-    whenIClickBack();
-
-    thenTheUrlShouldContain(PageURLs.purpose);
-    iCanSeeAHeadingThatContains("aviation");
-    thenTheCheckboxShouldBeChecked("#commercial");
-    whenIClickBack();
-
-    thenTheUrlShouldContain(PageURLs.environment);
-    thenTheRadioButtonShouldBeSelected("#aviation");
+    thenTheUrlShouldContain(PageURLs.checkYourAnswers);
+    iCanSeeMyBeaconDetails();
+    iCanSeeMyAdditionalBeaconInformation();
+    iCanSeeMyAviationUse(Purpose.COMMERCIAL);
+    iCanSeeMyPersonalDetails();
+    iCanSeeMyAddressDetails();
+    iCanSeeMyEmergencyContactDetails();
+    iCanGoBackAndEditMyAviationUse(Purpose.COMMERCIAL);
   });
 });
 
-export const givenIHaveEnteredMyAviationPleasureUse = (): void => {
+export const givenIHaveEnteredMyAviationUse = (purpose: Purpose): void => {
   thenTheUrlShouldContain(PageURLs.environment);
   givenIHaveSelected("#aviation");
   andIClickContinue();
 
   thenTheUrlShouldContain(PageURLs.purpose);
   iCanSeeAHeadingThatContains("aviation");
-  givenIHaveSelected("#pleasure");
+  givenIHaveSelected(`#${purpose.toLowerCase()}`);
   andIClickContinue();
 
   thenTheUrlShouldContain(PageURLs.activity);
   iCanSeeAHeadingThatContains("aviation");
-  iCanSeeAHeadingThatContains("pleasure");
-  givenIHaveSelected("#" + testAviationPleasureUse.type.activity.toLowerCase());
+  iCanSeeAHeadingThatContains(purpose.toLowerCase());
+  switch (purpose) {
+    case Purpose.COMMERCIAL:
+      givenIHaveSelected(
+        "#" + testAviationCommercialUse.type.activity.toLowerCase()
+      );
+      break;
+    case Purpose.PLEASURE:
+      givenIHaveSelected(
+        "#" + testAviationPleasureUse.type.activity.toLowerCase()
+      );
+      break;
+  }
   andIClickContinue();
 
   thenTheUrlShouldContain(PageURLs.aboutTheAircraft);
@@ -171,4 +119,109 @@ export const givenIHaveEnteredMyAviationPleasureUse = (): void => {
   thenTheUrlShouldContain(PageURLs.moreDetails);
   givenIHaveEnteredMoreDetailsAboutMyAircraft();
   andIClickContinue();
+};
+
+export const iCanGoBackAndEditMyAviationUse = (purpose: Purpose): void => {
+  givenIAmAt(PageURLs.checkYourAnswers);
+  whenIClickBack();
+  iCanEditMyEmergencyContactDetails();
+  whenIClickBack();
+  iCanEditMyAddressDetails();
+  whenIClickBack();
+  iCanEditMyPersonalDetails();
+  whenIClickBack();
+  iCanEditMyAdditionalUseInformation();
+  whenIClickBack();
+  iCanEditMyAircraftCommunications();
+  whenIClickBack();
+  iCanEditMyAircraftDetails();
+  whenIClickBack();
+  iCanEditMyActivity();
+  whenIClickBack();
+  iCanEditMyPurpose(purpose);
+  whenIClickBack();
+  iCanEditMyEnvironment(Environment.AVIATION);
+  whenIClickBack();
+  iCanEditMyAdditionalBeaconInformation();
+  whenIClickBack();
+  iCanEditMyBeaconDetails();
+  whenIClickBack();
+  iAmAt(PageURLs.start);
+};
+
+export const iCanEditMyAircraftCommunications = (): void => {
+  const comms = testAviationPleasureUse.communications;
+  comms.checkedFields.forEach((field) =>
+    cy.get(`#${field}`).should("be.checked")
+  );
+  cy.get("#satelliteTelephoneInput").should(
+    "have.value",
+    comms.satelliteTelephone
+  );
+  cy.get("#mobileTelephoneInput1").should("have.value", comms.mobileTelephone1);
+  cy.get("#mobileTelephoneInput2").should("have.value", comms.mobileTelephone2);
+  cy.get("#otherCommunicationInput").contains(comms.otherCommunication);
+};
+
+export const iCanEditMyAircraftDetails = (): void => {
+  const aircraft = testAviationPleasureUse.aircraft;
+  cy.get("#maxCapacity").should("have.value", aircraft.maxCapacity);
+  cy.get("#aircraftManufacturer").should("have.value", aircraft.manufacturer);
+  cy.get("#principalAirport").should("have.value", aircraft.principalAirport);
+  cy.get("#secondaryAirport").should("have.value", aircraft.secondaryAirport);
+  cy.get("#registrationMark").should("have.value", aircraft.registrationMark);
+  cy.get("#hexAddress").should("have.value", aircraft.hexAddress);
+  cy.get("#cnOrMsnNumber").should("have.value", aircraft.cnOrMsnNumber);
+  cy.get("#cnOrMsnNumber").should("have.value", aircraft.cnOrMsnNumber);
+  cy.get("#dongle-yes").should("be.checked");
+  cy.get("#beaconPosition").contains(aircraft.beaconPosition);
+};
+
+export const iCanEditMyActivity = (): void => {
+  cy.get(`input[value="${testAviationPleasureUse.type.activity}"]`).should(
+    "be.checked"
+  );
+};
+
+export const iCanEditMyPurpose = (purpose: Purpose): void => {
+  switch (purpose) {
+    case Purpose.COMMERCIAL:
+      cy.get(`input[value="${testAviationCommercialUse.type.purpose}"]`).should(
+        "be.checked"
+      );
+      break;
+    case Purpose.PLEASURE:
+      cy.get(`input[value="${testAviationPleasureUse.type.purpose}"]`).should(
+        "be.checked"
+      );
+      break;
+  }
+};
+
+export const iCanEditMyEnvironment = (environment: Environment): void => {
+  cy.get(`input[value="${environment}"]`).should("be.checked");
+};
+
+export const iCanSeeMyAviationUse = (purpose: Purpose): void => {
+  switch (purpose) {
+    case Purpose.COMMERCIAL:
+      Object.values(testAviationCommercialUse.type).forEach((value) => {
+        cy.get("main").contains(value);
+      });
+      break;
+    case Purpose.PLEASURE:
+      Object.values(testAviationPleasureUse.type).forEach((value) => {
+        cy.get("main").contains(value);
+      });
+      break;
+  }
+  Object.values(testAviationUse.aircraft).forEach((value) => {
+    cy.get("main").contains(value);
+  });
+  cy.get("main").contains(testAviationUse.communications.satelliteTelephone);
+  cy.get("main").contains(testAviationUse.communications.mobileTelephone1);
+  cy.get("main").contains(testAviationUse.communications.mobileTelephone2);
+  cy.get("main").contains(testAviationUse.communications.otherCommunication);
+  cy.get("main").contains(testAviationUse.moreDetails);
+  cy.get("main").contains("dongle");
 };
