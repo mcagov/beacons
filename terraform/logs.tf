6206198d78371993d47413519d2d4b986ed673ae
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-04-20 12:24:11 +0100

    Remove comment

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -2,8 +2,4 @@ resource "aws_cloudwatch_log_group" "log_group" {
   name              = "/${var.env}/mca/beacons"
   tags = module.beacons_label.tags
   retention_in_days = 30
-
-//  tags = {
-//    Name = "${var.env}-mca-log-group"
-//  }
 }
