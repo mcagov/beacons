7b6c8fefa36e9f2e247660fdb41fc49039f70cd2
Matthew Carr <matt.carr@madetech.com>
2021-04-22 13:11:37 +0100

    Add port for service

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -24,5 +24,5 @@ provider "aws" {
 
 locals {
   dns_sub_domain         = "${terraform.workspace}.beacons.local"
-  service_local_endpoint = "service.${local.dns_sub_domain}/spring-api"
+  service_local_endpoint = "service.${local.dns_sub_domain}:${var.service_port}/spring-api"
 }
