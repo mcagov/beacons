7a172618e461e274a80b08429226bcbee2fbb2aa
Ninamma Rai <ninamma.rai@outlook.com>
2021-05-25 16:16:36 +0100

    Add basic sign up or sign in page that matched wireframe

---
+++ src/pages/account/sign-up-or-sign-in.tsx
@@ -0,0 +1,86 @@
+import { GetServerSideProps } from "next";
+import React, { FunctionComponent } from "react";
+import { BeaconsForm } from "../../components/BeaconsForm";
+import { FormGroup } from "../../components/Form";
+import { RadioList, RadioListItem } from "../../components/RadioList";
+import { GovUKBody } from "../../components/Typography";
+import { FieldManager } from "../../lib/form/fieldManager";
+import { FormManager } from "../../lib/form/formManager";
+import { Validators } from "../../lib/form/validators";
+import {
+  DestinationIfValidCallback,
+  FormPageProps,
+  handlePageRequest,
+} from "../../lib/handlePageRequest";
+import { PageURLs } from "../../lib/urls";
+
+const getPageForm = ({ signUpOrSignIn }) => {
+  return new FormManager({
+    signUpOrSignIn: new FieldManager(signUpOrSignIn, [
+      Validators.required("Please select an option"),
+    ]),
+  });
+};
+
+export const SignUpOrSignIn: FunctionComponent<FormPageProps> = ({
+  form,
+  showCookieBanner,
+}: FormPageProps): JSX.Element => {
+  const pageHeading = "Do you have a Beacon Registry Account?";
+  const pageText = (
+    <GovUKBody>
+      {"You will need an account to register your beacon online."}
+    </GovUKBody>
+  );
+
+  const fieldName = "signUpOrSignIn";
+
+  return (
+    <BeaconsForm
+      formErrors={form.errorSummary}
+      previousPageUrl={PageURLs.start}
+      pageHeading={pageHeading}
+      showCookieBanner={showCookieBanner}
+      pageText={pageText}
+    >
+      <FormGroup errorMessages={form.fields.signUpOrSignIn.errorMessages}>
+        <RadioList conditional={true}>
+          <RadioListItem
+            id="signIn"
+            name={fieldName}
+            label="Sign in using my Beacon Registry Account"
+            hintText="You’ll have an account if you’ve registered a beacon before. Your log in details will be an email address and password"
+            value="signIn"
+            defaultChecked={form.fields.signUpOrSignIn.value === "signIn"}
+          />
+          <RadioListItem
+            id="signUp"
+            name={fieldName}
+            label="Create a Beacon Registry Account"
+            hintText="You must create and account before you can register your first beacon"
+            value="signUp"
+            defaultChecked={form.fields.signUpOrSignIn.value === "signUp"}
+          />
+        </RadioList>
+      </FormGroup>
+    </BeaconsForm>
+  );
+};
+
+const onSuccessfulFormCallback: DestinationIfValidCallback = (context) => {
+  switch (context.formData.signUpOrSignIn) {
+    case "signUp":
+      return PageURLs.signUp;
+    case "signIn":
+      return PageURLs.signIn;
+  }
+};
+
+export const getServerSideProps: GetServerSideProps = handlePageRequest(
+  "",
+  getPageForm,
+  (f) => f,
+  onSuccessfulFormCallback
+);
+
+export default SignUpOrSignIn;
