4f9465d2e063e59f8116d2a33c368c92ada74cfc
Ninamma Rai <ninamma.rai@outlook.com>
2021-05-28 11:14:31 +0100

    Test out next login page for b2c

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent } from "react";
+import { Grid } from "../../components/Grid";
+import { Layout } from "../../components/Layout";
+import { IfYouNeedHelp } from "../../components/Mca";
+import { PageHeading } from "../../components/Typography";
+
+const LoginPage: FunctionComponent = (): JSX.Element => {
+  const pageHeading = "Sign In using my Beacon Registry Account";
+
+  return (
+    <Layout title={pageHeading} showCookieBanner={false}>
+      <Grid mainContent={<PageHeading>{pageHeading}</PageHeading>} />
+      <div id="api"></div>
+      <IfYouNeedHelp />
+    </Layout>
+  );
+};
+
+export default LoginPage;
