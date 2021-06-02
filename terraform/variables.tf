4d3699e7e7047b93767128345bec796569296f10
Matthew Carr <matt.carr@madetech.com>
2021-06-02 17:01:51 +0100

    Add variables for next auth

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -139,26 +139,52 @@ variable "db_skip_final_snapshot" {
   type        = bool
   description = "Determines whether a final DB snapshot is created before the DB instance is deleted"
 }
-
 variable "webapp_client_secret" {
   sensitive   = true
   type        = string
   description = "The client secret for the consumer-facing web app, provided in Azure AD"
 }
-
 variable "webapp_client_id" {
   type        = string
   description = "The client identifier for the consumer-facing web app, provided in Azure AD"
 }
-
 variable "service_api_id" {
   sensitive   = true
   type        = string
   description = "The GUID for the Service API, provided in Azure AD"
 }
-
 variable "mca_azure_ad_tenant_id" {
   sensitive   = true
   type        = string
   description = "The UUID for the Azure AD tenant, provided in Azure AD"
+}
+variable "next_auth_url" {
+  type = string
+  description = "The NEXT AUTH environment variable used by NextAuth"
+}
+variable "mca_azure_b2c_client_id" {
+  type = string
+  description = "The Azure B2C Client ID for the B2C App Registration"
+}
+variable "mca_azure_b2c_client_secret" {
+  type = string
+  sensitive = true
+  description = "The client secret for the B2C App Registration"
+}
+variable "mca_azure_b2c_tenant_name" {
+  type = string
+  description = "The name of the Azure B2C tenant"
+} 
+variable "mca_azure_b2c_tenant_id" {
+  type = string
+  description = "The UUID for the Azure B2C tenant"
+}
+variable "mca_azure_b2c_login_flow" {
+  type = string
+  description = "The Sign In User Flow defined in Azure B2C"
+}
+variable "mca_azure_b2c_auth_jwt_secret" {
+  type = string
+  sensitive = true
+  descriptoin = "The random string used to hash tokens, sign cookies, and generate crytographic keys for Next Auth"
 }
