d36473cd42c14bd5905fed85a48de06817d2bab4
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-06-02 14:19:30 +0100

    Clear registration reference number on failure

--- src/pages/register-a-beacon/application-complete.tsx
+++ src/pages/register-a-beacon/application-complete.tsx
@@ -112,10 +112,10 @@ export const getServerSideProps: GetServerSideProps = withCookieRedirect(
             pageSubHeading =
               "We could not send you a confirmation email. But we have registered your beacon under the following reference id.";
           }
+        } else {
+          registration.referenceNumber = "";
         }
       } catch (e) {
-        // eslint-disable no-console
-        console.error(e);
         registration.referenceNumber = "";
         pageSubHeading =
           "We could not save your registration or send you a confirmation email. Please contact the Beacons Registry team.";
