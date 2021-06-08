541fdf56739303398fc438b537c304e0faa246b9
Matthew Carr <matt.carr@madetech.com>
2021-06-08 14:58:28 +0100

    Add initial app container interface for use cases

--- src/lib/appContainer.ts
+++ src/lib/appContainer.ts
@@ -1,1 +1,23 @@
-export class AppContainer {}
+import { IAuthenticateUser } from "../useCases/authenticateUser";
+import { ICreateRegistration } from "../useCases/createRegistration";
+import { ISendGovNotifyEmail } from "../useCases/sendGovNotifyEmail";
+
+export interface IAppContainer {
+  getAuthenticateUser: () => IAuthenticateUser;
+  getCreateRegistration: () => ICreateRegistration;
+  getSendGovNotifyEmail: () => ISendGovNotifyEmail;
+}
+
+export class AppContainer implements IAppContainer {
+  public getAuthenticateUser(): IAuthenticateUser {
+    return null;
+  }
+
+  public getCreateRegistration(): ICreateRegistration {
+    return null;
+  }
+
+  public getSendGovNotifyEmail(): ISendGovNotifyEmail {
+    return null;
+  }
+}
