a8b3d57f4e45781a06ea21b10a6f7172efcf3adb
Tom Maxwell <tom.maxwell@madetech.com>
2021-06-08 14:51:36 +0100

    WIP superlong wait, last chance for the b2c page :/

--- cypress/integration/common/selectors-and-assertions.spec.ts
+++ cypress/integration/common/selectors-and-assertions.spec.ts
@@ -124,7 +124,7 @@ export const givenIHaveUnselected = (selector: string): void => {
 };
 
 export const givenIHaveWaitedForAzureB2C = (): void => {
-  cy.wait(10000);
+  cy.wait(60000);
 };
 
 export const andIHaveEnteredNoInformation = (): void => null;
