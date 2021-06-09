0d2693e0780985628bad6f80d1c7c58dab76e9fa
Tom Maxwell <tom.maxwell@madetech.com>
2021-06-09 14:09:22 +0100

    add account holder gateway and single test

---
+++ src/lib/accountHolder/accountHolderIdResponseBody.ts
@@ -0,0 +1,3 @@
+export interface IAccountHolderIdResponseBody {
+  id: string;
+}
---
+++ test/gateways/accountHolderApiGateway.test.ts
@@ -0,0 +1,45 @@
+import axios, { AxiosResponse } from "axios";
+import { v4 } from "uuid";
+import { AccountHolderApiGateway } from "../../src/gateways/accountHolderApiGateway";
+import { IAccountHolderIdResponseBody } from "../../src/lib/accountHolder/accountHolderIdResponseBody";
+
+jest.mock("axios");
+
+describe("Account Holder API Gateway", () => {
+  let gateway: AccountHolderApiGateway;
+  const endpoint = "account-holder/auth-id";
+
+  beforeEach(() => {
+    gateway = new AccountHolderApiGateway();
+  });
+
+  describe("Getting an account holder", () => {
+    let authId;
+    let acountHolderId;
+    let token;
+
+    beforeEach(() => {
+      authId = v4();
+      acountHolderId = v4();
+      token = v4();
+    });
+
+    it("should return account holder id", async () => {
+      const expectedUrl = `${process.env.API_URL}/${endpoint}/${authId}`;
+      (axios as any).get.mockImplementation((url: string) => {
+        return {
+          data: {
+            id: acountHolderId,
+          },
+        } as AxiosResponse<IAccountHolderIdResponseBody>;
+      });
+
+      const expected = await gateway.getAccountHolderId(authId, token);
+
+      expect(expected).toBe(acountHolderId);
+      expect((axios as any).get).toHaveBeenLastCalledWith(expectedUrl, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+    });
+  });
+});
