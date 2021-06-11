1a7907fbdd8d3841601738901a5ecbc65e81392e
Ninamma Rai <ninamma.rai@outlook.com>
2021-06-11 10:43:44 +0100

    Removing test that's covered by typechecking

--- test/lib/container.test.ts
+++ test/lib/container.test.ts
@@ -1,18 +1,6 @@
-import { AppContainer } from "../../src/lib/appContainer";
 import { withContainer } from "../../src/lib/container";
 
 describe("Application Container callback", () => {
-  it("should return a new instance of the application container if none is provided", () => {
-    const providedContext: any = {};
-    const callback = jest.fn();
-    const withContainerFunction = withContainer(callback);
-
-    withContainerFunction(providedContext);
-
-    expect(providedContext.container).toBeDefined();
-    expect(providedContext.container).toBeInstanceOf(AppContainer);
-  });
-
   it("should not modify the container if one is already provided to the function", () => {
     const providedContext: any = { container: "hex id" };
     const callback = jest.fn();
