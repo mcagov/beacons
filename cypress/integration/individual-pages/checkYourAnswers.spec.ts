34f0f322910eec7f30be108d69d92f9c3cc8bf1a
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-06-14 13:18:38 +0100

    Update check-your-answers spec to retain cached form values on failed registration

--- cypress/integration/individual-pages/checkYourAnswers.spec.ts
+++ cypress/integration/individual-pages/checkYourAnswers.spec.ts
@@ -1,37 +1,35 @@
 import { PageURLs } from "../../../src/lib/urls";
-import { givenIHaveFilledInCheckBeaconDetailsPage } from "../common/i-can-enter-beacon-information.spec";
+import {
+  givenIHaveFilledInCheckBeaconDetailsPage,
+  iCanEditMyBeaconDetails,
+} from "../common/i-can-enter-beacon-information.spec";
 import {
   andIAmAt,
   givenIHaveACookieSetAndIVisit,
   givenIHaveClicked,
   iCanSeeAPageHeadingThatContains,
-  thenTheInputShouldOnlyContain,
 } from "../common/selectors-and-assertions.spec";
 
 describe("As a beacon owner, I want to check the details that were submitted", () => {
   const acceptAndSendButtonSelector = ".govuk-button--start";
   const homePageLinkSelector = ".govuk-header__link--homepage";
   const startButtonSelector = ".govuk-button--start";
-  const manufacturerFieldSelector = "#manufacturer";
-  const modelFieldSelector = "#model";
-  const hexIdFieldSelector = "#hexId";
 
   it("should display the page title", () => {
     givenIHaveACookieSetAndIVisit(PageURLs.checkYourAnswers);
     iCanSeeAPageHeadingThatContains("Check your answers");
   });
 
-  it("should clear the form when I click Accept and Send", () => {
+  it("should not clear the form when I click Accept and Send and the registration fails", () => {
     givenIHaveACookieSetAndIVisit(PageURLs.beaconInformation);
     givenIHaveFilledInCheckBeaconDetailsPage();
     andIAmAt(PageURLs.checkYourAnswers);
     givenIHaveClicked(acceptAndSendButtonSelector);
     andIAmAt(PageURLs.applicationComplete);
+    cy.get("div").contains("There was an error while registering your beacon");
     givenIHaveClicked(homePageLinkSelector);
     andIAmAt(PageURLs.start);
     givenIHaveClicked(startButtonSelector);
-    thenTheInputShouldOnlyContain("", manufacturerFieldSelector);
-    thenTheInputShouldOnlyContain("", modelFieldSelector);
-    thenTheInputShouldOnlyContain("", hexIdFieldSelector);
+    iCanEditMyBeaconDetails();
   });
 });
