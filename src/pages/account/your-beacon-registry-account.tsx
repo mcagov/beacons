05a98fc2a585af883a976039c18874922e9bf7f0
Tom Maxwell <tom.maxwell@madetech.com>
2021-06-15 09:55:32 +0100

    registry account placeholder page

--- src/pages/account/your-beacon-registry-account.tsx
+++ src/pages/account/your-beacon-registry-account.tsx
@@ -2,32 +2,56 @@ import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import React, { FunctionComponent } from "react";
 import { Grid } from "../../components/Grid";
 import { Layout } from "../../components/Layout";
-// import { BeaconsForm } from "../../components/BeaconsForm";
-// import { FormGroup } from "../../components/Form";
-// import { RadioList, RadioListItem } from "../../components/RadioList";
-// import { GovUKBody } from "../../components/Typography";
-// import { FieldManager } from "../../lib/form/fieldManager";
-// import { FormManager } from "../../lib/form/formManager";
-// import { Validators } from "../../lib/form/validators";
-import {
-  // DestinationIfValidCallback,
-  FormPageProps,
-} from "../../lib/handlePageRequest";
+import { BeaconRegistryContactInfo } from "../../components/Mca";
+import { PageHeading, SectionHeading } from "../../components/Typography";
 
 interface YourBeaconRegistyAccountPageProps {}
 
 export const YourBeaconRegistyAccount: FunctionComponent<YourBeaconRegistyAccountPageProps> =
-  ({}: // form,
-  // showCookieBanner,
-  FormPageProps): JSX.Element => {
+  ({}: YourBeaconRegistyAccountPageProps): JSX.Element => {
     const pageHeading = "Your Beacon Registy Account";
 
     return (
       <Layout title={pageHeading} showCookieBanner={false}>
-        <Grid mainContent={<></>} />
+        <Grid
+          mainContent={
+            <>
+              <PageHeading>{pageHeading}</PageHeading>
+              <YourDetails></YourDetails>
+              <YourBeacons></YourBeacons>
+              <RegisterANewBeacon></RegisterANewBeacon>
+              <Contact></Contact>
+            </>
+          }
+        />
       </Layout>
     );
   };
+
+const YourDetails: FunctionComponent = (): JSX.Element => (
+  <>
+    <SectionHeading>Your details</SectionHeading>
+  </>
+);
+
+const YourBeacons: FunctionComponent = (): JSX.Element => (
+  <>
+    <SectionHeading>You have x registered beacons</SectionHeading>
+  </>
+);
+
+const RegisterANewBeacon: FunctionComponent = (): JSX.Element => (
+  <>
+    <SectionHeading>Register a new beacon</SectionHeading>
+  </>
+);
+
+const Contact: FunctionComponent = (): JSX.Element => (
+  <>
+    <SectionHeading>Contact the UK Beacon Registry</SectionHeading>
+    <BeaconRegistryContactInfo />
+  </>
+);
 
 export const getServerSideProps: GetServerSideProps = async (
   context: GetServerSidePropsContext
