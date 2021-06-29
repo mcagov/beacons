abf09845c3bc6105f2d2424273f40423347a9afe
Isabel Jenkins <isabel.jenkins@madetech.com>
2021-06-29 16:59:31 +0100

    Got full beacon details to render on additional-beacon-use page

--- src/pages/register-a-beacon/additional-beacon-use.tsx
+++ src/pages/register-a-beacon/additional-beacon-use.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import React, { FunctionComponent } from "react";
 import { BeaconsForm } from "../../components/BeaconsForm";
 import { SummaryList, SummaryListItem } from "../../components/SummaryList";
+import { SectionHeading } from "../../components/Typography";
 import { FieldManager } from "../../lib/form/fieldManager";
 import { FormManager } from "../../lib/form/formManager";
 import { FormCacheFactory, FormSubmission } from "../../lib/formCache";
@@ -15,8 +16,18 @@ import {
   decorateGetServerSidePropsContext,
   setFormCache,
 } from "../../lib/middleware";
-import { AdditionalUses, BeaconUse } from "../../lib/registration/types";
-import { formatUrlQueryParams } from "../../lib/utils";
+import {
+  AdditionalUses,
+  BeaconUse,
+  Environment,
+} from "../../lib/registration/types";
+import { PageURLs } from "../../lib/urls";
+import {
+  formatUrlQueryParams,
+  makeEnumValueUserFriendly,
+  sentenceCase,
+  useRankString,
+} from "../../lib/utils";
 
 const definePageForm = ({
   additionalBeaconUse,
@@ -37,6 +48,13 @@ const AdditionalBeaconUse: FunctionComponent<FormPageProps> = ({
   const pageHeading = "Summary of how you use this beacon";
   const additionalBeaconName = "additionalBeaconUse";
 
+  const useSections = [];
+  for (const [index, use] of registration.uses.entries()) {
+    useSections.push(
+      <BeaconUseSection index={index} use={use} key={`row${index}`} />
+    );
+  }
+
   return (
     <BeaconsForm
       pageHeading={pageHeading}
@@ -45,7 +63,7 @@ const AdditionalBeaconUse: FunctionComponent<FormPageProps> = ({
       showCookieBanner={showCookieBanner}
       errorMessages={form.fields.additionalBeaconUse.errorMessages}
     >
-      <BeaconUseRow beaconUse={registration.uses[0]} />
+      {useSections}
 
       <button
         role="button"
@@ -82,27 +100,284 @@ const onSuccessfulFormCallback: DestinationIfValidCallback = async (
   }
 };
 
-interface IBeaconUseProps {
-  beaconUse: BeaconUse;
+interface BeaconUseSectionProps {
+  index: number;
+  use: BeaconUse;
+  href?: string;
 }
 
-const BeaconUseRow: FunctionComponent<IBeaconUseProps> = ({
-  beaconUse: { maxCapacity, otherCommunicationInput, moreDetails },
-}: IBeaconUseProps): JSX.Element => (
-  <>
-    <SummaryList>
-      <SummaryListItem labelText="Additional beacon information">
-        <p>{maxCapacity}</p>
-      </SummaryListItem>
-      <SummaryListItem labelText="Communications">
-        <p>{otherCommunicationInput}</p>
-      </SummaryListItem>
-      <SummaryListItem labelText="More details">
-        <p>{moreDetails}</p>
-      </SummaryListItem>
-    </SummaryList>
-  </>
-);
+const BeaconUseSection: FunctionComponent<BeaconUseSectionProps> = ({
+  index,
+  use,
+}: BeaconUseSectionProps): JSX.Element => {
+  const href = `${PageURLs.environment}?useIndex=${index}`;
+  let aboutTheSection = <></>;
+  let commsSection = <></>;
+  switch (use.environment) {
+    case Environment.MARITIME:
+      aboutTheSection = <AboutTheVesselSubSection index={index} use={use} />;
+      commsSection = (
+        <CommunicationsSubSection
+          index={index}
+          use={use}
+          href={PageURLs.vesselCommunications}
+        />
+      );
+      break;
+    case Environment.AVIATION:
+      aboutTheSection = <AboutTheAircraftSubSection index={index} use={use} />;
+      commsSection = (
+        <CommunicationsSubSection
+          index={index}
+          use={use}
+          href={PageURLs.aircraftCommunications}
+        />
+      );
+      break;
+    case Environment.LAND:
+      commsSection = (
+        <CommunicationsSubSection
+          index={index}
+          use={use}
+          href={PageURLs.landCommunications}
+        />
+      );
+      break;
+  }
+  return (
+    <>
+      <SectionHeading>
+        {sentenceCase(useRankString(index + 1))} use:{" "}
+        {makeEnumValueUserFriendly(use.activity)} (
+        {makeEnumValueUserFriendly(use.purpose)})
+      </SectionHeading>
+
+      {aboutTheSection}
+      {commsSection}
+      <MoreDetailsSubSection index={index} use={use} />
+    </>
+  );
+};
+
+const AboutTheVesselSubSection: FunctionComponent<BeaconUseSectionProps> = ({
+  index,
+  use,
+}: BeaconUseSectionProps): JSX.Element => {
+  const href = `${PageURLs.aboutTheVessel}?useIndex=${index}`;
+  return (
+    <>
+      <SummaryList>
+        <SummaryListItem labelText="About the vessel, rig or windfarm">
+          {use.vesselName && (
+            <BeaconUseDataRowItem label="Name" value={use.vesselName} />
+          )}
+          <BeaconUseDataRowItem
+            label="Max persons onboard"
+            value={use.maxCapacity}
+          />
+          {use.beaconLocation && (
+            <BeaconUseDataRowItem
+              label="Beacon position"
+              value={use.beaconLocation}
+            />
+          )}
+          {use.homeport && (
+            <BeaconUseDataRowItem label="Homeport" value={use.homeport} />
+          )}
+          {use.areaOfOperation && (
+            <BeaconUseDataRowItem
+              label="Area of operation"
+              value={use.areaOfOperation}
+            />
+          )}
+          {use.portLetterNumber && (
+            <BeaconUseDataRowItem
+              label="Port letter number"
+              value={use.portLetterNumber}
+            />
+          )}
+          {use.imoNumber && (
+            <BeaconUseDataRowItem label="IMO number" value={use.imoNumber} />
+          )}
+          {use.ssrNumber && (
+            <BeaconUseDataRowItem
+              label="Small Ships Register number"
+              value={use.ssrNumber}
+            />
+          )}
+          {use.rssNumber && (
+            <BeaconUseDataRowItem
+              label=" Registry of Shipping and Seamen (RSS) number"
+              value={use.rssNumber}
+            />
+          )}
+          {use.officialNumber && (
+            <BeaconUseDataRowItem
+              label="Official number"
+              value={use.officialNumber}
+            />
+          )}
+          {use.rigPlatformLocation && (
+            <BeaconUseDataRowItem
+              label="Windfarm, rig or platform location"
+              value={use.rigPlatformLocation}
+            />
+          )}
+        </SummaryListItem>
+      </SummaryList>
+    </>
+  );
+};
+
+const AboutTheAircraftSubSection: FunctionComponent<BeaconUseSectionProps> = ({
+  index,
+  use,
+}: BeaconUseSectionProps): JSX.Element => {
+  const href = `${PageURLs.aboutTheAircraft}?useIndex=${index}`;
+
+  return (
+    <>
+      <SummaryList>
+        <SummaryListItem labelText="About the aircraft">
+          <BeaconUseDataRowItem
+            label="Max persons onboard"
+            value={use.maxCapacity}
+          />
+          <BeaconUseDataRowItem
+            label="Manufacture and model: "
+            value={use.aircraftManufacturer}
+          />
+          <BeaconUseDataRowItem
+            label="Principal airport"
+            value={use.principalAirport}
+          />
+          <BeaconUseDataRowItem
+            label="Secondary airport"
+            value={use.secondaryAirport}
+          />
+          <BeaconUseDataRowItem
+            label="Registration mark"
+            value={use.registrationMark}
+          />
+          <BeaconUseDataRowItem label="24-bit HEX" value={use.hexAddress} />
+          <BeaconUseDataRowItem
+            label="CORE/Serial number"
+            value={use.cnOrMsnNumber}
+          />
+          {use.dongle && (
+            <BeaconUseDataRowItem label="Is this a dongle?" value="Yes" />
+          )}
+          <BeaconUseDataRowItem
+            label="Beacon position"
+            value={use.beaconPosition}
+          />
+        </SummaryListItem>
+      </SummaryList>
+    </>
+  );
+};
+
+const CommunicationsSubSection: FunctionComponent<BeaconUseSectionProps> = ({
+  index,
+  use,
+  href,
+}: BeaconUseSectionProps): JSX.Element => {
+  href = `${href}?useIndex=${index}`;
+
+  return (
+    <>
+      <SummaryList>
+        <SummaryListItem labelText="Communications">
+          {use.environment === Environment.MARITIME && (
+            <BeaconUseDataRowItem label="Callsign" value={use.callSign} />
+          )}
+          {use.fixedVhfRadio.includes("true") && (
+            <>
+              Fixed VHF/DSC
+              <br />
+            </>
+          )}
+          {use.fixedVhfRadio.includes("true") && use.fixedVhfRadioInput && (
+            <BeaconUseDataRowItem label="MMSI" value={use.fixedVhfRadioInput} />
+          )}
+          {use.vhfRadio.includes("true") && (
+            <>
+              VHF Radio
+              <br />
+            </>
+          )}
+          {use.portableVhfRadio.includes("true") && (
+            <>
+              Portable VHF/DSC
+              <br />
+            </>
+          )}
+          {use.portableVhfRadio.includes("true") &&
+            use.portableVhfRadioInput && (
+              <BeaconUseDataRowItem
+                label="Portable MMSI"
+                value={use.portableVhfRadioInput}
+              />
+            )}
+          {use.mobileTelephone.includes("true") && (
+            <BeaconUseDataRowItem
+              label="Mobile telephone (1)"
+              value={use.mobileTelephoneInput1}
+            />
+          )}
+          {use.mobileTelephone.includes("true") && (
+            <BeaconUseDataRowItem
+              label="Mobile telephone (2)"
+              value={use.mobileTelephoneInput2}
+            />
+          )}
+          {use.satelliteTelephone.includes("true") && (
+            <BeaconUseDataRowItem
+              label="Satellite telephone"
+              value={use.satelliteTelephoneInput}
+            />
+          )}
+          {use.otherCommunication.includes("true") && (
+            <BeaconUseDataRowItem
+              label="Other communication"
+              value={use.otherCommunicationInput}
+            />
+          )}
+        </SummaryListItem>
+      </SummaryList>
+    </>
+  );
+};
+
+const MoreDetailsSubSection: FunctionComponent<BeaconUseSectionProps> = ({
+  index,
+  use,
+}: BeaconUseSectionProps): JSX.Element => {
+  const href = `${PageURLs.moreDetails}?useIndex=${index}`;
+  return (
+    <>
+      <SummaryList>
+        <SummaryListItem labelText="More details">
+          <BeaconUseDataRowItem value={use.moreDetails} />
+        </SummaryListItem>
+      </SummaryList>
+    </>
+  );
+};
+
+interface CheckYourAnswersDataRowItemProps {
+  label?: string;
+  value?: string;
+}
+
+const BeaconUseDataRowItem: FunctionComponent<CheckYourAnswersDataRowItemProps> =
+  ({ label, value }: CheckYourAnswersDataRowItemProps): JSX.Element => (
+    <>
+      {label ? label + ": " : ""}
+      {value ? value : ""}
+      <br />
+    </>
+  );
 
 export const getServerSideProps: GetServerSideProps = handlePageRequest(
   "",
