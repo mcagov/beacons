514ecf82fc4d23ed4e2d6fac87f7443801cbdd7a
Ninamma Rai <ninamma.rai@outlook.com>
2021-07-05 16:44:59 +0100

    Replace Cypress test, that fails locally and weirdly passes in CI, with Jest one

--- test/pages/register-a-beacon/about-the-aircraft.test.tsx
+++ test/pages/register-a-beacon/about-the-aircraft.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 import { FormJSON } from "../../../src/lib/form/formManager";
 import AboutTheAircraft from "../../../src/pages/register-a-beacon/about-the-aircraft";
@@ -47,7 +47,12 @@ describe("AboutTheAircraft", () => {
     },
   };
 
-  it("renders the page", () => {
+  it("should have a back button which directs the user to the about the vessel page", () => {
     render(<AboutTheAircraft form={aboutTheAircraftForm} />);
+
+    expect(screen.getByText("Back", { exact: true })).toHaveAttribute(
+      "href",
+      "/register-a-beacon/activity?useIndex=0"
+    );
   });
 });
