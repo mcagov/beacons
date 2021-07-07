e448fefd414b9ea18da814ae29c889dc9b04c126
Ninamma Rai <ninamma.rai@outlook.com>
2021-07-07 11:11:27 +0100

    Increase subnet delete timeouts - needs more than 20m

--- terraform/network.tf
+++ terraform/network.tf
@@ -20,6 +20,9 @@ resource "aws_subnet" "public" {
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
+  timeouts {
+    delete = "45m"
+  }
 }
 
 resource "aws_subnet" "app" {
