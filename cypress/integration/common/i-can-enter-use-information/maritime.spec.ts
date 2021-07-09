74f81da6e4ccb0d5af56fa235aacaffe748c443a
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-07-09 12:43:06 +0100

    Maritime single beacon owner Cypress tests cover new design ✅

--- cypress/integration/common/i-can-enter-use-information/maritime.spec.ts
+++ cypress/integration/common/i-can-enter-use-information/maritime.spec.ts
@@ -1,8 +1,4 @@
-import {
-  AdditionalUses,
-  Environment,
-  Purpose,
-} from "../../../../src/lib/registration/types";
+import { Environment, Purpose } from "../../../../src/lib/registration/types";
 import { PageURLs } from "../../../../src/lib/urls";
 import { makeEnumValueUserFriendly } from "../../../../src/lib/utils";
 import {
@@ -31,7 +27,7 @@ import {
   whenIClearTheInput,
   whenIClickBack,
 } from "../selectors-and-assertions.spec";
-import { iCanEditMyEnvironment, iCanEditMyUses } from "./generic.spec";
+import { iCanEditMyEnvironment, iCanEditMyNUses } from "./generic.spec";
 
 export const givenIHaveEnteredMyMaritimeUse = (purpose: Purpose): void => {
   thenTheUrlShouldContain(PageURLs.environment);
@@ -164,7 +160,7 @@ export const iCanGoBackAndEditMyMaritimeUse = (purpose: Purpose): void => {
   whenIClickBack();
   iCanEditMyPersonalDetails();
   whenIClickBack();
-  iCanEditMyUses(AdditionalUses.NO);
+  iCanEditMyNUses(1);
   whenIClickBack();
   iCanEditMyAdditionalMaritimeUseInformation();
   whenIClickBack();
