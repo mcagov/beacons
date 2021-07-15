44d8eb577af08a441ca84e5131b9f62b5d3c7b62
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-07-15 14:29:41 +0100

    whenIClickBack() iAmEditingWhatIsNowMySecondUse() ✅

--- cypress/integration/single-beacon-owner/i-can-remove-a-use.spec.ts
+++ cypress/integration/single-beacon-owner/i-can-remove-a-use.spec.ts
@@ -1,75 +1,93 @@
 import { Purpose } from "../../../src/lib/registration/types";
 import { givenIHaveEnteredMyBeaconDetails } from "../common/i-can-enter-beacon-information.spec";
+import {
+  givenIHaveEnteredMyAviationUse,
+  iCanSeeMyAviationUse,
+} from "../common/i-can-enter-use-information/aviation.spec";
 import { andIHaveAnotherUse } from "../common/i-can-enter-use-information/generic.spec";
 import {
   givenIHaveEnteredMyLandUse,
+  iCanEditMyAdditionalLandUseMoreDetails,
   iCanSeeMyLandUse,
 } from "../common/i-can-enter-use-information/land.spec";
 import {
   givenIHaveEnteredMyMaritimeUse,
   iCanSeeMyMaritimeUse,
 } from "../common/i-can-enter-use-information/maritime.spec";
-import { givenIHaveClickedTheButtonContaining } from "../common/selectors-and-assertions.spec";
+import {
+  givenIHaveClickedTheButtonContaining,
+  whenIClickBack,
+} from "../common/selectors-and-assertions.spec";
 
 describe("As a beacon owner with several uses", () => {
   it("I can safely remove a use from my draft registration", () => {
-    givenIHaveTwoUses();
+    givenIHaveThreeUses();
     andIGoToDeleteMyMainUse();
     iAmPromptedToConfirmDeletionOfMyMainUse();
 
     givenIHaveClickedTheButtonContaining("Cancel");
-    iCanSeeMyTwoUses();
+    iCanSeeMyThreeUses();
     andIGoToDeleteMySecondUse();
     iAmPromptedToConfirmDeletionOfMySecondUse();
 
     givenIHaveClickedTheButtonContaining("Yes");
     iCanSeeMyMainUse();
-    iCannotSeeMySecondUseBecauseItIsDeleted();
+    iCannotSeeWhatWasMySecondUseBecauseItIsDeleted();
+    myThirdUseIsNowMySecondUse();
+
+    whenIClickBack();
+    iAmEditingWhatIsNowMySecondUse();
   });
 });
 
 const iCanSeeMyMainUse = () => iCanSeeMyLandUse();
 
-const givenIHaveTwoUses = () => {
+const myThirdUseIsNowMySecondUse = () =>
+  cy.get("h2").contains(/third use/i && /aviation/i && /commercial/i);
+
+const givenIHaveThreeUses = () => {
   givenIHaveEnteredMyBeaconDetails();
   givenIHaveEnteredMyLandUse();
   andIHaveAnotherUse();
   givenIHaveEnteredMyMaritimeUse(Purpose.PLEASURE);
+  andIHaveAnotherUse();
+  givenIHaveEnteredMyAviationUse(Purpose.COMMERCIAL);
 };
 
-const iCanSeeMyTwoUses = () => {
+const iCanSeeMyThreeUses = () => {
   iCanSeeMyLandUse();
   iCanSeeMyMaritimeUse(Purpose.PLEASURE);
+  iCanSeeMyAviationUse(Purpose.COMMERCIAL);
 };
 
-const iCannotSeeMySecondUseBecauseItIsDeleted = () => {
-  cy.get("main")
+const iCannotSeeWhatWasMySecondUseBecauseItIsDeleted = () =>
+  cy
+    .get("main")
     .contains(/maritime/i && /motor/i && /pleasure/i)
     .should("not.exist");
-};
 
-const andIGoToDeleteMyMainUse = () => {
-  cy.get("h2")
+const andIGoToDeleteMyMainUse = () =>
+  cy
+    .get("h2")
     .contains(/main use/i)
     .siblings()
     .contains(/delete/i)
     .click();
-};
 
-const andIGoToDeleteMySecondUse = () => {
-  cy.get("h2")
+const andIGoToDeleteMySecondUse = () =>
+  cy
+    .get("h2")
     .contains(/second use/i)
     .siblings()
     .contains(/delete/i)
     .click();
-};
 
-const iAmPromptedToConfirmDeletionOfMyMainUse = () => {
+const iAmPromptedToConfirmDeletionOfMyMainUse = () =>
   cy.get("h1").contains(/are you sure/i && /land/i && /cycling/i);
-};
 
-const iAmPromptedToConfirmDeletionOfMySecondUse = () => {
-  cy.get("h1").contains(
-    /are you sure/i && /maritime/i && /motor/i && /pleasure/i
-  );
-};
+const iAmPromptedToConfirmDeletionOfMySecondUse = () =>
+  cy
+    .get("h1")
+    .contains(/are you sure/i && /maritime/i && /motor/i && /pleasure/i);
+
+const iAmEditingWhatIsNowMySecondUse = iCanEditMyAdditionalLandUseMoreDetails;
