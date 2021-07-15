c71f13c788f1afad03fac8c3eef9827df459f97f
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-07-15 11:54:07 +0100

    deleteCachedUse throws errors

--- test/useCases/deleteCachedUse.test.ts
+++ test/useCases/deleteCachedUse.test.ts
@@ -2,18 +2,32 @@ import { CachedRegistrationGateway } from "../../src/gateways/CachedRegistration
 import { deleteCachedUse } from "../../src/useCases/deleteCachedUse";
 
 describe("deleteCachedUse", () => {
-  it("calls the injected CachedRegistrationGateway to delete the cached use", () => {
+  it("calls the injected CachedRegistrationGateway to delete the cached use", async () => {
     const submissionId = "test-submissionId";
     const useIndex = 0;
     const cachedRegistrationGateway: CachedRegistrationGateway = {
       deleteUse: jest.fn(),
     };
 
-    deleteCachedUse(submissionId, useIndex, cachedRegistrationGateway);
+    await deleteCachedUse(submissionId, useIndex, cachedRegistrationGateway);
 
     expect(cachedRegistrationGateway.deleteUse).toHaveBeenCalledWith(
       submissionId,
       useIndex
     );
+  });
+
+  it("throws if there is an error during deletion", async () => {
+    const submissionId = "test-submissionId";
+    const useIndex = 0;
+    const cachedRegistrationGateway: CachedRegistrationGateway = {
+      deleteUse: jest.fn().mockImplementation(() => {
+        throw new Error();
+      }),
+    };
+
+    await expect(
+      deleteCachedUse(submissionId, useIndex, cachedRegistrationGateway)
+    ).rejects.toThrow();
   });
 });
