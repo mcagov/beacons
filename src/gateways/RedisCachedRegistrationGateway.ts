a2e72b1714db7bfbfad2bc060cb8bc58544f1770
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-07-15 15:54:40 +0100

    Remove comments

--- src/gateways/RedisCachedRegistrationGateway.ts
+++ src/gateways/RedisCachedRegistrationGateway.ts
@@ -16,16 +16,10 @@ export class RedisCachedRegistrationGateway
       await cache.get(submissionId)
     ).getRegistration();
 
-    // console.log("sId: " + submissionId);
-    //
-    // console.log("1: " + registration.uses);
-
     const registrationMinusDeletedUse = {
       ...registration,
       uses: registration.uses.filter((use, i) => i !== useIndex),
     };
-
-    // console.log("2: " + registrationMinusDeletedUse.uses);
 
     await cache.set(
       submissionId,
