52d8da1188cbd73d4abc34c1ae53592107eabf0f
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-07-25 20:41:01 +0100

    Remove handlePageRequest from vessel-communications ✅

--- src/pages/register-a-beacon/vessel-communications.tsx
+++ src/pages/register-a-beacon/vessel-communications.tsx
@@ -9,119 +9,43 @@ import { AnchorLink, GovUKBody } from "../../components/Typography";
 import { FieldManager } from "../../lib/form/fieldManager";
 import { FormManager } from "../../lib/form/formManager";
 import { Validators } from "../../lib/form/validators";
-import { FormSubmission } from "../../lib/formCache";
-import { FormPageProps, handlePageRequest } from "../../lib/handlePageRequest";
+import { FormPageProps } from "../../lib/handlePageRequest";
+import { withCookiePolicy } from "../../lib/middleware";
+import { BeaconsGetServerSidePropsContext } from "../../lib/middleware/BeaconsGetServerSidePropsContext";
+import { withContainer } from "../../lib/middleware/withContainer";
+import { withSession } from "../../lib/middleware/withSession";
 import { ofcomLicenseUrl, PageURLs } from "../../lib/urls";
+import { BeaconUseFormMapper } from "../../presenters/BeaconUseFormMapper";
+import { RegistrationFormMapper } from "../../presenters/RegistrationFormMapper";
+import { makeRegistrationMapper } from "../../presenters/UseMapper";
+import { BeaconsPageRouter } from "../../router/BeaconsPageRouter";
+import { IfNoUseIndexRule } from "../../router/rules/IfNoUseIndexRule";
+import { IfUserSubmittedInvalidFormRule } from "../../router/rules/IfUserSubmittedInvalidFormRule";
+import { IfUserSubmittedValidFormRule } from "../../router/rules/IfUserSubmittedValidFormRule";
+import { IfUserViewedFormRule } from "../../router/rules/IfUserViewedFormRule";
 
-interface FormInputProps {
-  value: string;
+interface VesselCommunicationsForm {
+  callSign: string;
+  vhfRadio: string;
+  fixedVhfRadio: string;
+  fixedVhfRadioInput: string;
+  portableVhfRadio: string;
+  portableVhfRadioInput: string;
+  satelliteTelephone: string;
+  satelliteTelephoneInput: string;
+  mobileTelephone: string;
+  mobileTelephoneInput1: string;
+  mobileTelephoneInput2: string;
+  otherCommunication: string;
+  otherCommunicationInput: string;
 }
-
-const matchingConditionIsTrueForKey = (key: string) => ({
-  dependsOn: key,
-  meetingCondition: (value) => value.includes("true"),
-});
-
-const definePageForm = ({
-  callSign,
-  vhfRadio,
-  fixedVhfRadio,
-  fixedVhfRadioInput,
-  portableVhfRadio,
-  portableVhfRadioInput,
-  satelliteTelephone,
-  satelliteTelephoneInput,
-  mobileTelephone,
-  mobileTelephoneInput1,
-  mobileTelephoneInput2,
-  otherCommunication,
-  otherCommunicationInput,
-}: FormSubmission): FormManager => {
-  return new FormManager({
-    callSign: new FieldManager(callSign),
-    vhfRadio: new FieldManager(vhfRadio),
-    fixedVhfRadio: new FieldManager(fixedVhfRadio),
-    fixedVhfRadioInput: new FieldManager(
-      fixedVhfRadioInput,
-      [
-        Validators.required(
-          "We need your MMSI number if you have a fixed VHF/DSC radio"
-        ),
-        Validators.wholeNumber(
-          "Your fixed MMSI number must only include numbers 0 to 9, with no letters or other characters"
-        ),
-        Validators.isLength(
-          "Your fixed MMSI number must be exactly nine digits long",
-          9
-        ),
-      ],
-      [matchingConditionIsTrueForKey("fixedVhfRadio")]
-    ),
-    portableVhfRadio: new FieldManager(portableVhfRadio),
-    portableVhfRadioInput: new FieldManager(
-      portableVhfRadioInput,
-      [
-        Validators.required(
-          "We need your portable MMSI number if you have a portable VHF/DSC radio"
-        ),
-        Validators.wholeNumber(
-          "Your portable MMSI number must only include numbers 0 to 9, with no letters or other characters"
-        ),
-        Validators.isLength(
-          "Your portable MMSI number must be exactly nine digits long",
-          9
-        ),
-      ],
-      [matchingConditionIsTrueForKey("portableVhfRadio")]
-    ),
-    satelliteTelephone: new FieldManager(satelliteTelephone),
-    satelliteTelephoneInput: new FieldManager(
-      satelliteTelephoneInput,
-      [
-        Validators.required(
-          "We need your phone number if you have a satellite telephone"
-        ),
-        Validators.phoneNumber(
-          "Enter a satellite telephone number in the correct format"
-        ),
-      ],
-      [matchingConditionIsTrueForKey("satelliteTelephone")]
-    ),
-    mobileTelephone: new FieldManager(mobileTelephone),
-    mobileTelephoneInput1: new FieldManager(
-      mobileTelephoneInput1,
-      [
-        Validators.required(
-          "We need your telephone number if you have a mobile telephone"
-        ),
-        Validators.phoneNumber(
-          "Enter a mobile telephone number, like 07700 982736 or +447700912738"
-        ),
-      ],
-      [matchingConditionIsTrueForKey("mobileTelephone")]
-    ),
-    mobileTelephoneInput2: new FieldManager(mobileTelephoneInput2),
-    otherCommunication: new FieldManager(otherCommunication),
-    otherCommunicationInput: new FieldManager(
-      otherCommunicationInput,
-      [
-        Validators.required("We need your other communication"),
-        Validators.maxLength(
-          "Other communication has too many characters",
-          250
-        ),
-      ],
-      [matchingConditionIsTrueForKey("otherCommunication")]
-    ),
-  });
-};
 
 const VesselCommunications: FunctionComponent<FormPageProps> = ({
   form,
   showCookieBanner,
 }: FormPageProps): JSX.Element => {
   const pageHeading =
-    "How can we communicate with you, when on this vessel, rig or windfarm?";
+    "How can we communicate with you when you are in this vessel, rig or windfarm?";
 
   const pageText: ReactNode = (
     <>
@@ -152,6 +76,10 @@ const VesselCommunications: FunctionComponent<FormPageProps> = ({
     </BeaconsForm>
   );
 };
+
+interface FormInputProps {
+  value: string;
+}
 
 const CallSign: FunctionComponent<FormInputProps> = ({
   value,
@@ -284,9 +212,176 @@ const TypesOfCommunication: FunctionComponent<FormPageProps> = ({
   </FormFieldset>
 );
 
-export const getServerSideProps: GetServerSideProps = handlePageRequest(
-  "/register-a-beacon/more-details",
-  definePageForm
+export const getServerSideProps: GetServerSideProps = withCookiePolicy(
+  withContainer(
+    withSession(async (context: BeaconsGetServerSidePropsContext) => {
+      const nextPage = PageURLs.moreDetails;
+
+      return await new BeaconsPageRouter([
+        new IfNoUseIndexRule(context),
+        new IfUserViewedFormRule<VesselCommunicationsForm>(
+          context,
+          validationRules,
+          mapper(context)
+        ),
+        new IfUserSubmittedInvalidFormRule<VesselCommunicationsForm>(
+          context,
+          validationRules,
+          mapper(context)
+        ),
+        new IfUserSubmittedValidFormRule<VesselCommunicationsForm>(
+          context,
+          validationRules,
+          mapper(context),
+          nextPage
+        ),
+      ]).execute();
+    })
+  )
 );
+
+const mapper = (
+  context: BeaconsGetServerSidePropsContext
+): RegistrationFormMapper<VesselCommunicationsForm> =>
+  (() => {
+    const beaconUseMapper: BeaconUseFormMapper<VesselCommunicationsForm> = {
+      toDraftBeaconUse: (form) => ({
+        callSign: form.callSign,
+        vhfRadio: form.vhfRadio,
+        fixedVhfRadio: form.fixedVhfRadio,
+        fixedVhfRadioInput: form.fixedVhfRadioInput,
+        portableVhfRadio: form.portableVhfRadio,
+        portableVhfRadioInput: form.portableVhfRadioInput,
+        satelliteTelephone: form.satelliteTelephone,
+        satelliteTelephoneInput: form.satelliteTelephoneInput,
+        mobileTelephone: form.mobileTelephone,
+        mobileTelephoneInput1: form.mobileTelephoneInput1,
+        mobileTelephoneInput2: form.mobileTelephoneInput2,
+        otherCommunication: form.otherCommunication,
+        otherCommunicationInput: form.otherCommunicationInput,
+      }),
+      toForm: (draftRegistration) => ({
+        callSign: draftRegistration.callSign,
+        vhfRadio: draftRegistration.vhfRadio,
+        fixedVhfRadio: draftRegistration.fixedVhfRadio,
+        fixedVhfRadioInput: draftRegistration.fixedVhfRadioInput,
+        portableVhfRadio: draftRegistration.portableVhfRadio,
+        portableVhfRadioInput: draftRegistration.portableVhfRadioInput,
+        satelliteTelephone: draftRegistration.satelliteTelephone,
+        satelliteTelephoneInput: draftRegistration.satelliteTelephoneInput,
+        mobileTelephone: draftRegistration.mobileTelephone,
+        mobileTelephoneInput1: draftRegistration.mobileTelephoneInput1,
+        mobileTelephoneInput2: draftRegistration.mobileTelephoneInput2,
+        otherCommunication: draftRegistration.otherCommunication,
+        otherCommunicationInput: draftRegistration.otherCommunicationInput,
+      }),
+    };
+
+    const useIndex = parseInt(context.query.useIndex as string);
+
+    return makeRegistrationMapper<VesselCommunicationsForm>(
+      useIndex,
+      beaconUseMapper
+    );
+  })();
+
+const validationRules = ({
+  callSign,
+  vhfRadio,
+  fixedVhfRadio,
+  fixedVhfRadioInput,
+  portableVhfRadio,
+  portableVhfRadioInput,
+  satelliteTelephone,
+  satelliteTelephoneInput,
+  mobileTelephone,
+  mobileTelephoneInput1,
+  mobileTelephoneInput2,
+  otherCommunication,
+  otherCommunicationInput,
+}: VesselCommunicationsForm): FormManager => {
+  const matchingConditionIsTrueForKey = (key: string) => ({
+    dependsOn: key,
+    meetingCondition: (value) => value.includes("true"),
+  });
+
+  return new FormManager({
+    callSign: new FieldManager(callSign),
+    vhfRadio: new FieldManager(vhfRadio),
+    fixedVhfRadio: new FieldManager(fixedVhfRadio),
+    fixedVhfRadioInput: new FieldManager(
+      fixedVhfRadioInput,
+      [
+        Validators.required(
+          "We need your MMSI number if you have a fixed VHF/DSC radio"
+        ),
+        Validators.wholeNumber(
+          "Your fixed MMSI number must only include numbers 0 to 9, with no letters or other characters"
+        ),
+        Validators.isLength(
+          "Your fixed MMSI number must be exactly nine digits long",
+          9
+        ),
+      ],
+      [matchingConditionIsTrueForKey("fixedVhfRadio")]
+    ),
+    portableVhfRadio: new FieldManager(portableVhfRadio),
+    portableVhfRadioInput: new FieldManager(
+      portableVhfRadioInput,
+      [
+        Validators.required(
+          "We need your portable MMSI number if you have a portable VHF/DSC radio"
+        ),
+        Validators.wholeNumber(
+          "Your portable MMSI number must only include numbers 0 to 9, with no letters or other characters"
+        ),
+        Validators.isLength(
+          "Your portable MMSI number must be exactly nine digits long",
+          9
+        ),
+      ],
+      [matchingConditionIsTrueForKey("portableVhfRadio")]
+    ),
+    satelliteTelephone: new FieldManager(satelliteTelephone),
+    satelliteTelephoneInput: new FieldManager(
+      satelliteTelephoneInput,
+      [
+        Validators.required(
+          "We need your phone number if you have a satellite telephone"
+        ),
+        Validators.phoneNumber(
+          "Enter a satellite telephone number in the correct format"
+        ),
+      ],
+      [matchingConditionIsTrueForKey("satelliteTelephone")]
+    ),
+    mobileTelephone: new FieldManager(mobileTelephone),
+    mobileTelephoneInput1: new FieldManager(
+      mobileTelephoneInput1,
+      [
+        Validators.required(
+          "We need your telephone number if you have a mobile telephone"
+        ),
+        Validators.phoneNumber(
+          "Enter a mobile telephone number, like 07700 982736 or +447700912738"
+        ),
+      ],
+      [matchingConditionIsTrueForKey("mobileTelephone")]
+    ),
+    mobileTelephoneInput2: new FieldManager(mobileTelephoneInput2),
+    otherCommunication: new FieldManager(otherCommunication),
+    otherCommunicationInput: new FieldManager(
+      otherCommunicationInput,
+      [
+        Validators.required("We need your other communication"),
+        Validators.maxLength(
+          "Other communication has too many characters",
+          250
+        ),
+      ],
+      [matchingConditionIsTrueForKey("otherCommunication")]
+    ),
+  });
+};
 
 export default VesselCommunications;
