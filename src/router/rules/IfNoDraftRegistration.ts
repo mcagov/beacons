974d0be7744ca99921a1dabf2b4b716fe219eb0d
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-07-26 12:48:38 +0100

    Create IfNoDraftRegistration rule

---
+++ src/router/rules/IfNoDraftRegistration.ts
@@ -0,0 +1,58 @@
+import { GetServerSidePropsResult } from "next";
+import { DraftRegistration } from "../../entities/DraftRegistration";
+import { BeaconsGetServerSidePropsContext } from "../../lib/middleware/BeaconsGetServerSidePropsContext";
+import { redirectUserTo } from "../../lib/redirectUserTo";
+import { formSubmissionCookieId } from "../../lib/types";
+import { PageURLs } from "../../lib/urls";
+import { Rule } from "./Rule";
+
+export class IfNoDraftRegistration implements Rule {
+  private readonly context: BeaconsGetServerSidePropsContext;
+
+  constructor(context: BeaconsGetServerSidePropsContext) {
+    this.context = context;
+  }
+
+  public async condition(): Promise<boolean> {
+    return (
+      this.draftRegistrationCookieIdIsMissing() ||
+      (await this.draftRegistrationDoesNotExist())
+    );
+  }
+
+  public async action(): Promise<GetServerSidePropsResult<any>> {
+    if (this.draftRegistrationCookieIdIsMissing())
+      return redirectUserTo(PageURLs.start);
+
+    await this.createBlankDraftRegistration();
+
+    return this.reloadPage();
+  }
+
+  private async createBlankDraftRegistration(): Promise<void> {
+    const blankDraftRegistration: DraftRegistration = {};
+
+    await this.context.container.saveDraftRegistration(
+      this.cookieId(),
+      blankDraftRegistration
+    );
+  }
+
+  private reloadPage() {
+    return redirectUserTo(this.context.req.url);
+  }
+
+  private cookieId(): string {
+    return this.context.req?.cookies[formSubmissionCookieId];
+  }
+
+  private draftRegistrationCookieIdIsMissing(): boolean {
+    return !this.cookieId();
+  }
+
+  private async draftRegistrationDoesNotExist() {
+    return !(await this.context.container.getDraftRegistration(
+      this.cookieId()
+    ));
+  }
+}
