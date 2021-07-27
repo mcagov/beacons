7cfa5e843fbe337b65d43215e5d40ad6717addc0
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-07-27 14:00:19 +0100

    Unit tests for makeDraftRegistrationMapper

--- src/presenters/makeDraftRegistrationMapper.ts
+++ src/presenters/makeDraftRegistrationMapper.ts
@@ -6,15 +6,13 @@ export const makeDraftRegistrationMapper = <T>(
   beaconUseMapper: BeaconUseFormMapper<T>
 ): RegistrationFormMapper<T> => ({
   toDraftRegistration: (form) => {
-    console.log("form: ", form);
     return {
-      uses: new Array(useIndex > 1 ? useIndex - 1 : 1)
+      uses: new Array(useIndex >= 1 ? useIndex + 1 : 1)
         .fill({})
         .fill(beaconUseMapper.toDraftBeaconUse(form), useIndex, useIndex + 1),
     };
   },
   toForm: (draftRegistration) => {
-    console.log("draftRegistration: ", draftRegistration);
     return beaconUseMapper.toForm(draftRegistration.uses[useIndex]);
   },
 });
