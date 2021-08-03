a269c4cb1e1e63b98c189246e367d19b4b5fbd09
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-08-03 16:33:20 +0100

    Delete unused middleware fn seedCache

--- src/lib/middleware.ts
+++ src/lib/middleware.ts
@@ -1,30 +1,8 @@
 import { CookieSerializeOptions, serialize } from "cookie";
 import { IncomingMessage, ServerResponse } from "http";
 import { GetServerSidePropsContext } from "next";
-import { NextApiRequestCookies } from "next/dist/next-server/server/api-utils";
 import parse from "urlencoded-body-parser";
-import { v4 as uuidv4 } from "uuid";
-import { FormCacheFactory, IFormCache } from "./formCache";
 import { formSubmissionCookieId } from "./types";
-
-export const setFormSubmissionCookie = async (
-  context: GetServerSidePropsContext,
-  seedCacheFn: (id: string) => Promise<void> = seedCache
-): Promise<void> => {
-  const cookies: NextApiRequestCookies = context.req.cookies;
-
-  if (!cookies || !cookies[formSubmissionCookieId]) {
-    const id: string = uuidv4();
-
-    await seedCacheFn(id);
-    setCookieHeader(id, context.res);
-  }
-};
-
-const seedCache = async (id: string): Promise<void> => {
-  const cache: IFormCache = FormCacheFactory.getCache();
-  await cache.update(id);
-};
 
 export const setCookieHeader = (id: string, res: ServerResponse): void => {
   const options: CookieSerializeOptions = {
