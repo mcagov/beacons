aab8b1cb583aaebf4bb12f101670c0b7093b148e
Stephen Strudwick <stephenstrudwick@stephenwicksair.home>
2021-08-25 09:31:43 +0100

    dashboard linked to terraformed alarms

--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -2,16 +2,16 @@ resource "aws_cloudwatch_dashboard" "service-health" {
   dashboard_name = "beacons-dashboard"
 
   dashboard_body = <<-EOT
-{                   
-    "widgets": [        
-        {           
+{
+    "widgets": [
+        {
             "height": 3,
-            "width": 6, 
+            "width": 6,
             "y": 7,
             "x": 0,
             "type": "metric",
             "properties": {
-                "title": "Low CPU Credit Balance",
+                "title": "CPU Credit Balance",
                 "annotations": {
                     "alarms": [
                         "${module.aws-rds-alarms.alarm_cpu_credit_balance_too_low[0].arn}"
@@ -20,8 +20,268 @@ resource "aws_cloudwatch_dashboard" "service-health" {
                 "view": "singleValue",
                 "stacked": false,
                 "type": "chart"
-            }       
-        }              
+            }
+        },
+        {
+            "height": 3,
+            "width": 6,
+            "y": 10,
+            "x": 6,
+            "type": "metric",
+            "properties": {
+                "title": "Freeable Memory",
+                "annotations": {
+                    "alarms": [
+                        "${module.aws-rds-alarms.alarm_memory_freeable_too_low.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "height": 3,
+            "width": 6,
+            "y": 10,
+            "x": 0,
+            "type": "metric",
+            "properties": {
+                "title": "Free Storage Space",
+                "annotations": {
+                    "alarms": [
+                        "${module.aws-rds-alarms.alarm_disk_free_storage_space_too_low.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "height": 3,
+            "width": 6,
+            "y": 7,
+            "x": 6,
+            "type": "metric",
+            "properties": {
+                "title": "Swap Usage",
+                "annotations": {
+                    "alarms": [
+                        "${module.aws-rds-alarms.alarm_memory_swap_usage_too_high.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "height": 3,
+            "width": 6,
+            "y": 10,
+            "x": 12,
+            "type": "metric",
+            "properties": {
+                "title": "CPU Utilization",
+                "annotations": {
+                    "alarms": [
+                        "${module.aws-rds-alarms.alarm_cpu_utilization_too_high.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "height": 3,
+            "width": 6,
+            "y": 7,
+            "x": 12,
+            "type": "metric",
+            "properties": {
+                "title": "Disk Queue Depth",
+                "annotations": {
+                    "alarms": [
+                        "${module.aws-rds-alarms.alarm_disk_queue_depth_too_high.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "height": 6,
+            "width": 6,
+            "y": 7,
+            "x": 18,
+            "type": "metric",
+            "properties": {
+                "title": "Anomalous Connection Count",
+                "annotations": {
+                    "alarms": [
+                        "${module.aws-rds-alarms.alarm_connection_count_anomalous.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "height": 3,
+            "width": 6,
+            "y": 2,
+            "x": 0,
+            "type": "metric",
+            "properties": {
+                "title": "Webapp Health",
+                "annotations": {
+                    "alarms": [
+                        "${aws_cloudwatch_metric_alarm.webapp-health.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "height": 2,
+            "width": 24,
+            "y": 13,
+            "x": 0,
+            "type": "text",
+            "properties": {
+                "markdown": "# ECS Cluster Health\n## Alarms"
+            }
+        },
+        {
+            "height": 2,
+            "width": 24,
+            "y": 0,
+            "x": 0,
+            "type": "text",
+            "properties": {
+                "markdown": "# Beacons Health\n## Health Check Alarms"
+            }
+        },
+        {
+            "height": 3,
+            "width": 6,
+            "y": 2,
+            "x": 6,
+            "type": "metric",
+            "properties": {
+                "title": "Webapp Task Count",
+                "annotations": {
+                    "alarms": [
+                        "${aws_cloudwatch_metric_alarm.ecs_webapp_task_count_too_low.arn}"
+                    ]
+                },
+                "view": "singleValue"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 12,
+            "y": 2,
+            "width": 6,
+            "height": 3,
+            "properties": {
+                "title": "API Service Task Count",
+                "annotations": {
+                    "alarms": [
+                        "${aws_cloudwatch_metric_alarm.ecs_service_task_count_too_low.arn}"
+                    ]
+                },
+                "view": "singleValue"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 15,
+            "width": 6,
+            "height": 3,
+            "properties": {
+                "title": "Webapp Memory Utilization",
+                "annotations": {
+                    "alarms": [
+                        "${aws_cloudwatch_metric_alarm.ecs_webapp_memory_too_high.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 6,
+            "y": 15,
+            "width": 6,
+            "height": 3,
+            "properties": {
+                "title": "API Service Memory Utilization",
+                "annotations": {
+                    "alarms": [
+                        "${aws_cloudwatch_metric_alarm.ecs_service_memory_too_high.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 12,
+            "y": 15,
+            "width": 6,
+            "height": 3,
+            "properties": {
+                "title": "Webapp CPU Utilization",
+                "annotations": {
+                    "alarms": [
+                        "${aws_cloudwatch_metric_alarm.ecs_webapp_cpu_too_high.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 18,
+            "y": 15,
+            "width": 6,
+            "height": 3,
+            "properties": {
+                "title": "API Service CPU Utilization",
+                "annotations": {
+                    "alarms": [
+                        "${aws_cloudwatch_metric_alarm.ecs_service_cpu_too_high.arn}"
+                    ]
+                },
+                "view": "singleValue",
+                "stacked": false,
+                "type": "chart"
+            }
+        },
+        {
+            "height": 2,
+            "width": 24,
+            "y": 5,
+            "x": 0,
+            "type": "text",
+            "properties": {
+                "markdown": "# Database Health\n## Alarms"
+            }
+        }
     ]
 }
 EOT
