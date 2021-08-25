d7a632ce05d9a2791436ee6e9ddcd1bd5df3c6b5
Olly Swanson <olly.swanson95@gmail.com>
2021-08-25 14:05:29 +0100

    Moved submissionId cookie test to separate file

--- cypress/integration/individual-pages/serviceStart.spec.ts
+++ cypress/integration/individual-pages/serviceStart.spec.ts
@@ -1,5 +1,3 @@
-import { givenIHaveSignedIn } from "../common/selectors-and-assertions.spec";
-
 describe("Service start page", () => {
   const acceptRejectCookieId = "acceptRejectId";
 
@@ -13,15 +11,5 @@ describe("Service start page", () => {
     cy.visit("/");
     cy.get("button").contains("Hide this message").click();
     cy.getCookie(acceptRejectCookieId).should("exist");
-  });
-});
-
-describe("Your beacon account registry account page", () => {
-  const submissionCookieId = "submissionId";
-
-  it("Sets the cookie for the form submission", () => {
-    givenIHaveSignedIn();
-    cy.visit("/account/your-beacon-registry-account");
-    cy.getCookie(submissionCookieId).should("exist");
   });
 });
