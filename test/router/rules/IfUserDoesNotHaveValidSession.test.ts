52f9f2b84af49c2bcc25296224104b94e13698fd
Olly Swanson <olly.swanson95@gmail.com>
2021-08-26 12:47:59 +0100

    Added unit test for IfUserDoesNotHaveValidSession

---
+++ test/router/rules/IfUserDoesNotHaveValidSession.test.ts
@@ -0,0 +1,62 @@
+import { BeaconsSession } from "../../../src/gateways/NextAuthUserSessionGateway";
+import { PageURLs } from "../../../src/lib/urls";
+import { IfUserDoesNotHaveValidSession } from "../../../src/router/rules/IfUserDoesNotHaveValidSession";
+
+describe("IfUserDoesNotHaveValidSession", () => {
+  it("routes the user to the sign up or sign in page if the session is not valid", async () => {
+    const mockGetSession = jest.fn();
+    mockGetSession.mockReturnValueOnce(null);
+
+    const context = {
+      container: {
+        sessionGateway: {
+          getSession: mockGetSession,
+        },
+      },
+    };
+
+    const rule = new IfUserDoesNotHaveValidSession(context as any);
+
+    const condition = await rule.condition();
+    expect(condition).toBe(true);
+
+    const result = await rule.action();
+
+    expect(mockGetSession).toHaveBeenCalledTimes(1);
+    expect(result).toStrictEqual({
+      redirect: {
+        statusCode: 303,
+        destination: PageURLs.signUpOrSignIn,
+      },
+    });
+  });
+
+  it("does not trigger if the user has a valid session", async () => {
+    const mockGetSession = jest.fn();
+    const session: BeaconsSession = {
+      user: {
+        authId: "foo",
+        name: "bar",
+        email: "baz",
+        image: "qux",
+      },
+      expires: "quux",
+    };
+    mockGetSession.mockReturnValueOnce(session);
+
+    const context = {
+      container: {
+        sessionGateway: {
+          getSession: mockGetSession,
+        },
+      },
+    };
+
+    const rule = new IfUserDoesNotHaveValidSession(context as any);
+
+    const condition = await rule.condition();
+
+    expect(mockGetSession).toHaveBeenCalledTimes(1);
+    expect(condition).toBe(false);
+  });
+});
