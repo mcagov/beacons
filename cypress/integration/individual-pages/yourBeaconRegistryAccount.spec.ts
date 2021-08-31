33b5b28556f76f8cfdf1783c8c5c1f2c0698d4ea
Olly Swanson <olly.swanson95@gmail.com>
2021-08-31 09:16:38 +0100

    Update yourBeaconRegistryAccount.spec.ts

--- cypress/integration/individual-pages/yourBeaconRegistryAccount.spec.ts
+++ cypress/integration/individual-pages/yourBeaconRegistryAccount.spec.ts
@@ -1,9 +1,10 @@
+import { PageURLs } from "../../../src/lib/urls";
 import { givenIDoNotHaveAValidSession } from "../common/selectors-and-assertions.spec";
 
 describe("Your beacon registry account", () => {
   it("Redirects the user to the sign up or sign in page if the user does not have a valid session", () => {
     givenIDoNotHaveAValidSession();
     cy.visit("/account/your-beacon-registry-account");
-    cy.url().should("eq", "http://localhost:3000/account/sign-up-or-sign-in");
+    cy.url().should("contain", PageURLs.signUpOrSignIn);
   });
 });
