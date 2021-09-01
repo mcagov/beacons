70cd2901d2841182f096ac21de91840b655d92e2
Olly Swanson <olly.swanson95@gmail.com>
2021-09-01 13:34:27 +0100

    Update test description to reflect purpose

--- cypress/integration/individual-pages/yourBeaconRegistryAccount.spec.ts
+++ cypress/integration/individual-pages/yourBeaconRegistryAccount.spec.ts
@@ -2,7 +2,7 @@ import { PageURLs } from "../../../src/lib/urls";
 import { givenIDoNotHaveAValidSession } from "../common/selectors-and-assertions.spec";
 
 describe("Your beacon registry account", () => {
-  it("Redirects the user to the sign up or sign in page if the user does not have a valid session", () => {
+  it("Redirects the user to the unauthenticated page if the user does not have a valid session", () => {
     givenIDoNotHaveAValidSession();
     cy.visit("/account/your-beacon-registry-account");
     cy.url().should("contain", PageURLs.unauthenticated);
