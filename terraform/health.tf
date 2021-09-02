2118061fc19e241dd8e0c29d7442c10543ebd293
Stephen Strudwick <stephenstrudwick@stephenwicksair.home>
2021-09-02 16:05:38 +0100

    Added API service health check to r53 us-east and dashboard widget

--- terraform/health.tf
+++ terraform/health.tf
@@ -27,3 +27,33 @@ resource "aws_route53_health_check" "webapp_health_check" {
   type              = "HTTPS_STR_MATCH"
   search_string     = "Ship shape and Bristol fashion"
 }
+
+resource "aws_cloudwatch_metric_alarm" "service_health" {
+  namespace           = "AWS/Route53"
+  alarm_name          = "service-health-alarm"
+  metric_name         = "HealthCheckStatus"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  period              = "60"
+  statistic           = "Minimum"
+  threshold           = "0"
+  alarm_description   = "This metric monitors API service health"
+  provider            = aws.us-east
+  alarm_actions       = var.enable_alerts == true ? [aws_sns_topic.sns_service_alerts.arn] : []
+  ok_actions          = var.enable_alerts == true ? [aws_sns_topic.sns_service_alerts.arn] : []
+
+  dimensions = {
+    HealthCheckId = aws_route53_health_check.service_health_check.id
+  }
+}
+
+resource "aws_route53_health_check" "service_health_check" {
+  reference_name    = "service-health-check"
+  failure_threshold = 5
+  fqdn              = var.webapp_fqdn
+  port              = 443
+  request_interval  = "30"
+  resource_path     = var.service_health_check_path
+  type              = "HTTPS_STR_MATCH"
+  search_string     = "UP"
+}
