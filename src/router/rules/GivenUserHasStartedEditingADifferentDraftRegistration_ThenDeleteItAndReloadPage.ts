e938857b71e031caf76b8df515b48e879b358285
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-06 11:06:36 +0100

    Complete GivenUserHasStartedEditingADifferentDraftRegistration_ThenDeleteItAndReloadPage unit testing 😄

--- src/router/rules/GivenUserHasStartedEditingADifferentDraftRegistration_ThenDeleteItAndReloadPage.ts
+++ src/router/rules/GivenUserHasStartedEditingADifferentDraftRegistration_ThenDeleteItAndReloadPage.ts
@@ -1,9 +1,10 @@
 import { GetServerSidePropsResult } from "next";
 import { BeaconsGetServerSidePropsContext } from "../../lib/middleware/BeaconsGetServerSidePropsContext";
+import { redirectUserTo } from "../../lib/redirectUserTo";
 import { formSubmissionCookieId } from "../../lib/types";
 import { Rule } from "./Rule";
 
-export class GivenUserHasStartedEditingADifferentDraftRegistration_ThenDeleteIt
+export class GivenUserHasStartedEditingADifferentDraftRegistration_ThenDeleteItAndReloadPage
   implements Rule
 {
   private context: BeaconsGetServerSidePropsContext;
@@ -12,14 +13,18 @@ export class GivenUserHasStartedEditingADifferentDraftRegistration_ThenDeleteIt
     this.context = context;
   }
 
-  async condition(): Promise<boolean> {
+  public async condition(): Promise<boolean> {
     return (
       this.context.req.cookies[formSubmissionCookieId] !==
       this.context.query?.id
     );
   }
 
-  action(): Promise<GetServerSidePropsResult<any>> {
-    return Promise.resolve(undefined);
+  public async action(): Promise<GetServerSidePropsResult<any>> {
+    await this.context.container.deleteDraftRegistration(
+      this.context.req.cookies[formSubmissionCookieId]
+    );
+
+    return redirectUserTo(this.context.req.url);
   }
 }
