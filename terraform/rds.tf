3d638b3a5865892c0fe9ee4b1f4c91825e5ab471
Matthew Carr <matt.carr@madetech.com>
2021-09-07 10:20:22 +0100

    Bump postgres version to 12.7

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "postgres" {
   identifier                      = "${terraform.workspace}-beacons-database"
   allocated_storage               = var.db_storage
   engine                          = "postgres"
-  engine_version                  = "12.5"
+  engine_version                  = "12.7"
   db_subnet_group_name            = aws_db_subnet_group.db.id
   vpc_security_group_ids          = [aws_security_group.db.id]
   deletion_protection             = var.db_delete_protection
@@ -14,6 +14,7 @@ resource "aws_db_instance" "postgres" {
   skip_final_snapshot             = var.db_skip_final_snapshot
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
   allow_major_version_upgrade     = true
+  auto_minor_version_upgrade = true
   backup_window                   = var.backup_window
   backup_retention_period         = var.backup_retention_period
   copy_tags_to_snapshot           = true
