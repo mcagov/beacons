72750d263f9f81a767b7ec655f904a96357863c6
Ninamma Rai <32230328+Ninamma@users.noreply.github.com>
2021-09-09 12:06:21 +0100

    Add updateRegistration method on beacons API gateway

--- src/gateways/interfaces/BeaconGateway.ts
+++ src/gateways/interfaces/BeaconGateway.ts
@@ -4,5 +4,10 @@ import { IDeleteBeaconRequest } from "../BeaconsApiBeaconGateway";
 export interface BeaconGateway {
   sendRegistration: (draftRegistration: DraftRegistration) => Promise<boolean>;
 
+  updateRegistration: (
+    draftRegistration: DraftRegistration,
+    registrationId: string
+  ) => Promise<boolean>;
+
   deleteBeacon: (json: IDeleteBeaconRequest) => Promise<boolean>;
 }
