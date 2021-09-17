eb48f0d0e8a8ff352b3ca1bb650e45ba9bedbe13
Olly Swanson <olly.swanson95@gmail.com>
2021-09-17 11:11:40 +0100

    Updated test cases

--- test/router/rules/WhenUserSubmitsFeedback.test.ts
+++ test/router/rules/WhenUserSubmitsFeedback.test.ts
@@ -1,11 +1,17 @@
+const referenceId = "a-not-so-uuid";
+jest.mock("uuid", () => ({ v4: () => referenceId }));
+
 import { FeedbackURLs } from "../../../src/lib/urls";
 import { validationRules } from "../../../src/pages/feedback";
 import { WhenUserSubmitsFeedback } from "../../../src/router/rules/WhenUserSubmitsFeedback";
 
 describe("WhenUserSubmitsFeedback", () => {
+  const templateId = "templateId";
+  const email = "not-a-real-email@nope.com";
+
   beforeAll(() => {
-    process.env.GOV_NOTIFY_FEEDBACK_EMAIL_TEMPLATE = "template-id";
-    process.env.GOV_NOTIFY_FEEDBACK_EMAIL_ADDRESS = "not-a-real-email@nope.com";
+    process.env.GOV_NOTIFY_FEEDBACK_EMAIL_TEMPLATE = templateId;
+    process.env.GOV_NOTIFY_FEEDBACK_EMAIL_ADDRESS = email;
   });
 
   it("should route the user to the feedback confirmation page with success if the user successfully submits feedback", async () => {
@@ -14,7 +20,12 @@ describe("WhenUserSubmitsFeedback", () => {
       sendEmail: mockSendEmail,
     };
 
-    const mockForm = {};
+    const satisfactionRating = "verySatisfied";
+    const howCouldWeImproveThisService = "You can't, it's perfect";
+    const mockForm = {
+      satisfactionRating,
+      howCouldWeImproveThisService,
+    };
     const mockParseFormDataAs = jest.fn().mockReturnValueOnce(mockForm);
 
     const context = {
@@ -34,6 +45,15 @@ describe("WhenUserSubmitsFeedback", () => {
 
     const result = await rule.action();
     expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      templateId,
+      email,
+      expect.objectContaining({
+        satisfactionRating,
+        howCouldWeImproveThisService,
+        referenceId,
+      })
+    );
     expect(result).toStrictEqual({
       redirect: {
         statusCode: 303,
@@ -48,7 +68,13 @@ describe("WhenUserSubmitsFeedback", () => {
       sendEmail: mockSendEmail,
     };
 
-    const mockForm = {};
+    const satisfactionRating = "veryDissatisfied";
+    const howCouldWeImproveThisService = "You should have used Ruby on Rails";
+
+    const mockForm = {
+      satisfactionRating,
+      howCouldWeImproveThisService,
+    };
     const mockParseFormDataAs = jest.fn().mockReturnValueOnce(mockForm);
 
     const context = {
@@ -68,6 +94,15 @@ describe("WhenUserSubmitsFeedback", () => {
 
     const result = await rule.action();
     expect(mockSendEmail).toHaveBeenCalledTimes(1);
+    expect(mockSendEmail).toHaveBeenCalledWith(
+      templateId,
+      email,
+      expect.objectContaining({
+        satisfactionRating,
+        howCouldWeImproveThisService,
+        referenceId,
+      })
+    );
     expect(result).toStrictEqual({
       redirect: {
         statusCode: 303,
