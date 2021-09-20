2f9e9dda3fcdd4cc2de75a42c92af878d558440e
SianRMadeTech <sian.richards@madetech.com>
2021-09-20 14:57:54 +0100

    wait added to view a legacy cyp test

--- cypress/integration/single-beacon-owner/i-can-view-a-legacy-beacon.spec.ts
+++ cypress/integration/single-beacon-owner/i-can-view-a-legacy-beacon.spec.ts
@@ -1,11 +1,15 @@
 import { iHavePreviouslyRegisteredALegacyBeacon } from "../../common/i-have-a-legacy-beacon.spec";
-import { givenIHaveSignedIn } from "../../common/selectors-and-assertions.spec";
+import {
+  givenIHaveSignedIn,
+  givenIHaveWaitedForBeaconsApi,
+} from "../../common/selectors-and-assertions.spec";
 import { legacyBeaconRequest } from "../../fixtures/migration";
 
 describe("As an account holder", () => {
   it("I can view a legacy beacon linked to my email", () => {
     givenIHaveSignedIn();
     iHavePreviouslyRegisteredALegacyBeacon(legacyBeaconRequest);
+    givenIHaveWaitedForBeaconsApi(10000);
     cy.visit("/account/your-beacon-registry-account");
   });
 });
