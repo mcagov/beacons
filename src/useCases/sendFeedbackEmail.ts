84df9febd02633b57ee4b88dee74877d21929929
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-21 15:34:24 +0100

    Hard code GovNotify template IDs
    See https://govuk-prototype-kit.herokuapp.com/docs/using-notify

--- src/useCases/sendFeedbackEmail.ts
+++ src/useCases/sendFeedbackEmail.ts
@@ -2,21 +2,21 @@ import { v4 as uuidv4 } from "uuid";
 import { EmailServiceGateway } from "../gateways/interfaces/EmailServiceGateway";
 import { FormSubmission } from "../presenters/formSubmission";
 
-//TODO: Maybe restructure and add to the container object
 export const sendFeedbackEmail = async (
   emailServiceGateway: EmailServiceGateway,
   feedback: FormSubmission
 ): Promise<boolean> => {
-  const templateId = process.env.GOV_NOTIFY_FEEDBACK_EMAIL_TEMPLATE;
-  const email = process.env.GOV_NOTIFY_FEEDBACK_EMAIL_ADDRESS;
+  const feedbackEmailTemplateId = "87dc177e-942f-4484-95ba-18580e937280";
+  const feedbackDestinationEmailAddress =
+    process.env.GOV_NOTIFY_FEEDBACK_EMAIL_ADDRESS;
 
-  if (templateId) {
-    return emailServiceGateway.sendEmail(templateId, email, {
+  return emailServiceGateway.sendEmail(
+    feedbackEmailTemplateId,
+    feedbackDestinationEmailAddress,
+    {
       referenceId: uuidv4(),
       satisfactionRating: feedback.satisfactionRating,
       howCouldWeImproveThisService: feedback.howCouldWeImproveThisService,
-    });
-  }
-
-  return false;
+    }
+  );
 };
