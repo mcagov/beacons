70f01f28b84a7cd9a3b7625db2bf7d3adcc90240
SianRMadeTech <sian.richards@madetech.com>
2021-09-22 09:49:05 +0100

    cypress test .only removed

--- cypress/endToEnd/i-can-view-a-legacy-beacon.spec.ts
+++ cypress/endToEnd/i-can-view-a-legacy-beacon.spec.ts
@@ -3,7 +3,7 @@ import { givenIHaveSignedIn } from "../common/selectors-and-assertions.spec";
 import { legacyBeaconRequest } from "../fixtures/migration";
 
 describe("As an account holder", () => {
-  it.only("I can view a legacy beacon linked to my email", () => {
+  it("I can view a legacy beacon linked to my email", () => {
     givenIHaveSignedIn();
     iHavePreviouslyRegisteredALegacyBeacon(legacyBeaconRequest);
     cy.visit("/account/your-beacon-registry-account");
