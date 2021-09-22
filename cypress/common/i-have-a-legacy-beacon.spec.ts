1ca6e3f5d83efdf9350dfb16a14b8ee5539c5fe9
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-09-22 09:47:16 +0100

    Remove session/account stuff from iHavePreviouslyRegisteredALegacyBeacon

--- cypress/common/i-have-a-legacy-beacon.spec.ts
+++ cypress/common/i-have-a-legacy-beacon.spec.ts
@@ -1,69 +1,21 @@
 import axios from "axios";
-import { AadAuthGateway } from "../../src/gateways/AadAuthGateway";
-import { BeaconsApiAccountHolderGateway } from "../../src/gateways/BeaconsApiAccountHolderGateway";
-import { AuthGateway } from "../../src/gateways/interfaces/AuthGateway";
 import { ILegacyBeaconRequest } from "../../src/gateways/interfaces/LegacyBeaconRequest";
-import { LegacyBeaconGateway } from "../../src/gateways/LegacyBeaconGateway";
-import { IAppContainer } from "../../src/lib/IAppContainer";
-import { getOrCreateAccountHolder } from "../../src/useCases/getOrCreateAccountHolder";
-import { givenIHaveWaitedForBeaconsApi } from "./selectors-and-assertions.spec";
-
-const createLegacyBeacon = async (
-  apiUrl: string,
-  migratedLegacyBeaconEndpoint: string,
-  legacyBeaconRequest: ILegacyBeaconRequest
-) => {
-  const url = `${apiUrl}/${migratedLegacyBeaconEndpoint}`;
-  const response = await axios.post(url, legacyBeaconRequest, {
-    auth: {
-      username: "user",
-      password: "password",
-    },
-  });
-  cy.log("legacy beacon successfully posted");
-};
 
 export const iHavePreviouslyRegisteredALegacyBeacon = async (
   legacyBeaconRequest: ILegacyBeaconRequest
 ): Promise<void> => {
-  const beaconsApiAuthGateway: AuthGateway = new AadAuthGateway({
-    auth: {
-      clientId: Cypress.env("WEBAPP_CLIENT_ID"),
-      authority: `https://login.microsoftonline.com/${Cypress.env(
-        "AAD_TENANT_ID"
-      )}`,
-      clientSecret: Cypress.env("WEBAPP_CLIENT_SECRET"),
-    },
-    apiId: Cypress.env("AAD_API_ID"),
-  });
+  const url = `${Cypress.env("API_URL")}/migrate/legacy-beacon`;
 
-  const accountHolderApiGateway = new BeaconsApiAccountHolderGateway(
-    Cypress.env("API_URL"),
-    beaconsApiAuthGateway
-  );
-
-  const legacyBeaconGateway = new LegacyBeaconGateway(Cypress.env("API_URL"));
-
-  const container: Partial<IAppContainer> = {
-    legacyBeaconGateway: legacyBeaconGateway,
-    accountHolderGateway: accountHolderApiGateway,
-  };
-
-  const sessionEndpoint = "/api/auth/session";
-
-  cy.request(sessionEndpoint).then(async (response) => {
-    cy.log("logged in");
-    const session = response.body;
-    const migrateLegacyBeaconEndpoint = "migrate/legacy-beacon";
-
-    const accountHolder = await getOrCreateAccountHolder(container)(session);
-
-    givenIHaveWaitedForBeaconsApi(10000);
-
-    await createLegacyBeacon(
-      Cypress.env("API_URL"),
-      migrateLegacyBeaconEndpoint,
-      legacyBeaconRequest
-    );
-  });
+  try {
+    await axios.post(url, legacyBeaconRequest, {
+      auth: {
+        username: "user",
+        password: "password",
+      },
+    });
+    cy.log("LegacyBeacon successfully POSTed");
+  } catch (e) {
+    cy.log("Seeding LegacyBeacon failed with message ", e);
+    throw e;
+  }
 };
