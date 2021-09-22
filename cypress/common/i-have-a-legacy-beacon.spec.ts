19a0cd2e6260007ee3c75a9c7681ea91043f1da8
SianRMadeTech <sian.richards@madetech.com>
2021-09-22 10:28:48 +0100

    migrate leg beacon promise in cypress request

--- cypress/common/i-have-a-legacy-beacon.spec.ts
+++ cypress/common/i-have-a-legacy-beacon.spec.ts
@@ -5,17 +5,18 @@ export const iHavePreviouslyRegisteredALegacyBeacon = async (
   legacyBeaconRequest: ILegacyBeaconRequest
 ): Promise<void> => {
   const url = `${Cypress.env("API_URL")}/migrate/legacy-beacon`;
-
-  try {
-    await axios.post(url, legacyBeaconRequest, {
-      auth: {
-        username: "user",
-        password: "password",
-      },
-    });
-    cy.log("LegacyBeacon successfully POSTed");
-  } catch (e) {
-    cy.log("Seeding LegacyBeacon failed with message ", e);
-    throw e;
-  }
+  cy.request("/api/auth/session").then(async () => {
+    try {
+      await axios.post(url, legacyBeaconRequest, {
+        auth: {
+          username: "user",
+          password: "password",
+        },
+      });
+      cy.log("LegacyBeacon successfully POSTed");
+    } catch (e) {
+      cy.log("Seeding LegacyBeacon failed with message ", e);
+      throw e;
+    }
+  });
 };
