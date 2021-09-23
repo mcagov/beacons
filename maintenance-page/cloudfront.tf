14476d643440507f8e7ca5566abde4cd1060e304
Stephen Strudwick <stephenstrudwick@stephenwicksair.home>
2021-09-23 16:22:31 +0100

    changed 404 behaviour to always show the service down page and not confuse users

--- maintenance-page/cloudfront.tf
+++ maintenance-page/cloudfront.tf
@@ -15,8 +15,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   custom_error_response {
     error_code = "404"
-    response_code = "404"
-    response_page_path = "/error.html"
+    response_code = "200"
+    response_page_path = "/index.html"
   }
 
   default_cache_behavior {
