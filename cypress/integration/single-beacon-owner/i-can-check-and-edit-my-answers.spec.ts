88517942d64cf88fed5a12119cc3a826462944fc
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-10-12 12:24:44 +0100

    Delete empty Cypress test

--- cypress/integration/single-beacon-owner/i-can-check-and-edit-my-answers.spec.ts
+++
@@ -1,8 +0,0 @@
-describe("As a beacon owner with a completed form,", () => {
-  xit("I can change my responses before final submission", () => {
-    // TODO
-    // Click every "Change" link from check-your-answers, expect to see existing
-    // then go back to check-your-answers
-    return null;
-  });
-});
