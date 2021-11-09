592e24d36df20bd6a57337e0b72ed78513093d28
Olly Swanson <olly.swanson95@gmail.com>
2021-11-09 13:44:49 +0000

    Signout page correctly redirects to B2C signout

--- src/pages/account/sign-out.tsx
+++ src/pages/account/sign-out.tsx
@@ -1,15 +1,21 @@
+import { GetServerSideProps } from "next";
 import { signOut } from "next-auth/client";
 import React, { FunctionComponent } from "react";
 import { Layout } from "../../components/Layout";
 import { GovUKBody, PageHeading } from "../../components/Typography";
 import { AccountPageURLs, GeneralPageURLs } from "../../lib/urls";
 
-export const SignOut: FunctionComponent = (): JSX.Element => {
-  const federatedLoginSignOutUri = `https://${process.env.AZURE_B2C_TENANT_NAME}
-    .b2clogin.com/${process.env.AZURE_B2C_TENANT_NAME}
-    .onmicrosoft.com/${process.env.AZURE_B2C_LOGIN_FLOW}
-    /oauth2/v2.0/logout?post_logout_redirect_uri=
-    ${process.env.NEXTAUTH_URL}${GeneralPageURLs.start}`;
+interface SignOutProps {
+  federatedSignOutUrl: string;
+}
+
+export const SignOut: FunctionComponent<SignOutProps> = ({
+  federatedSignOutUrl,
+}): JSX.Element => {
+  const logoutHandler = async () => {
+    await signOut();
+    window.location.assign(federatedSignOutUrl);
+  };
 
   const pageHeading = "Are you sure you want to sign out?";
   return (
@@ -18,10 +24,7 @@ export const SignOut: FunctionComponent = (): JSX.Element => {
       <GovUKBody>
         <>
           <div className="govuk-button-group">
-            <button
-              className="govuk-button"
-              onClick={() => signOut({ callbackUrl: federatedLoginSignOutUri })}
-            >
+            <button className="govuk-button" onClick={logoutHandler}>
               Yes, I want to sign out
             </button>
             <a
@@ -38,3 +41,18 @@ export const SignOut: FunctionComponent = (): JSX.Element => {
 };
 
 export default SignOut;
+
+export const getServerSideProps: GetServerSideProps<SignOutProps> =
+  async () => {
+    const federatedSignOutUrl = `https://${process.env.AZURE_B2C_TENANT_NAME}\
+.b2clogin.com/${process.env.AZURE_B2C_TENANT_NAME}\
+.onmicrosoft.com/${process.env.AZURE_B2C_LOGIN_FLOW}\
+/oauth2/v2.0/logout?post_logout_redirect_uri=\
+${process.env.NEXTAUTH_URL}${GeneralPageURLs.start}`;
+
+    return {
+      props: {
+        federatedSignOutUrl,
+      },
+    };
+  };
