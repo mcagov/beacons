c9dcf6446a5f6cbeaea7ac50c89ff05941194b54
Olly Swanson <olly.swanson95@gmail.com>
2021-12-10 14:49:47 +0000

    Add log statements to getAccountHolderRegistration

--- src/useCases/getAccountHoldersRegistration.ts
+++ src/useCases/getAccountHoldersRegistration.ts
@@ -1,6 +1,7 @@
 import { Registration } from "../entities/Registration";
 import { beaconToRegistration } from "../lib/beaconToRegistration";
 import { IAppContainer } from "../lib/IAppContainer";
+import logger from "../logger";
 
 export type GetAccountHoldersRegistrationFn = (
   registrationId: string,
@@ -15,6 +16,12 @@ export const getAccountHoldersRegistration =
   ): Promise<Registration> => {
     const beacons = await accountHolderGateway.getAccountBeacons(
       accountHolderId
+    );
+    logger.info(
+      `getAccountHoldersRegistration - Beacons Len: ${beacons.length}`
+    );
+    logger.info(
+      `getAccountHoldersRegistration - Registration Id: ${registrationId}`
     );
     const beacon = beacons.find((beacon) => beacon.id === registrationId);
     return beaconToRegistration(beacon);
