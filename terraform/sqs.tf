48103103a8c14cf0f4b744b3d38b3bbb5e4e57a5
Stuart Mindt <stuart.mindt@madetech.com>
2021-12-15 10:09:27 +0000

    Removing file pushed by accident sqs.tf

--- terraform/sqs.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_sqs_queue" "beacons_queue" {
-  name                        = "beacons-queue.fifo"
-  fifo_queue                  = true
-  tags = {
-    Environment = "development"
-  }
-}
