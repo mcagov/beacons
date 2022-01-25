aca72eedf0bd41041eab79d8ca84c774232a3d2e
Olly Swanson <olly.swanson95@gmail.com>
2022-01-25 18:36:04 +0000

    Add tls security policy for https opensearch

--- terraform/opensearch.tf
+++ terraform/opensearch.tf
@@ -35,7 +35,8 @@ resource "aws_elasticsearch_domain" "opensearch" {
   }
 
   domain_endpoint_options {
-    enforce_https = true
+    enforce_https       = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
   ebs_options {
