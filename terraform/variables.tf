e44085297298774c9d5fcbef33fa2c4665b4da8d
Olly Swanson <olly.swanson95@gmail.com>
2022-02-07 16:26:03 +0000

    Add workflow to build opensearch-proxy container and register it with ECR.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -319,3 +319,7 @@ variable "opensearch_ebs_volume_size" {
   type        = number
   description = "Size of Elastic Block Store backing each data node in GB - min 10, max 100"
 }
+variable "openearch_proxy_image_tag" {
+  type = string
+  description = "Image tag of the Opensearch proxy container in ECR"
+}
