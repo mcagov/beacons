ee1a6a549c7bed0f42ac475d6ffcae7bc623ec5b
Olly Swanson <olly.swanson95@gmail.com>
2022-02-07 16:51:20 +0000

    Change port 443 to port 80 (http instead of https)

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -91,7 +91,7 @@ resource "aws_lb_listener_rule" "opensearch_proxy" {
 
 resource "aws_alb_target_group" "opensearch_proxy" {
   name        = "${terraform.workspace}-opensearch-proxy-target-group"
-  port        = 443
+  port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
