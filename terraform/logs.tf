0cd6ebf345ff10d1c2b7a0557f78e205cd3bd51c
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-02-08 16:58:13 +0000

    Simplify with inline policy JSON using example
    https://github.com/umotif-public/terraform-aws-alb/blob/2.1.0/examples/alb-with-s3-access-logs/main.tf

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -6,60 +6,58 @@ resource "aws_cloudwatch_log_group" "log_group" {
 resource "aws_s3_bucket" "logs" {
   bucket = "${module.beacons_label.namespace}-${module.beacons_label.name}-${module.beacons_label.environment}-logs"
   acl    = "log-delivery-write"
-}
 
-resource "aws_s3_bucket_policy" "logs_s3_policy" {
-  bucket = aws_s3_bucket.logs.id
-  policy = data.aws_iam_policy_document.log_policy.json
-}
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
-output "logs_s3_bucket_policy" {
-  value = data.aws_iam_policy_document.log_policy.json
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowELBRootAccount",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${local.aws_alb_regional_account_id}:root"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.logs.bucket}/*"
+    },
+    {
+      "Sid": "AWSLogDeliveryWrite",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "delivery.logs.amazonaws.com"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.logs.bucket}/*",
+      "Condition": {
+        "StringEquals": {
+          "s3:x-amz-acl": "bucket-owner-full-control"
+        }
+      }
+    },
+    {
+      "Sid": "AWSLogDeliveryAclCheck",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "delivery.logs.amazonaws.com"
+      },
+      "Action": "s3:GetBucketAcl",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.logs.bucket}"
+    }
+  ]
+}
+POLICY
 }
 
 # Global ELB account ID for eu-west-2 region
 # See https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-logging-bucket-permissions
 locals {
   aws_alb_regional_account_id = "652711504416"
-}
-
-data "aws_iam_policy_document" "log_policy" {
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.logs.arn}/${aws_alb.main.access_logs[0].prefix}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${local.aws_alb_regional_account_id}:root"]
-    }
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.logs.arn}/${aws_alb.main.access_logs[0].prefix}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["delivery.logs.amazonaws.com"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      values   = ["bucket-owner-full-control"]
-      variable = "s3:x-amz-acl"
-    }
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:GetBucketAcl"]
-    resources = [aws_s3_bucket.logs.arn]
-
-    principals {
-      type        = "Service"
-      identifiers = ["delivery.logs.amazonaws.com"]
-    }
-  }
 }
