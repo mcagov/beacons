1444a12e152a9ea48cce5376860c858e5184ad12
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-02-08 17:38:56 +0000

    Permit OpenSearch Proxy SG to speak to the OpenSearch SG via HTTPS

--- terraform/security.tf
+++ terraform/security.tf
@@ -94,7 +94,7 @@ resource "aws_security_group" "opensearch" {
     protocol        = "tcp"
     from_port       = 443
     to_port         = 443
-    security_groups = aws_security_group.ecs_tasks[*].id
+    security_groups = flatten([aws_security_group.ecs_tasks[*].id, aws_security_group.opensearch_proxy[*].id])
   }
 
   egress {
