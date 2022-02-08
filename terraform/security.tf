87defdcec81590be8566188fef5f5b32a6f2a24e
Olly Swanson <olly.swanson95@gmail.com>
2022-02-08 13:59:15 +0000

    Add http (port 80) ingress to opensearch_proxy security group.

--- terraform/security.tf
+++ terraform/security.tf
@@ -117,6 +117,13 @@ resource "aws_security_group" "opensearch_proxy" {
     security_groups = [aws_security_group.lb.id]
   }
 
+  ingress {
+    protocol = "tcp"
+    from_port = 80
+    to_port   = 80
+    security_groups = [aws_security_group.lb.id]
+  }
+
   egress {
     protocol    = "-1"
     from_port   = 0
