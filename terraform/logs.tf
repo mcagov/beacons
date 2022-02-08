8c885e2c5792403a78005e1387f015f98a9ab663
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-02-08 16:41:25 +0000

    Fully qualified principal identifier in logging S3 permissions document

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -13,22 +13,28 @@ resource "aws_s3_bucket_policy" "logs_s3_policy" {
   policy = data.aws_iam_policy_document.log_policy.json
 }
 
+# Global ELB account ID for eu-west-2 region
+# See https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html#access-logging-bucket-permissions
+locals {
+  aws_alb_regional_account_id = "652711504416"
+}
+
 data "aws_iam_policy_document" "log_policy" {
   statement {
     effect    = "Allow"
     actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.logs.arn}/*"]
+    resources = ["${aws_s3_bucket.logs.arn}/${aws_alb.main.access_logs[0].prefix}/AWSLogs/${data.aws_caller_identity.current.account_id}"]
 
     principals {
       type        = "AWS"
-      identifiers = [aws_alb.main.arn]
+      identifiers = ["arn:aws:iam::${local.aws_alb_regional_account_id}:root"]
     }
   }
 
   statement {
     effect    = "Allow"
     actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.logs.arn}/*"]
+    resources = ["${aws_s3_bucket.logs.arn}/${aws_alb.main.access_logs[0].prefix}/AWSLogs/${data.aws_caller_identity.current.account_id}"]
 
     principals {
       type        = "Service"
@@ -45,7 +51,7 @@ data "aws_iam_policy_document" "log_policy" {
   statement {
     effect    = "Allow"
     actions   = ["s3:GetBucketAcl"]
-    resources = ["${aws_s3_bucket.logs.arn}/*"]
+    resources = [aws_s3_bucket.logs.arn]
 
     principals {
       type        = "Service"
