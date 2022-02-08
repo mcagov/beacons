57533faa3522d728e566a2f8a1953b0a3e256eeb
Olly Swanson <olly.swanson95@gmail.com>
2022-02-08 14:01:43 +0000

    Ran Terraform fmt

--- terraform/security.tf
+++ terraform/security.tf
@@ -118,9 +118,9 @@ resource "aws_security_group" "opensearch_proxy" {
   }
 
   ingress {
-    protocol = "tcp"
-    from_port = 80
-    to_port   = 80
+    protocol        = "tcp"
+    from_port       = 80
+    to_port         = 80
     security_groups = [aws_security_group.lb.id]
   }
 
