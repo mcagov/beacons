603b7bd70cfe217115317ce1edf42386e0e92558
Olly Swanson <olly.swanson95@gmail.com>
2022-02-18 13:24:30 +0000

    When a registration is deleted, emit a BeaconDeleted domain event.
    When the registration deletion transaction commits, emit a domain event
    that allows, for example, the search results to be updated to reflect
    this change.

--- service/src/main/java/uk/gov/mca/beacons/api/search/listeners/BeaconListener.java
+++ service/src/main/java/uk/gov/mca/beacons/api/search/listeners/BeaconListener.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 import uk.gov.mca.beacons.api.beacon.domain.events.BeaconCreated;
+import uk.gov.mca.beacons.api.beacon.domain.events.BeaconDeleted;
 import uk.gov.mca.beacons.api.beacon.domain.events.BeaconUpdated;
 import uk.gov.mca.beacons.api.search.BeaconSearchService;
 
@@ -24,6 +25,11 @@ public class BeaconListener {
 
   @TransactionalEventListener
   public void whenCustomerUpdatesABeacon(BeaconUpdated event) {
+    beaconSearchService.index(event.getBeaconId());
+  }
+
+  @TransactionalEventListener
+  public void whenCustomerDeletesABeaconRegistration(BeaconDeleted event) {
     beaconSearchService.index(event.getBeaconId());
   }
 }
