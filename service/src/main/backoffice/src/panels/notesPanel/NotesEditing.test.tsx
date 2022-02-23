64db274250184218260ff4dce32effffa269b886
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-02-23 11:26:09 +0000

    Fix test warnings about state manipulation not being wrapped in an act() callback

--- service/src/main/backoffice/src/panels/notesPanel/NotesEditing.test.tsx
+++ service/src/main/backoffice/src/panels/notesPanel/NotesEditing.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { act, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { NoteType } from "../../entities/INote";
 import { NotesEditing } from "./NotesEditing";
@@ -8,17 +8,21 @@ describe("NotesEditing", () => {
     render(<NotesEditing onSave={jest.fn()} onCancel={jest.fn()} />);
 
     const incidentNoteRadio = screen.getByTestId(/incident-note-type/i);
-    userEvent.click(incidentNoteRadio);
-
     const generalNoteRadio = screen.getByTestId(/general-note-type/i);
-    userEvent.click(generalNoteRadio);
+
+    act(() => {
+      userEvent.click(incidentNoteRadio);
+      userEvent.click(generalNoteRadio);
+    });
   });
 
   it("user can type a note into text field", async () => {
     render(<NotesEditing onSave={jest.fn()} onCancel={jest.fn()} />);
 
     const noteInputField = screen.getByPlaceholderText("Add a note here");
-    userEvent.type(noteInputField, "Here is a note");
+    act(() => {
+      userEvent.type(noteInputField, "Here is a note");
+    });
 
     expect(await screen.findByDisplayValue("Here is a note")).toBeVisible();
   });
@@ -28,17 +32,20 @@ describe("NotesEditing", () => {
     render(<NotesEditing onSave={onSave} onCancel={jest.fn()} />);
 
     const generalNoteRadio = screen.getByTestId(/general-note-type/i);
-    userEvent.click(generalNoteRadio);
-
     const noteInputField = screen.getByPlaceholderText("Add a note here");
-    userEvent.type(noteInputField, "Here is a note");
+    act(() => {
+      userEvent.click(generalNoteRadio);
+      userEvent.type(noteInputField, "Here is a note");
+    });
 
     let saveButton = screen.getByTestId(/save/i);
     await waitFor(() => {
       expect(saveButton).toBeEnabled();
     });
 
-    userEvent.click(saveButton);
+    act(() => {
+      userEvent.click(saveButton);
+    });
 
     await waitFor(() => {
       expect(onSave).toHaveBeenCalledWith({
@@ -53,13 +60,14 @@ describe("NotesEditing", () => {
     render(<NotesEditing onSave={jest.fn()} onCancel={onCancel} />);
 
     const incidentNoteRadio = screen.getByTestId(/incident-note-type/i);
-    userEvent.click(incidentNoteRadio);
-
     const noteInputField = screen.getByPlaceholderText("Add a note here");
-    userEvent.type(noteInputField, "Here is a note");
-
     const cancelButton = screen.getByRole("button", { name: "Cancel" });
-    userEvent.click(cancelButton);
+
+    act(() => {
+      userEvent.click(incidentNoteRadio);
+      userEvent.type(noteInputField, "Here is a note");
+      userEvent.click(cancelButton);
+    });
 
     await waitFor(() => {
       expect(onCancel).toHaveBeenCalled();
@@ -68,22 +76,25 @@ describe("NotesEditing", () => {
 
   it("does not allow user to submit an incomplete note", async () => {
     render(<NotesEditing onSave={jest.fn()} onCancel={jest.fn()} />);
-
-    await waitFor(() => {
-      expect(screen.getByTestId(/save/i)).toBeDisabled();
-    });
-
     const noteInputField = screen.getByPlaceholderText("Add a note here");
-    userEvent.type(noteInputField, "Here is a note");
+    const generalNoteRadio = screen.getByTestId(/general-note-type/i);
 
     await waitFor(() => {
       expect(screen.getByTestId(/save/i)).toBeDisabled();
     });
 
-    userEvent.clear(noteInputField);
+    act(() => {
+      userEvent.type(noteInputField, "Here is a note");
+    });
 
-    const generalNoteRadio = screen.getByTestId(/general-note-type/i);
-    userEvent.click(generalNoteRadio);
+    await waitFor(() => {
+      expect(screen.getByTestId(/save/i)).toBeDisabled();
+    });
+
+    act(() => {
+      userEvent.clear(noteInputField);
+      userEvent.click(generalNoteRadio);
+    });
 
     await waitFor(() => {
       expect(screen.getByTestId(/save/i)).toBeDisabled();
