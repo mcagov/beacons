1e90f8373c3dc2c8e5119d063d5c77cb16c9285b
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-03-01 15:36:25 +0000

    Set the application user to have the readall role
    A backend role in OpenSearch is something from a third-party IDP, e.g.
    SAML.
    
    This commit changes to use a 'normal' role from the internal user
    directory.

--- opensearch/proxy/create-opensearch-user.sh
+++ opensearch/proxy/create-opensearch-user.sh
@@ -16,7 +16,7 @@ then
 
     REQUEST_BODY=$( jq -n \
        --arg password "$APPLICATION_PASSWORD" \
-       '{password: $password, backend_roles: ["readall"]}')
+       '{password: $password, roles: ["readall"]}')
 
     curl -XPUT -u "$credentials" https://"$OPENSEARCH_DOMAIN"/_plugins/_security/api/internalusers/"$APPLICATION_USERNAME" -H 'Content-Type: application/json' \
     -d "$REQUEST_BODY" --retry 10
