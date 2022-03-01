291def30e7913916f7d776337d052ab2af84f41e
Olly Swanson <olly.swanson95@gmail.com>
2022-03-01 12:21:41 +0000

    Change OPENSEARCH_URI to OPENSEARCH_DOMAIN to match changes.

--- opensearch/proxy/create-opensearch-user.sh
+++ opensearch/proxy/create-opensearch-user.sh
@@ -8,7 +8,7 @@ fi
 
 credentials=$MASTER_USER:$MASTER_PASSWORD
 
-res=$(curl -s -o /dev/null -w '%{http_code}\n' -u "$credentials" https://"$OPENSEARCH_URI"/_plugins/_security/api/internalusers/"$APPLICATION_USERNAME" --retry 10)
+res=$(curl -s -o /dev/null -w '%{http_code}\n' -u "$credentials" https://"$OPENSEARCH_DOMAIN"/_plugins/_security/api/internalusers/"$APPLICATION_USERNAME" --retry 10)
 
 if ((res == 404));
 then
@@ -18,7 +18,7 @@ then
        --arg password "$APPLICATION_PASSWORD" \
        '{password: $password, backend_roles: ["readall"]}')
 
-    curl -XPUT -u "$credentials" https://"$OPENSEARCH_URI"/_plugins/_security/api/internalusers/"$APPLICATION_USERNAME" -H 'Content-Type: application/json' \
+    curl -XPUT -u "$credentials" https://"$OPENSEARCH_DOMAIN"/_plugins/_security/api/internalusers/"$APPLICATION_USERNAME" -H 'Content-Type: application/json' \
     -d "$REQUEST_BODY" --retry 10
 else
     echo "User already exists, exiting..."
