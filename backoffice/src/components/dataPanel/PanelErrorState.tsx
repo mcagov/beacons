a4bbd327a5e53c51af27ebb8ba8fb99eaefb1c47
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-03-08 11:39:44 +0000

    Allow Error component to take error message as a child as well as prop
    Co-authored-by: Olly Swanson <olly.swanson95@gmail.com>

--- backoffice/src/components/dataPanel/PanelErrorState.tsx
+++ backoffice/src/components/dataPanel/PanelErrorState.tsx
@@ -3,14 +3,15 @@ import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 import { FunctionComponent } from "react";
 
 interface IPanelError {
-  message: string;
+  message?: string;
 }
 
 export const ErrorState: FunctionComponent<IPanelError> = ({
   message,
+  children,
 }): JSX.Element => (
   <Box role="alert" textAlign="center">
     <ErrorOutlineIcon />
-    <Typography>{message}</Typography>
+    <Typography>{message ?? children}</Typography>
   </Box>
 );
