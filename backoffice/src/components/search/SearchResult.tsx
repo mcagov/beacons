1c18f88ed21726d6dc3bfee31065a4fa2caf0b38
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-03-15 16:59:29 +0000

    Add BeaconStatus to SearchResult
    Co-authored-by: Olly Swanson <42551149+ollyswanson@users.noreply.github.com>

--- backoffice/src/components/search/SearchResult.tsx
+++ backoffice/src/components/search/SearchResult.tsx
@@ -3,57 +3,72 @@ import { Podcasts } from "@mui/icons-material";
 import { Link as RouterLink } from "react-router-dom";
 import { ResultCard } from "@appbaseio/reactivesearch";
 import React from "react";
+import { BeaconStatus } from "../../entities/BeaconStatus";
 
 export const SearchResult = ({ item }: { item: any }): JSX.Element => {
   return (
     <ResultCard key={item._id}>
-      <Chip
-        label={item.hexId}
-        icon={<Podcasts />}
-        component={RouterLink}
-        to={(item.isLegacy ? "/legacy-beacons/" : "/beacons/") + item.id}
-        clickable
-      />
-      <ResultCard.Description>
-        <table style={{ paddingTop: "1rem" }}>
-          <tbody>
-            {exists(item.vesselMmsiNumbers) && (
-              <tr>
-                <th>MMSI number(s):</th>
-                <td>{item.vesselMmsiNumbers}</td>
-              </tr>
-            )}
-            {exists(item.vesselNames) && (
-              <tr>
-                <th>Vessel name(s):</th>
-                <td>{item.vesselNames}</td>
-              </tr>
-            )}
-            {exists(item.vesselCallsigns) && (
-              <tr>
-                <th>Callsign(s):</th>
-                <td>{item.vesselCallsigns}</td>
-              </tr>
-            )}
-            {exists(item.aircraftRegistrationMarks) && (
-              <tr>
-                <th>Aircraft registration mark(s):</th>
-                <td>{item.aircraftRegistrationMarks}</td>
-              </tr>
-            )}
-            {exists(item.aircraft24bitHexAddresses) && (
-              <tr>
-                <th>Aircraft 24-bit hex address(es):</th>
-                <td>{item.aircraft24bitHexAddresses}</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </ResultCard.Description>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: "100%",
+        }}
+      >
+        <Chip
+          label={item.hexId}
+          icon={<Podcasts />}
+          component={RouterLink}
+          to={(item.isLegacy ? "/legacy-beacons/" : "/beacons/") + item.id}
+          clickable
+        />
+        <ResultCard.Description>
+          <table style={{ paddingTop: "1rem" }}>
+            <tbody>
+              {exists(item.vesselMmsiNumbers) && (
+                <tr>
+                  <th>MMSI number(s):</th>
+                  <td>{item.vesselMmsiNumbers}</td>
+                </tr>
+              )}
+              {exists(item.vesselNames) && (
+                <tr>
+                  <th>Vessel name(s):</th>
+                  <td>{item.vesselNames}</td>
+                </tr>
+              )}
+              {exists(item.vesselCallsigns) && (
+                <tr>
+                  <th>Callsign(s):</th>
+                  <td>{item.vesselCallsigns}</td>
+                </tr>
+              )}
+              {exists(item.aircraftRegistrationMarks) && (
+                <tr>
+                  <th>Aircraft registration mark(s):</th>
+                  <td>{item.aircraftRegistrationMarks}</td>
+                </tr>
+              )}
+              {exists(item.aircraft24bitHexAddresses) && (
+                <tr>
+                  <th>Aircraft 24-bit hex address(es):</th>
+                  <td>{item.aircraft24bitHexAddresses}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </ResultCard.Description>
+        <StatusBar status={item.beaconStatus} />
+      </div>
     </ResultCard>
   );
 };
 
 function exists<T>(field: Array<T> | unknown): boolean {
   return Array.isArray(field) && field.length > 0;
+}
+
+function StatusBar({ status }: { status: BeaconStatus }): JSX.Element {
+  return <Chip label={status} size={"small"} />;
 }
---
+++ backoffice/src/entities/BeaconStatus.ts
@@ -0,0 +1,4 @@
+/**
+ * The set of valid statuses a Beacon or LegacyBeacon should have
+ */
+export type BeaconStatus = "NEW" | "MIGRATED" | "CLAIMED" | "DELETED";
