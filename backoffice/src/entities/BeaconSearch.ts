293be56f600d2defdb5e26cfee1e9f507491abc6
Olly Swanson <olly.swanson95@gmail.com>
2022-03-21 10:46:24 +0000

    Parse items returned from reactivesearch into results for rendering.
    Added types for `BeaconSearchItem` that is returned by reactivesearch
    and `BeaconSearchResult` which is the "parsed" item with processing
    such as deduplication and validation of certain fields.

---
+++ backoffice/src/entities/BeaconSearch.ts
@@ -0,0 +1,83 @@
+import { UseEnvironment } from "./UseEnvironment";
+import { BeaconStatus } from "./BeaconStatus";
+
+type DateString = string;
+
+export type BeaconUseDocument = {
+  environment: UseEnvironment | string;
+  activity: string | null;
+  callsign: string | null;
+  purpose: string | null;
+  vesselName: string | null;
+  aircraftRegistrationMark: string | null;
+  mmsi: string | null;
+};
+
+export type BeaconOwnerDocument = {
+  ownerEmail: string;
+  ownerName: string;
+};
+
+export type BeaconSearchItem = {
+  _id: string;
+  hexId: string | null;
+  isLegacy: boolean;
+  beaconStatus: BeaconStatus;
+  createdDate: DateString;
+  lastModifiedDate: DateString;
+  referenceNumber: string | null;
+  vesselMmsiNumbers: Array<string> | unknown;
+  vesselNames: Array<string>;
+  vesselCallsigns: Array<string>;
+  aircraftRegistrationMarks: Array<string>;
+  aircraft24bitHexAddresses: Array<string>;
+  beaconOwner: BeaconOwnerDocument;
+  beaconUses: Array<BeaconUseDocument>;
+};
+
+export type BeaconSearchResult = {
+  _id: string;
+  hexId: string | null;
+  isLegacy: boolean;
+  beaconStatus: BeaconStatus;
+  createdDate: DateString;
+  lastModifiedDate: DateString;
+  referenceNumber: string | null;
+  vesselMmsiNumbers: Array<string>;
+  vesselNames: Array<string>;
+  vesselCallsigns: Array<string>;
+  aircraftRegistrationMarks: Array<string>;
+  aircraft24bitHexAddresses: Array<string>;
+  beaconOwner: BeaconOwnerDocument;
+  beaconUses: Array<BeaconUseDocument>;
+};
+
+export function parseBeaconSearchItem(
+  item: BeaconSearchItem
+): BeaconSearchResult {
+  return {
+    _id: item._id,
+    hexId: item.hexId,
+    isLegacy: item.isLegacy,
+    beaconStatus: item.beaconStatus,
+    createdDate: item.createdDate,
+    lastModifiedDate: item.lastModifiedDate,
+    referenceNumber: item.referenceNumber,
+    vesselNames: processItemArray(item.vesselNames),
+    vesselMmsiNumbers: processItemArray(item.vesselMmsiNumbers),
+    vesselCallsigns: processItemArray(item.vesselCallsigns),
+    aircraftRegistrationMarks: processItemArray(item.aircraftRegistrationMarks),
+    aircraft24bitHexAddresses: processItemArray(item.aircraft24bitHexAddresses),
+    beaconOwner: item.beaconOwner,
+    beaconUses: item.beaconUses,
+  };
+}
+
+// Deduplicates results, removes empty strings, and returns an empty array if not of expected type: Array<string>
+function processItemArray<T extends string>(arr: Array<T> | unknown): Array<T> {
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+
+  return Array.from(new Set(arr.filter((item) => item.length > 0)));
+}
