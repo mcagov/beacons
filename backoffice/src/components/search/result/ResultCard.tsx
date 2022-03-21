51aeff2ced9316056138db9cb7c219fed7ee47aa
Olly Swanson <olly.swanson95@gmail.com>
2022-03-21 10:54:35 +0000

    `ResultCard` component for displaying searchable fields.

---
+++ backoffice/src/components/search/result/ResultCard.tsx
@@ -0,0 +1,50 @@
+import { BeaconSearchResult } from "../../../entities/BeaconSearch";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+} from "@mui/material";
+import { Podcasts } from "@mui/icons-material";
+import { Link as RouterLink } from "react-router-dom";
+import { Environments } from "./Environments";
+import { Uses } from "./Uses";
+import { Owner } from "./Owner";
+
+export function ResultCard({
+  result,
+}: {
+  result: BeaconSearchResult;
+}): JSX.Element {
+  return (
+    <Card sx={{ width: 300 }}>
+      <CardHeader
+        avatar={<Podcasts />}
+        title={result.hexId ?? "N/A"}
+        subheader={result.beaconStatus}
+      />
+      <CardContent>
+        <Environments beaconUses={result.beaconUses} />
+        <Uses
+          vesselMmsiNumbers={result.vesselMmsiNumbers}
+          vesselNames={result.vesselNames}
+          vesselCallsigns={result.vesselCallsigns}
+          aircraftRegistrationMarks={result.aircraftRegistrationMarks}
+          aircraft24bitHexAddresses={result.aircraft24bitHexAddresses}
+        />
+        <Owner beaconOwner={result.beaconOwner} />
+      </CardContent>
+      <CardActions>
+        <Button
+          size={"small"}
+          color={"secondary"}
+          component={RouterLink}
+          to={(result.isLegacy ? "/legacy-beacons/" : "/beacons/") + result._id}
+        >
+          View Registration
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
