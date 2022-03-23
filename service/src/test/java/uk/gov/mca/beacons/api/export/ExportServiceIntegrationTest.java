298fca654984efc5c44477cf2a01fe1e34ba6dda
Olly Swanson <olly.swanson95@gmail.com>
2022-03-23 10:08:14 +0000

    Change delimiter in test CSV reader from ", " to ",".
    There are limitations related to the escaping of delimiters and quotes
    with the Spring Batch implementation of the CSV writer, therefore
    we are only worrying about rows being written, but not about the format
    of the rows until we have the "vertical slice" for CSV exports
    implemented.
    
    Co-authored by: Zack Adlington <54271433+zackads@users.noreply.github.com>

--- service/src/test/java/uk/gov/mca/beacons/api/export/ExportServiceIntegrationTest.java
+++ service/src/test/java/uk/gov/mca/beacons/api/export/ExportServiceIntegrationTest.java
@@ -1,8 +1,7 @@
 package uk.gov.mca.beacons.api.export;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -23,66 +22,94 @@ import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.mca.beacons.api.WebIntegrationTest;
 
 class ExportServiceIntegrationTest extends WebIntegrationTest {
-    private final Path testLocalStorageDirectory;
-    private final ExportService exportService;
 
-    @Autowired
-    public ExportServiceIntegrationTest(JobLauncher jobLauncher, Job exportToSpreadsheetJob) throws IOException {
-        this.testLocalStorageDirectory = Files.createTempDirectory("beacons-export");
-        this.exportService = new ExportService(
-                jobLauncher,
-                exportToSpreadsheetJob,
-                this.testLocalStorageDirectory,
-                new File("beacons_data.csv"));
+  private final Path testLocalStorageDirectory;
+  private final ExportService exportService;
+
+  @Autowired
+  public ExportServiceIntegrationTest(
+    JobLauncher jobLauncher,
+    Job exportToSpreadsheetJob
+  ) throws IOException {
+    this.testLocalStorageDirectory =
+      Files.createTempDirectory("beacons-export");
+    this.exportService =
+      new ExportService(
+        jobLauncher,
+        exportToSpreadsheetJob,
+        this.testLocalStorageDirectory,
+        new File("beacons_data.csv")
+      );
+  }
+
+  @AfterEach
+  void resetFilesystemState() throws IOException {
+    FileUtils.cleanDirectory(testLocalStorageDirectory.toFile());
+  }
+
+  @Test
+  public void givenBeaconsAndLegacyBeacons_whenExportIsTriggered_thenSpreadsheetContainsData()
+    throws Exception {
+    String accountHolderId_1 = seedAccountHolder();
+    String accountHolderId_2 = seedAccountHolder();
+    String id1 = seedRegistration(
+      WebIntegrationTest.RegistrationUseCase.SINGLE_BEACON,
+      accountHolderId_1
+    );
+    String id2 = seedRegistration(
+      WebIntegrationTest.RegistrationUseCase.SINGLE_BEACON,
+      accountHolderId_2
+    );
+    String id3 = seedLegacyBeacon();
+
+    exportService.exportBeaconsToSpreadsheet();
+
+    List<List<String>> spreadsheet = readCsv(
+      exportService.getLatestExcelExport()
+    );
+
+    List<String> headerRow = spreadsheet.get(0);
+    List<List<String>> dataRows = spreadsheet.subList(1, spreadsheet.size());
+    assertThat(headerRow, hasItems("ID"));
+    assertThat(dataRows, hasSize(3));
+    assertThat(
+      getColumnValuesByHeading(spreadsheet, "ID"),
+      containsInAnyOrder(id1, id2, id3)
+    );
+  }
+
+  @Test
+  public void givenBeaconsAndLegacyBeacons_whenExportIsTriggered_thenSpreasheetIsOrderedByLastModifiedDate() {
+    // TODO Look at the last modified date column and assert on ordering
+  }
+
+  private List<List<String>> readCsv(byte[] bytearray) throws IOException {
+    String COMMA_DELIMITER = ",";
+
+    List<List<String>> records = new ArrayList<>();
+    BufferedReader br = new BufferedReader(
+      new StringReader(new String(bytearray))
+    );
+    String line;
+    while ((line = br.readLine()) != null) {
+      String[] values = line.split(COMMA_DELIMITER);
+      records.add(Arrays.asList(values));
     }
 
-    @AfterEach
-    void resetFilesystemState() throws IOException {
-        FileUtils.cleanDirectory(testLocalStorageDirectory.toFile());
-    }
+    return records;
+  }
 
-    @Test
-    public void givenBeaconsAndLegacyBeacons_whenExportIsTriggered_thenSpreadsheetContainsData() throws Exception {
-        String accountHolderId_1 = seedAccountHolder();
-        String accountHolderId_2 = seedAccountHolder();
-        String id1 = seedRegistration(WebIntegrationTest.RegistrationUseCase.SINGLE_BEACON, accountHolderId_1);
-        String id2 = seedRegistration(WebIntegrationTest.RegistrationUseCase.SINGLE_BEACON, accountHolderId_2);
-        String id3 = seedLegacyBeacon();
+  private List<String> getColumnValuesByHeading(
+    List<List<String>> spreadsheet,
+    String heading
+  ) {
+    List<String> headerRow = spreadsheet.get(0);
+    int columnIndex = headerRow.indexOf(heading);
 
-        exportService.exportBeaconsToSpreadsheet();
-
-        List<List<String>> spreadsheet = readCsv(exportService.getLatestExcelExport());
-
-        List<String> headerRow = spreadsheet.get(0);
-        List<List<String>> dataRows = spreadsheet.subList(1, spreadsheet.size());
-        assertThat(headerRow, containsInAnyOrder("ID", "Hex ID", "Owner name"));
-        assertThat(dataRows, hasSize(3));
-        assertThat(getColumnValuesByHeading(spreadsheet, "ID"), containsInAnyOrder(id1, id2, id3));
-    }
-
-    @Test
-    public void givenBeaconsAndLegacyBeacons_whenExportIsTriggered_thenSpreasheetIsOrderedByLastModifiedDate() {
-        // TODO Look at the last modified date column and assert on ordering
-    }
-
-    private List<List<String>> readCsv(byte[] bytearray) throws IOException {
-        String COMMA_DELIMITER = ", ";
-
-        List<List<String>> records = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new StringReader(new String(bytearray)));
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] values = line.split(COMMA_DELIMITER);
-            records.add(Arrays.asList(values));
-        }
-
-        return records;
-    }
-
-    private List<String> getColumnValuesByHeading(List<List<String>> spreadsheet, String heading) {
-        List<String> headerRow = spreadsheet.get(0);
-        int columnIndex = headerRow.indexOf(heading);
-
-        return spreadsheet.stream().skip(1).map((row) -> row.get(columnIndex)).collect(Collectors.toList());
-    }
+    return spreadsheet
+      .stream()
+      .skip(1)
+      .map(row -> row.get(columnIndex))
+      .collect(Collectors.toList());
+  }
 }
