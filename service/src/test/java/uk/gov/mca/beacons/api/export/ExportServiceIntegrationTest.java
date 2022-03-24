d82c1acd80f3b5e8f0d57efdab79ea34d3d8c64c
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-03-24 11:44:33 +0000

    Remove parameterisation of ExportServiceIntegrationTest
    Because a temporary test directory is now created for each test context by the BaseIntegrationTest.
    
    Co-authored-by: Olly Swanson <42551149+ollyswanson@users.noreply.github.com>

--- service/src/test/java/uk/gov/mca/beacons/api/export/ExportServiceIntegrationTest.java
+++ service/src/test/java/uk/gov/mca/beacons/api/export/ExportServiceIntegrationTest.java
@@ -7,50 +7,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import uk.gov.mca.beacons.api.WebIntegrationTest;
 
 class ExportServiceIntegrationTest extends WebIntegrationTest {
-
-  private final Path testLocalStorageDirectory;
-  private final ExportService exportService;
-
   @Autowired
-  public ExportServiceIntegrationTest(
-    JobLauncher jobLauncher,
-    @Qualifier("simpleAsyncJobLauncher") JobLauncher asyncJobLauncher,
-    Job exportToSpreadsheetJob
-  ) throws IOException {
-    this.testLocalStorageDirectory =
-      Files.createTempDirectory("beacons-export");
-    this.exportService =
-      new ExportService(
-        jobLauncher,
-        asyncJobLauncher,
-        exportToSpreadsheetJob,
-        new FileSystemResource(
-          Files.createTempFile("export-service", "integration-test")
-        )
-      );
-  }
-
-  @AfterEach
-  void resetFilesystemState() throws IOException {
-    FileUtils.cleanDirectory(testLocalStorageDirectory.toFile());
-  }
+  private ExportService exportService;
 
   @Test
   public void givenBeaconsAndLegacyBeacons_whenExportIsTriggered_thenSpreadsheetContainsData()
