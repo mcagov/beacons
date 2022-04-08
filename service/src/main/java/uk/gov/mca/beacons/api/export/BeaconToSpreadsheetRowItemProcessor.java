e93bba70de656c34bca4b26244c928e313e5ed62
Olly Swanson <olly.swanson95@gmail.com>
2022-04-08 11:41:47 +0100

    Require new readOnly transactions in the processor step.
    This appears to avoid unnecessary flushing of objects retrieved by
    Hibernate, resulting in a much quicker processing step.

--- service/src/main/java/uk/gov/mca/beacons/api/export/BeaconToSpreadsheetRowItemProcessor.java
+++ service/src/main/java/uk/gov/mca/beacons/api/export/BeaconToSpreadsheetRowItemProcessor.java
@@ -4,45 +4,48 @@ import java.util.List;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.mca.beacons.api.beacon.domain.Beacon;
 import uk.gov.mca.beacons.api.beacon.domain.BeaconId;
 import uk.gov.mca.beacons.api.beaconowner.domain.BeaconOwner;
-import uk.gov.mca.beacons.api.beaconowner.domain.BeaconOwnerRepository;
+import uk.gov.mca.beacons.api.beaconowner.domain.BeaconOwnerReadOnlyRepository;
 import uk.gov.mca.beacons.api.beaconuse.domain.BeaconUse;
-import uk.gov.mca.beacons.api.beaconuse.domain.BeaconUseRepository;
+import uk.gov.mca.beacons.api.beaconuse.domain.BeaconUseReadOnlyRepository;
 import uk.gov.mca.beacons.api.emergencycontact.domain.EmergencyContact;
-import uk.gov.mca.beacons.api.emergencycontact.domain.EmergencyContactRepository;
+import uk.gov.mca.beacons.api.emergencycontact.domain.EmergencyContactReadOnlyRepository;
 import uk.gov.mca.beacons.api.export.SpreadsheetRow;
 
 @Component
 class BeaconToSpreadsheetRowItemProcessor
   implements ItemProcessor<Beacon, SpreadsheetRow> {
 
-  private final BeaconOwnerRepository beaconOwnerRepository;
-  private final BeaconUseRepository beaconUseRepository;
-  private final EmergencyContactRepository emergencyContactRepository;
+  private final BeaconOwnerReadOnlyRepository beaconOwnerRepository;
+  private final BeaconUseReadOnlyRepository beaconUseRepository;
+  private final EmergencyContactReadOnlyRepository emergencyContactRepository;
 
   @Autowired
   public BeaconToSpreadsheetRowItemProcessor(
-    BeaconOwnerRepository beaconOwnerRepository,
-    BeaconUseRepository beaconUseRepository,
-    EmergencyContactRepository emergencyContactRepository
+    BeaconOwnerReadOnlyRepository beaconOwnerRepository,
+    BeaconUseReadOnlyRepository beaconUseRepository,
+    EmergencyContactReadOnlyRepository emergencyContactRepository
   ) {
     this.beaconOwnerRepository = beaconOwnerRepository;
     this.beaconUseRepository = beaconUseRepository;
     this.emergencyContactRepository = emergencyContactRepository;
   }
 
+  @Transactional(propagation = Propagation.REQUIRES_NEW, readOnly = true)
   @Override
   public SpreadsheetRow process(Beacon beacon) {
     BeaconId beaconId = beacon.getId();
     BeaconOwner beaconOwner = beaconOwnerRepository
       .findBeaconOwnerByBeaconId(beaconId)
       .orElse(null);
-    List<BeaconUse> beaconUses = beaconUseRepository.getBeaconUseByBeaconId(
+    List<BeaconUse> beaconUses = beaconUseRepository.findBeaconUsesByBeaconId(
       beaconId
     );
-    List<EmergencyContact> emergencyContacts = emergencyContactRepository.getByBeaconId(
+    List<EmergencyContact> emergencyContacts = emergencyContactRepository.findEmergencyContactsByBeaconId(
       beaconId
     );
 
--- service/src/main/java/uk/gov/mca/beacons/api/search/jobs/steps/ReindexSearchBeaconProcessor.java
+++ service/src/main/java/uk/gov/mca/beacons/api/search/jobs/steps/ReindexSearchBeaconProcessor.java
@@ -4,11 +4,15 @@ import java.util.List;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.mca.beacons.api.beacon.domain.Beacon;
 import uk.gov.mca.beacons.api.beacon.domain.BeaconId;
 import uk.gov.mca.beacons.api.beaconowner.domain.BeaconOwner;
+import uk.gov.mca.beacons.api.beaconowner.domain.BeaconOwnerReadOnlyRepository;
 import uk.gov.mca.beacons.api.beaconowner.domain.BeaconOwnerRepository;
 import uk.gov.mca.beacons.api.beaconuse.domain.BeaconUse;
+import uk.gov.mca.beacons.api.beaconuse.domain.BeaconUseReadOnlyRepository;
 import uk.gov.mca.beacons.api.beaconuse.domain.BeaconUseRepository;
 import uk.gov.mca.beacons.api.search.documents.BeaconSearchDocument;
 
@@ -16,25 +20,26 @@ import uk.gov.mca.beacons.api.search.documents.BeaconSearchDocument;
 public class ReindexSearchBeaconProcessor
   implements ItemProcessor<Beacon, BeaconSearchDocument> {
 
-  private final BeaconOwnerRepository beaconOwnerRepository;
-  private final BeaconUseRepository beaconUseRepository;
+  private final BeaconOwnerReadOnlyRepository beaconOwnerRepository;
+  private final BeaconUseReadOnlyRepository beaconUseRepository;
 
   @Autowired
   public ReindexSearchBeaconProcessor(
-    BeaconOwnerRepository beaconOwnerRepository,
-    BeaconUseRepository beaconUseRepository
+    BeaconOwnerReadOnlyRepository beaconOwnerRepository,
+    BeaconUseReadOnlyRepository beaconUseRepository
   ) {
     this.beaconOwnerRepository = beaconOwnerRepository;
     this.beaconUseRepository = beaconUseRepository;
   }
 
+  @Transactional(propagation = Propagation.REQUIRES_NEW, readOnly = true)
   @Override
   public BeaconSearchDocument process(Beacon beacon) {
     BeaconId beaconId = beacon.getId();
     BeaconOwner beaconOwner = beaconOwnerRepository
       .findBeaconOwnerByBeaconId(beaconId)
       .orElse(null);
-    List<BeaconUse> beaconUses = beaconUseRepository.getBeaconUseByBeaconId(
+    List<BeaconUse> beaconUses = beaconUseRepository.findBeaconUsesByBeaconId(
       beaconId
     );
 
