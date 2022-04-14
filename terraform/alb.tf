aa541e4b2bad953d6da61df669842ec43f27c5e7
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-04-14 16:48:58 +0100

    Use an ssl_policy compatible with ALBs
    See:
    - https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html#tls-security-policies
    
    We chose the ssl_policy that:
    1. Supports TLS1.2 only (no 1.0 or 1.1)
    2. Supports a greater number of ciphers than the other options.
    
    We think this will tred the balance between accessibility and security.

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -30,7 +30,7 @@ resource "aws_alb_listener" "front_end_ssl" {
   load_balancer_arn = aws_alb.main.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-FS-1-2-2019-08"
   certificate_arn   = var.ssl_certificate_arn
 
   default_action {
