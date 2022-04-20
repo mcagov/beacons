1518df90382c9f1ffbc3fe4ee9b5b43a670f2d85
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-04-20 10:56:15 +0100

    Make authenticated POST requests chainable
    Allowing subsequent Cypress actions to take place, e.g. "Create an
    AccountHolder _then_ seed a Beacon".
    
    Co-authored-by: Olly Swanson <42551149+ollyswanson@users.noreply.github.com>

--- tests/cypress/common/make-authenticated-POST-request.spec.ts
+++ tests/cypress/common/make-authenticated-POST-request.spec.ts
@@ -5,11 +5,13 @@
  * @param url Where to POST it
  */
 import RequestBody = Cypress.RequestBody;
+import Response = Cypress.Response;
+import Chainable = Cypress.Chainable;
 
-export const makeAuthenticatedPOSTRequest = async (
+export function makeAuthenticatedPOSTRequest<T>(
   body: RequestBody,
   url: string
-): Promise<void> => {
+): Chainable<Response<T>> {
   const tenant = Cypress.env("AAD_TENANT_ID");
   const authUrl = `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/token`;
   const clientId = Cypress.env("WEBAPP_CLIENT_ID");
@@ -18,34 +20,36 @@ export const makeAuthenticatedPOSTRequest = async (
   const grantType = "client_credentials";
 
   // Retrieve an auth token from Azure AD
-  cy.request({
-    method: "POST",
-    form: true,
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: {
-      client_id: clientId,
-      scope: scope,
-      client_secret: clientSecret,
-      grant_type: grantType,
-    },
-    url: `${authUrl}`,
-  }).then(
-    // POST body to URL using the retrieved auth token
-    (response: {
+  return cy
+    .request({
+      method: "POST",
+      form: true,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: {
-        access_token: string;
-        token_type: string;
-        expires_in: number;
-      };
-    }) => {
-      cy.request({
-        method: "POST",
-        auth: {
-          bearer: response.body.access_token,
-        },
-        url,
-        body,
-      });
-    }
-  );
-};
+        client_id: clientId,
+        scope: scope,
+        client_secret: clientSecret,
+        grant_type: grantType,
+      },
+      url: `${authUrl}`,
+    })
+    .then(
+      // POST body to URL using the retrieved auth token
+      (response: {
+        body: {
+          access_token: string;
+          token_type: string;
+          expires_in: number;
+        };
+      }) => {
+        cy.request({
+          method: "POST",
+          auth: {
+            bearer: response.body.access_token,
+          },
+          url,
+          body,
+        });
+      }
+    );
+}
