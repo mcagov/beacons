5b8d17c0170d8adf2f6bb7c5a2b738694f0b0804
Zack Adlington <54271433+zackads@users.noreply.github.com>
2022-04-27 15:28:38 +0100

    Add "refresh" button to error message
    We intend that this prompts the user to self-recover from error if
    error-checking in code fails.
    
    Co-authored-by: Olly Swanson <42551149+ollyswanson@users.noreply.github.com>

--- backoffice/src/components/dataPanel/PanelErrorState.tsx
+++ backoffice/src/components/dataPanel/PanelErrorState.tsx
@@ -1,5 +1,5 @@
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { FunctionComponent } from "react";
 
 interface IPanelError {
@@ -13,5 +13,12 @@ export const ErrorState: FunctionComponent<IPanelError> = ({
   <Box role="alert" textAlign="center">
     <ErrorOutlineIcon />
     <Typography>{message ?? children}</Typography>
+    <Button
+      onClick={() => {
+        window.location.reload();
+      }}
+    >
+      Click here to refresh the page
+    </Button>
   </Box>
 );
