ffc3ac2792c8ba88734412816a48e58b84d9c0c6
Olly Swanson <olly.swanson95@gmail.com>
2022-04-27 14:17:35 +0100

    Add failures + successes to same chart for improved comparison.
    Co-authored by: Zack Adlington <54271433+zackads@users.noreply.github.com>

--- terraform/dashboard_application.tf
+++ terraform/dashboard_application.tf
@@ -3,78 +3,45 @@ resource "aws_cloudwatch_dashboard" "application_health" {
 
   dashboard_body = <<-EOT
 {
-    "widgets": [
-        {
-            "type": "log",
-            "x": 0,
-            "y": 2,
-            "width": 24,
-            "height": 6,
-            "properties": {
-                "query": "SOURCE '/${terraform.workspace}/mca/beacons' | fields @timestamp, @message\n| sort @timestamp desc\n| filter @message like /Failed to register/",
-                "region": "${data.aws_region.current.name}",
-                "stacked": false,
-                "view": "table"
-            }
-        },
-        {
-            "type": "text",
-            "x": 0,
-            "y": 0,
-            "width": 24,
-            "height": 1,
-            "properties": {
-                "markdown": "# Registrations \n"
-            }
-        },
-        {
-            "type": "text",
-            "x": 0,
-            "y": 1,
-            "width": 24,
-            "height": 1,
-            "properties": {
-                "markdown": "## Failed registrations"
-            }
-        },
-        {
-            "type": "log",
-            "x": 0,
-            "y": 8,
-            "width": 24,
-            "height": 6,
-            "properties": {
-                "query": "SOURCE '/${terraform.workspace}/mca/beacons' | fields @timestamp, @message\n| sort @timestamp desc\n| fields strcontains(@message, \"Failed to register\") as failed_to_register\n| stats sum(failed_to_register) as failures_to_register by bin(1d) as day\n| sort day asc\n",
-                "region": "${data.aws_region.current.name}",
-                "stacked": false,
-                "title": "Failed registrations per day",
-                "view": "bar"
-            }
-        },
-        {
-            "type": "text",
-            "x": 0,
-            "y": 14,
-            "width": 24,
-            "height": 1,
-            "properties": {
-                "markdown": "## Successful registrations"
-            }
-        },
-        {
-            "type": "log",
-            "x": 0,
-            "y": 15,
-            "width": 24,
-            "height": 6,
-            "properties": {
-                "query": "SOURCE '/${terraform.workspace}/mca/beacons' | fields @timestamp, @message\n| fields strcontains(@message, \"Registration sent\") as registration_sent\n| stats sum(registration_sent) as registrations_sent by bin(1d) as day\n| sort day asc\n",
-                "region": "${data.aws_region.current.name}",
-                "title": "Successful registrations per day",
-                "view": "bar"
-            }
-        }
-    ]
+  "widgets": [
+      {
+          "height": 6,
+          "width": 24,
+          "y": 2,
+          "x": 0,
+          "type": "log",
+          "properties": {
+              "query": "SOURCE '/${terraform.workspace}/mca/beacons' | fields @timestamp, @message\n| sort @timestamp desc\n| filter @message like /Failed to register/",
+              "region": "${data.aws_region.current.name}",
+              "stacked": false,
+              "view": "table"
+          }
+      },
+      {
+          "height": 2,
+          "width": 24,
+          "y": 0,
+          "x": 0,
+          "type": "text",
+          "properties": {
+              "markdown": "# Attempted Registrations\n\nAn \"attempt\" to register a beacon is defined as the attempt to persist a registration at the moment the user confirms that they have completed the registration form and clicks \"Accept and send\". This therefore does not track failures earlier in the user journey such as \"failure to create an account.\""
+          }
+      },
+      {
+          "type": "log",
+          "x": 0,
+          "y": 8,
+          "width": 24,
+          "height": 6,
+          "properties": {
+              "query": "SOURCE '/${terraform.workspace}/mca/beacons' | fields @timestamp, @message\n| fields strcontains(@message, \"Registration sent\") as registration_succeeded\n| fields strcontains(@message, \"Failed to register\") as registration_failed\n| stats sum(registration_succeeded) as registrations_succeeded, sum(registration_failed) as registrations_failed by bin(1d) as day\n| sort day asc",
+              "region": "${data.aws_region.current.name}",
+              "stacked": false,
+              "title": "Attempted registrations per day",
+              "view": "bar"
+          }
+      }
+  ]
 }
 EOT
 }
