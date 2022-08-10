3cf9b7146e1a06c4b302ad1137ce85e7c60eeb4b
Evie Skinner <evie.skinner@madetech.com>
2022-08-10 14:17:39 +0100

    If B2C down, /service-health will still return a 200. Just the message will say what's wrong

--- webapp/src/pages/api/service-health.ts
+++ webapp/src/pages/api/service-health.ts
@@ -1,12 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { AadAuthGateway } from "../../../gateways/AadAuthGateway";
+import { AadAuthGateway } from "../../gateways/AadAuthGateway";
 import { IServiceHealthResponse } from "./IServiceHealthResponse";
 
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-  res.status(200).json({ status: "Ship shape and Bristol fashion" });
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  const serviceHealthResponse = await getB2CServiceHealthResponse();
+  res
+    .status(serviceHealthResponse.statusCode)
+    .json({ status: serviceHealthResponse.message });
 };
 
-// use this function above
 export async function getB2CServiceHealthResponse(): Promise<IServiceHealthResponse> {
   const serviceHealthResponse: IServiceHealthResponse = {
     statusCode: 200,
@@ -15,9 +20,8 @@ export async function getB2CServiceHealthResponse(): Promise<IServiceHealthRespo
   const azureAdAuthGateway = new AadAuthGateway();
   const signedInAccounts = await azureAdAuthGateway.getSignedInAccounts();
 
-  if (!signedInAccounts.length) {
-    serviceHealthResponse.statusCode = 401;
-    serviceHealthResponse.message = "Azure B2C is not responding";
+  if (signedInAccounts.length === null) {
+    serviceHealthResponse.message = "Azure B2C login is not responding";
   }
 
   return serviceHealthResponse;
