bee432caf5f1895067a23538d8b53f1c401d479f
Sam Kendell <sam.kendell@madetech.com>
2022-09-05 12:55:23 +0100

    Added new error page, refactoring

--- webapp/src/gateways/B2CGateway.ts
+++ webapp/src/gateways/B2CGateway.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import logger from "../logger";
 
 export class B2CGateway {
-  private readonly tenantName = process.env.AZURE_B2C_TENANT_NAME + "error";
+  private readonly tenantName = process.env.AZURE_B2C_TENANT_NAME;
   private readonly loginFlow = process.env.AZURE_B2C_LOGIN_FLOW;
   private readonly clientId = process.env.AZURE_B2C_CLIENT_ID;
   private readonly loginUrl = `https://${this.tenantName}.b2clogin.com/${this.tenantName}.onmicrosoft.com/${this.loginFlow}/oauth2/v2.0/authorize?client_id=${this.clientId}`;
