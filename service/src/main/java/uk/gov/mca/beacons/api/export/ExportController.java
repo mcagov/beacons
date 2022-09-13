68e3d38e156da95f20925f57adb67ee5923bd176
Sam Kendell <sam.kendell@madetech.com>
2022-09-13 12:38:33 +0100

    cert data endpoint

--- service/src/main/java/uk/gov/mca/beacons/api/export/ExportController.java
+++ service/src/main/java/uk/gov/mca/beacons/api/export/ExportController.java
@@ -119,6 +119,29 @@ class ExportController {
     return servePdf(file, "Letter.pdf");
   }
 
+  @GetMapping(value = "/certificate/data/{uuid}")
+  public ResponseEntity<Map<String, Object>> getCertificateDataByBeaconId(
+    @PathVariable("uuid") UUID rawBeaconId
+  ) throws Exception {
+    BeaconId beaconId = new BeaconId(rawBeaconId);
+    Registration registration = registrationService.getByBeaconId(beaconId);
+
+    if (registration == null) {
+      throw new ResourceNotFoundException();
+    }
+
+    Map<String, Object> data = registrationService.getCertificateData(
+      registration
+    );
+
+    data.put("contactNumber", contantNumber);
+
+    return ResponseEntity
+      .ok()
+      .contentType(MediaType.APPLICATION_JSON)
+      .body(data);
+  }
+
   @GetMapping(value = "/certificate/{uuid}")
   public ResponseEntity<byte[]> getCertificateByBeaconId(
     @PathVariable("uuid") UUID rawBeaconId
