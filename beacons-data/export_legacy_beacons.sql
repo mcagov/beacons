86987d13bf15ff82bb5a06f0dada5aa8975a6b2d
Evie Skinner <evie.skinner@madetech.com>
2022-11-14 17:20:16 +0000

    Joining to legacy_beacon_claim_event table in order to return deleted (claimed) statuses

--- beacons-data/export_legacy_beacons.sql
+++ beacons-data/export_legacy_beacons.sql
@@ -1,18 +1,23 @@
 SELECT 
-	hex_id, 
-	UPPER(data -> 'beacon' ->> 'departRefId')AS dept_ref, 
-	UPPER(beacon_status) AS beacon_status,
-	TO_CHAR(TO_TIMESTAMP(data -> 'beacon' ->> 'firstRegistrationDate', 'YYYY-MM-DDXHH24:MI:SS.MS'), 'dd/mm/yyyy') AS created_date, 
-	TO_CHAR(last_modified_date :: DATE, 'dd/mm/yyyy') AS last_modified_date,
-	UPPER(data -> 'owner' ->> 'ownerName')AS owner_full_name,
-	UPPER(data -> 'owner' ->> 'email')AS owner_email,
-	UPPER(data -> 'owner' ->> 'address1')AS owner_address_line1,
-	UPPER(data -> 'owner' ->> 'address2')AS owner_address_line2,
-	UPPER(data -> 'owner' ->> 'address3')AS owner_address_line3,
-	UPPER(data -> 'owner' ->> 'address4')AS owner_address_line4,
-	UPPER(data -> 'owner' ->> 'postCode')AS owner_postcode,
-	UPPER(data -> 'owner' ->> 'county')AS owner_county,
-	UPPER(data -> 'owner' ->> 'townOrCity')AS owner_town_or_city,
-	UPPER(data -> 'owner' ->> 'country')AS owner_country
+	legacy_beacon.hex_id, 
+	UPPER(legacy_beacon.data -> 'beacon' ->> 'departRefId')AS dept_ref, 
+	CASE 
+		WHEN legacy_beacon_claim_event.claim_event_type IS NOT NULL THEN 'DELETED (CLAIMED)' 
+		ELSE legacy_beacon.beacon_status 
+	END as beacon_status,
+	TO_CHAR(TO_TIMESTAMP(legacy_beacon.data -> 'beacon' ->> 'firstRegistrationDate', 'YYYY-MM-DDXHH24:MI:SS.MS'), 'dd/mm/yyyy') AS created_date, 
+	TO_CHAR(legacy_beacon.last_modified_date :: DATE, 'dd/mm/yyyy') AS last_modified_date,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'ownerName')AS owner_full_name,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'email')AS owner_email,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'address1')AS owner_address_line1,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'address2')AS owner_address_line2,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'address3')AS owner_address_line3,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'address4')AS owner_address_line4,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'postCode')AS owner_postcode,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'county')AS owner_county,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'townOrCity')AS owner_town_or_city,
+	UPPER(legacy_beacon.data -> 'owner' ->> 'country')AS owner_country,
+	legacy_beacon_claim_event.claim_event_type
 FROM legacy_beacon
+LEFT JOIN legacy_beacon_claim_event ON legacy_beacon_claim_event.legacy_beacon_id = legacy_beacon.id
 WHERE last_modified_date >= '2013/01/01';
