d0b0cec606d5c0042558c3986e27bc3ab9e5d393
Evie Skinner <evie.skinner@madetech.com>
2022-11-30 14:49:32 +0000

    Removed legacy beacon tests for now

--- service/src/test/java/uk/gov/mca/beacons/api/legacybeacon/application/LegacyBeaconServiceIntegrationTest.java
+++ service/src/test/java/uk/gov/mca/beacons/api/legacybeacon/application/LegacyBeaconServiceIntegrationTest.java
@@ -31,66 +31,6 @@ public class LegacyBeaconServiceIntegrationTest extends BaseIntegrationTest {
     assert claimedLegacyBeacons.get(0).isClaimed();
   }
 
-  @Test
-  void delete_shouldMarkLegacyBeaconStatusAsDeleted() throws Exception {
-    final String hexId = "9D0E1D1B8C00001";
-    final String email = "ownerbeacon@beacons.com";
-    LegacyBeacon legacyBeacon = initLegacyBeacon();
-    legacyBeaconService.create(legacyBeacon);
-
-    List<LegacyBeacon> deletedLegacyBeacons = legacyBeaconService.delete(
-      hexId,
-      email,
-      "The Beacon Registry Team deleted the record with reason: 'Duplicate'"
-    );
-
-    assert deletedLegacyBeacons.get(0).getBeaconStatus() == "DELETED";
-  }
-
-  @Test
-  void delete_shouldRemoveAllTheOwnersPersonallyIdentifiableInformation()
-    throws Exception {
-    final String hexId = "9D0E1D1B8C00001";
-    final String email = "ownerbeacon@beacons.com";
-    LegacyBeacon legacyBeacon = initLegacyBeacon();
-    legacyBeaconService.create(legacyBeacon);
-
-    List<LegacyBeacon> deletedLegacyBeacons = legacyBeaconService.delete(
-      hexId,
-      email,
-      "The Beacon Registry Team deleted the record with reason: 'Destroyed'"
-    );
-
-    LegacyBeacon deletedLegacyBeacon = deletedLegacyBeacons.get(0);
-    LegacyOwner ownerData = legacyBeacon.getData().getOwner();
-
-    assert ownerData.getAddress1() == null;
-    assert ownerData.getOwnerName() == null;
-    assert ownerData.getEmail() == null;
-  }
-
-  @Test
-  void delete_shouldMarkTheBeaconAsWithdrawnAndAddTheDeletionReasonAsAWithdrawnReason()
-    throws Exception {
-    final String hexId = "9D0E1D1B8C00001";
-    final String email = "ownerbeacon@beacons.com";
-    LegacyBeacon legacyBeacon = initLegacyBeacon();
-    legacyBeaconService.create(legacyBeacon);
-
-    List<LegacyBeacon> deletedLegacyBeacons = legacyBeaconService.delete(
-      hexId,
-      email,
-      "The Beacon Registry Team deleted the record with reason: 'Incorrectly registered'"
-    );
-
-    LegacyBeacon deletedLegacyBeacon = deletedLegacyBeacons.get(0);
-    LegacyBeaconDetails beaconDetails = legacyBeacon.getData().getBeacon();
-
-    assert beaconDetails.getIsWithdrawn() == "Y";
-    assert beaconDetails.getWithdrawnReason() ==
-    "The Beacon Registry Team deleted the record with reason: 'Incorrectly registered'";
-  }
-
   private LegacyBeacon initLegacyBeacon() throws Exception {
     LegacyBeacon legacyBeacon = new LegacyBeacon();
     LegacyData legacyData = getLegacyBeaconData();
