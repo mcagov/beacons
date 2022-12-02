217f93ab065abe05549c1041032f583913e86f8f
Evie Skinner <evie.skinner@madetech.com>
2022-12-02 16:11:59 +0000

    Joined legacy_beacon to claim event table to return the correct status for legacy beacons in the FOI script. Updated the export script accordingly for legacy

--- beacons-data/foi_request_legacy_beacons.sql
+++ beacons-data/foi_request_legacy_beacons.sql
@@ -1,12 +1,11 @@
 SELECT 
-	hex_id,
-	UPPER(beacon_status) AS beacon_status,
-	TO_CHAR(TO_TIMESTAMP(data -> 'beacon' ->> 'firstRegistrationDate', 'YYYY-MM-DDXHH24:MI:SS.MS'), 'dd/mm/yyyy') AS date_first_registered, 
-	TO_CHAR(last_modified_date :: DATE, 'dd/mm/yyyy') AS date_last_modified,
-	UPPER(data -> 'beacon' ->> 'beaconType') AS beacon_type,
-	UPPER(data -> 'beacon' ->> 'mti') AS mti,
-	UPPER(data -> 'beacon' ->> 'manufacturer') AS manufacturer,
-	UPPER(data -> 'beacon' ->> 'model') AS model,
-	COUNT(id)
-FROM legacy_beacon
-	GROUP BY hex_id, beacon_status, date_first_registered, date_last_modified, beacon_type, mti, manufacturer, model;
+	legacy_beacon.hex_id,
+	COALESCE(UPPER('DELETED (' || claim_event_type::text || 'ED)'), UPPER(legacy_beacon.beacon_status)) AS beacon_status,
+	TO_CHAR(TO_TIMESTAMP(legacy_beacon.data -> 'beacon' ->> 'firstRegistrationDate', 'YYYY-MM-DDXHH24:MI:SS.MS'), 'dd/mm/yyyy') AS date_first_registered, 
+	TO_CHAR(legacy_beacon.last_modified_date :: DATE, 'dd/mm/yyyy') AS date_last_modified,
+	UPPER(legacy_beacon.data -> 'beacon' ->> 'beaconType') AS beacon_type,
+	UPPER(legacy_beacon.data -> 'beacon' ->> 'mti') AS mti,
+	UPPER(legacy_beacon.data -> 'beacon' ->> 'manufacturer') AS manufacturer,
+	UPPER(legacy_beacon.data -> 'beacon' ->> 'model') AS model
+FROM legacy_beacon_claim_event
+RIGHT JOIN legacy_beacon ON legacy_beacon_claim_event.legacy_beacon_id = legacy_beacon.id;
--- beacons-data/foi_request_modern_beacons.sql
+++ beacons-data/foi_request_modern_beacons.sql
@@ -2,7 +2,7 @@ SELECT
 	hex_id, 
     UPPER(beacon_status) AS beacon_status, 
     TO_CHAR(created_date :: DATE, 'dd/mm/yyyy') AS date_first_registered,
-    TO_CHAR(beacon.last_modified_date :: DATE, 'dd/mm/yyyy') AS date_lASt_modified,
+    TO_CHAR(beacon.last_modified_date :: DATE, 'dd/mm/yyyy') AS date_last_modified,
 	UPPER(beacon_type) AS beacon_type,
 	UPPER(mti) AS mti,
 	UPPER(manufacturer) AS manufacturer,
