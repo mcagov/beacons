a07c9e85657aa7917f3c931df15c608d5619703b
Evie Skinner <evie.skinner@madetech.com>
2022-12-15 15:00:59 +0000

    Re-added old getBeaconStatus() method. Added unit tests. Kept new version of claim() which updates the beaconStatus of a legacy beacon to CLAIMED in the DB

--- service/src/main/java/uk/gov/mca/beacons/api/legacybeacon/domain/LegacyBeacon.java
+++ service/src/main/java/uk/gov/mca/beacons/api/legacybeacon/domain/LegacyBeacon.java
@@ -45,6 +45,7 @@ public class LegacyBeacon extends BaseAggregateRoot<LegacyBeaconId> {
   private String ownerEmail;
 
   @Setter
+  @Getter
   private String beaconStatus;
 
   @Setter
@@ -74,7 +75,6 @@ public class LegacyBeacon extends BaseAggregateRoot<LegacyBeaconId> {
   @NotNull
   private OffsetDateTime lastModifiedDate;
 
-  // delete story
   public boolean isClaimed() {
     if (actions == null) {
       return false;
@@ -93,18 +93,6 @@ public class LegacyBeacon extends BaseAggregateRoot<LegacyBeaconId> {
     }
   }
 
-  // before delete story
-
-  public void claim() {
-    initActions();
-    if (!isClaimed()) {
-      LegacyBeaconClaimAction claimAction = new LegacyBeaconClaimAction();
-      actions.add(claimAction);
-      this.registerEvent(new LegacyBeaconClaimed(this));
-    }
-  }
-
-  // reinstate this and remove the default getter from beaconStatus
   public String getBeaconStatus() {
     if (isClaimed()) {
       return "CLAIMED";
