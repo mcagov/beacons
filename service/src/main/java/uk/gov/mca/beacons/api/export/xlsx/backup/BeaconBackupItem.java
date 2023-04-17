9b91f1ce760605d09ece96af43cf3e2eb6f5a135
Evie Skinner <evie.skinner@madetech.com>
2023-04-17 10:14:53 +0100

    Readd modern beacon specific fields to BeaconBackupItem

--- service/src/main/java/uk/gov/mca/beacons/api/export/xlsx/backup/BeaconBackupItem.java
+++ service/src/main/java/uk/gov/mca/beacons/api/export/xlsx/backup/BeaconBackupItem.java
@@ -1,5 +1,6 @@
 package uk.gov.mca.beacons.api.export.xlsx.backup;
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 import javax.persistence.*;
@@ -10,6 +11,7 @@ import org.hibernate.annotations.Type;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import uk.gov.mca.beacons.api.accountholder.domain.AccountHolderId;
 import uk.gov.mca.beacons.api.legacybeacon.domain.LegacyData;
 
 @Getter
@@ -53,6 +55,50 @@ public class BeaconBackupItem {
   private OffsetDateTime lastModifiedDate;
 
   // modern beacon fields
+  @Setter
+  @NotNull
+  private String manufacturer;
+
+  @Setter
+  @NotNull
+  private String model;
+
+  @Setter
+  @NotNull
+  private String manufacturerSerialNumber;
+
+  @Setter
+  private String referenceNumber;
+
+  @Setter
+  private AccountHolderId accountHolderId;
+
+  @Setter
+  private String chkCode;
+
+  @Setter
+  private LocalDate batteryExpiryDate;
+
+  @Setter
+  private LocalDate lastServicedDate;
+
+  @Setter
+  private String mti;
+
+  @Setter
+  private Boolean svdr;
+
+  @Setter
+  private String csta;
+
+  @Setter
+  private String beaconType;
+
+  @Setter
+  private String protocol;
+
+  @Setter
+  private String coding;
 
   // legacy beacon fields
   @Setter
