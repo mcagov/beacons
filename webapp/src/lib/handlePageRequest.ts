a486f83849ad811ea6c0a11a9c475d34dc531855
Sam <sam.brown@madetech.com>
2023-09-08 16:50:50 +0100

    Hotfix 2 for Second Generation

--- webapp/src/lib/handlePageRequest.ts
+++ webapp/src/lib/handlePageRequest.ts
@@ -2,10 +2,7 @@ import { DraftRegistration } from "../entities/DraftRegistration";
 import { FormSubmission } from "../presenters/formSubmission";
 import { FormJSON, FormManager } from "./form/FormManager";
 
-export type FormManagerFactory = (
-  formData: FormSubmission,
-  secondGeneration: boolean
-) => FormManager;
+export type FormManagerFactory = (formData: FormSubmission) => FormManager;
 
 export interface DraftRegistrationPageProps {
   form: FormJSON;
