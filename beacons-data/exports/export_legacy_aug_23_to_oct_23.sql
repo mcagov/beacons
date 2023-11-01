1e5cd71db7ec68685e3e1e4ef5507a820f9d8ec6
Sam Kendell <sam.kendell@madetech.com>
2023-11-01 16:51:30 +0000

    updated script

--- beacons-data/exports/export_legacy_aug_23_to_oct_23.sql
+++ beacons-data/exports/export_legacy_aug_23_to_oct_23.sql
@@ -1,8 +1,8 @@
-SELECT 
-	legacy_beacon.hex_id, 
-	UPPER(legacy_beacon.data -> 'beacon' ->> 'departRefId')AS dept_ref, 
+SELECT
+	legacy_beacon.hex_id,
+	UPPER(legacy_beacon.data -> 'beacon' ->> 'departRefId')AS dept_ref,
 	COALESCE(UPPER('DELETED (' || claim_event_type::text || 'ED)'), UPPER(legacy_beacon.beacon_status)) AS beacon_status,
-	TO_CHAR(TO_TIMESTAMP(legacy_beacon.data -> 'beacon' ->> 'firstRegistrationDate', 'YYYY-MM-DDXHH24:MI:SS.MS'), 'dd/mm/yyyy') AS created_date, 
+	TO_CHAR(TO_TIMESTAMP(legacy_beacon.data -> 'beacon' ->> 'firstRegistrationDate', 'YYYY-MM-DDXHH24:MI:SS.MS'), 'dd/mm/yyyy') AS created_date,
 	TO_CHAR(legacy_beacon.last_modified_date :: DATE, 'dd/mm/yyyy') AS last_modified_date,
 	UPPER(legacy_beacon.data -> 'owner' ->> 'ownerName')AS owner_full_name,
 	UPPER(legacy_beacon.data -> 'owner' ->> 'email')AS owner_email,
@@ -16,4 +16,4 @@ SELECT
 	UPPER(legacy_beacon.data -> 'owner' ->> 'country')AS owner_country
 FROM legacy_beacon_claim_event
 RIGHT JOIN legacy_beacon ON legacy_beacon_claim_event.legacy_beacon_id = legacy_beacon.id
-WHERE last_modified_date >= '2023/05/23';
+WHERE last_modified_date >= '2023/07/31' AND last_modified_date < '2023/11/01';
