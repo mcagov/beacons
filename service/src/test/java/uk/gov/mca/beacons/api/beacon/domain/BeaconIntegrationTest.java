4f7e66f952451cadc9d4eaecaa1e99e3670f0352
Sam <sam.brown@madetech.com>
2023-11-27 15:29:26 +0000

    add Update beacon test'

--- service/src/test/java/uk/gov/mca/beacons/api/beacon/domain/BeaconIntegrationTest.java
+++ service/src/test/java/uk/gov/mca/beacons/api/beacon/domain/BeaconIntegrationTest.java
@@ -1,12 +1,15 @@
 package uk.gov.mca.beacons.api.beacon.domain;
 
+import java.util.Optional;
 import java.util.UUID;
+import javax.validation.constraints.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.mca.beacons.api.BaseIntegrationTest;
 import uk.gov.mca.beacons.api.accountholder.domain.AccountHolder;
 import uk.gov.mca.beacons.api.accountholder.domain.AccountHolderId;
 import uk.gov.mca.beacons.api.accountholder.domain.AccountHolderRepository;
+import uk.gov.mca.beacons.api.beacon.application.BeaconService;
 
 public class BeaconIntegrationTest extends BaseIntegrationTest {
 
@@ -16,18 +19,15 @@ public class BeaconIntegrationTest extends BaseIntegrationTest {
   @Autowired
   BeaconRepository beaconRepository;
 
+  @Autowired
+  BeaconService beaconService;
+
   @Test
   public void shouldSaveBeacon() {
     // setup
     AccountHolderId accountHolderId = createAccountHolder();
 
-    Beacon beacon = new Beacon();
-    beacon.setBeaconType("SSAS");
-    beacon.setBeaconStatus(BeaconStatus.NEW);
-    beacon.setHexId("1D1234123412345");
-    beacon.setManufacturer("Test Manufacturer");
-    beacon.setModel("Test model");
-    beacon.setManufacturerSerialNumber("Test serial number");
+    Beacon beacon = generateBeacon();
     beacon.setAccountHolderId(accountHolderId);
 
     // act
@@ -37,6 +37,27 @@ public class BeaconIntegrationTest extends BaseIntegrationTest {
     assert savedBeacon.getId() != null;
     assert savedBeacon.getCreatedDate() != null;
     assert savedBeacon.getLastModifiedDate() != null;
+  }
+
+  @Test
+  public void shouldUpdateBeaconStatusToChange() {
+    // setup
+    AccountHolderId accountHolderId = createAccountHolder();
+
+    Beacon beacon = generateBeacon();
+    beacon.setAccountHolderId(accountHolderId);
+
+    // act
+    Beacon savedBeacon = beaconRepository.save(beacon);
+    assert savedBeacon.getBeaconStatus() == BeaconStatus.NEW;
+    savedBeacon.setModel("Different");
+    beaconService.update(savedBeacon.getId(), savedBeacon);
+    Beacon retrievedBeacon = beaconService
+      .findById(savedBeacon.getId())
+      .orElse(null);
+    //assert
+    assert retrievedBeacon != null;
+    assert retrievedBeacon.getBeaconStatus() == BeaconStatus.CHANGE;
   }
 
   private AccountHolderId createAccountHolder() {
@@ -49,5 +70,17 @@ public class BeaconIntegrationTest extends BaseIntegrationTest {
     );
 
     return savedAccountHolder.getId();
+  }
+
+  @NotNull
+  private static Beacon generateBeacon() {
+    Beacon beacon = new Beacon();
+    beacon.setBeaconType("SSAS");
+    beacon.setBeaconStatus(BeaconStatus.NEW);
+    beacon.setHexId("1D1234123412345");
+    beacon.setManufacturer("Test Manufacturer");
+    beacon.setModel("Test model");
+    beacon.setManufacturerSerialNumber("Test serial number");
+    return beacon;
   }
 }
