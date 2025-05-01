afb9a459d0490f98c1870c286c109ca3777b6bd7
Ahmad Vegah <ahmadvegah@gmail.com>
2025-05-01 10:49:06 +0100

    Revert "Merge pull request #778 from WillGibson/move-tests-to-enable-mutation-testing"
    This reverts commit 92cc79c8382c7a56a6ef08682db27247d263c53f, reversing
    changes made to 43b8a8b3e6ef34c7d8f4acaaf379328428af9b64.
    
    # Conflicts:
    #	webapp/test/lib/registration/DeprecatedRegistration.spec.ts

---
+++ webapp/test/components/domain/CountrySelect.test.tsx
@@ -0,0 +1,61 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { CountrySelect } from "../../../src/components/domain/formElements/CountrySelect";
+import countriesJson from "../../../src/lib/countries/countries.json";
+
+describe("CountrySelect Component", () => {
+  it("renders without errors", () => {
+    render(
+      <CountrySelect
+        id="country"
+        name="country"
+        defaultValue="Wales, United Kingdom"
+      />
+    );
+    const countrySelect = screen.getByRole("combobox");
+    expect(countrySelect).toBeInTheDocument();
+  });
+
+  it("has the correct default value selected", () => {
+    render(
+      <CountrySelect
+        id="country"
+        name="country"
+        defaultValue="Wales, United Kingdom"
+      />
+    );
+    const countrySelect = screen.getByRole("combobox");
+    expect(countrySelect).toHaveValue("Wales, United Kingdom");
+  });
+
+  it("displays the correct country options", () => {
+    render(
+      <CountrySelect
+        id="country"
+        name="country"
+        defaultValue="Wales, United Kingdom"
+      />
+    );
+    countriesJson.forEach((country) => {
+      const countryOption = screen.getByText(country);
+      expect(countryOption).toBeInTheDocument();
+    });
+  });
+
+  it("updates selected value on change", () => {
+    render(
+      <CountrySelect
+        id="country"
+        name="country"
+        defaultValue="Wales, United Kingdom"
+      />
+    );
+    const countrySelect = screen.getByRole("combobox");
+    fireEvent.change(countrySelect, { target: { value: "Germany" } });
+    expect(countrySelect).toHaveValue("Germany");
+  });
+});
