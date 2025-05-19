5487275ab800a106baf3aa671c500d23723e8775
andiempett <andi.empett@madetech.com>
2025-05-19 11:36:20 +0100

    WIP backoffice migrate CRA to vite migrating to vite from react-scripts fixes for vulnerabilities and a newer supported tool

---
+++ backoffice/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr(), tsconfigPaths()],
+  server: {
+    open: true,
+    port: 3001,
+    proxy: {
+      "/backoffice": "http://localhost:3005",
+    },
+  },
+  define: {
+    "process.env": {},
+  },
+});
