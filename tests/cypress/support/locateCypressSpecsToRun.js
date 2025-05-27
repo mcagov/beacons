a374fffb12a44d0263d3fb02c70670bb0961e494
Ahmad Vegah <ahmadvegah@gmail.com>
2025-05-27 12:15:22 +0100

    chore: Run Cypress Tests in Parallel in Github Actions
    Our end2end tests run sequentially, and we also run these tests mutiple times (on push, push to dev and push to staging). This will hopefully speed up continuous integration and deployment.

---
+++ tests/cypress/support/locateCypressSpecsToRun.js
@@ -0,0 +1,17 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const getAllFiles = (dirPath, arrayOfFiles = []) => {
+  const files = fs.readdirSync(dirPath, { withFileTypes: true });
+
+  files.forEach((file) => {
+    if (file.isDirectory()) {
+      arrayOfFiles = getAllFiles(`${dirPath}/${file.name}`, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(`${path.join(dirPath, "/", file.name)}`);
+    }
+  });
+  return arrayOfFiles;
+};
+
+const specs = getAllFiles("cypress/endToEnd");
+process.stdout.write(`${JSON.stringify(specs)}\n`);
