0dddc7021c41eeacdc7601dddcb0a2cb62644d3d
Ahmad Vegah <37585803+asvegah@users.noreply.github.com>
2025-06-10 14:29:04 +0100

    229 add autoscaling to service api (#871)
    * infra: add autoscaling infra to beacons service
    
    - refactor autoscaling to reusable module
    - adjust metric statistic for Maximum instead of Average for scaling up.
    - adjust metric statistic for Average for scaling down

---
+++ terraform/modules/ecs-autoscaling/variables.tf
@@ -0,0 +1,31 @@
+variable "service_name" {
+  type        = string
+  description = "The name of the ECS service (e.g., 'webapp' or 'service')."
+}
+
+variable "ecs_service_resource_id" {
+  type        = string
+  description = "The full resource ID of the ECS service."
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "The name of the ECS cluster."
+}
+
+variable "min_capacity" {
+  type        = number
+  description = "The minimum number of tasks for the service."
+  default     = 1
+}
+
+variable "max_capacity" {
+  type        = number
+  description = "The maximum number of tasks for the service."
+  default     = 6
+}
+
+variable "workspace" {
+  type        = string
+  description = "The current Terraform workspace."
+}
