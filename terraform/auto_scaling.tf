289a0743a3d82f8ebd37133530388c2d3b46ac61
Ahmad Vegah <ahmadvegah@gmail.com>
2025-06-10 15:54:25 +0100

    tidying up alarm naming for terraform.

--- terraform/auto_scaling.tf
+++ terraform/auto_scaling.tf
@@ -1,6 +1,6 @@
 # Apply Autoscaling to the 'webapp'
 module "webapp_autoscaling" {
-  source    = "./modules/ecs-autoscaling"
+  source = "./modules/ecs-autoscaling"
 
   service_name            = aws_ecs_service.webapp.name
   cluster_name            = aws_ecs_cluster.main.name
@@ -12,7 +12,7 @@ module "webapp_autoscaling" {
 
 # Apply Autoscaling to the 'service'
 module "service_autoscaling" {
-  source    = "./modules/ecs-autoscaling"
+  source = "./modules/ecs-autoscaling"
 
   service_name            = aws_ecs_service.service.name
   cluster_name            = aws_ecs_cluster.main.name
