5e4fc13c459aaea2cf74574ee1ab8cee8684627b
Ahmad Vegah <ahmadvegah@gmail.com>
2025-06-10 11:02:29 +0100

    infra: add autoscaling infra to beacons service
    - refactor autoscaling to reusable module
    - adjust metric statistic for Maximum instead of Average for scaling up.
    - adjust metric statistic for Average for scaling down

--- terraform/auto_scaling.tf
+++ terraform/auto_scaling.tf
@@ -1,10 +1,10 @@
 # Apply Autoscaling to the 'webapp'
 module "webapp_autoscaling" {
-  source     = "./modules/ecs-autoscaling"
-  workspace  = terraform.workspace
+  source    = "./modules/ecs-autoscaling"
+  workspace = terraform.workspace
 
-  service_name = aws_ecs_service.webapp.name
-  cluster_name = aws_ecs_cluster.main.name
+  service_name            = aws_ecs_service.webapp.name
+  cluster_name            = aws_ecs_cluster.main.name
   ecs_service_resource_id = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.webapp.name}"
 
   min_capacity = var.webapp_count # or a hardcoded value like 2
@@ -13,11 +13,11 @@ module "webapp_autoscaling" {
 
 # Apply Autoscaling to the 'service'
 module "service_autoscaling" {
-  source     = "./modules/ecs-autoscaling"
-  workspace  = terraform.workspace
+  source    = "./modules/ecs-autoscaling"
+  workspace = terraform.workspace
 
-  service_name = aws_ecs_service.service.name
-  cluster_name = aws_ecs_cluster.main.name
+  service_name            = aws_ecs_service.service.name
+  cluster_name            = aws_ecs_cluster.main.name
   ecs_service_resource_id = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.service.name}"
 
   min_capacity = 2
