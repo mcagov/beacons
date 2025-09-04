9304080d97311158a8472523005f2aa0b1cd0f56
Will Fort <will.fort@madetech.com>
2025-09-04 04:46:49 +0100

    added case for null when adding ID property

--- webapp/src/useCases/getDraftRegistration.ts
+++ webapp/src/useCases/getDraftRegistration.ts
@@ -5,6 +5,9 @@ export const getDraftRegistration =
   ({ draftRegistrationGateway }: IAppContainer) =>
   async (id: string): Promise<DraftRegistration> => {
     const draftRegistration = await draftRegistrationGateway.read(id);
+    if (!draftRegistration) {
+      return draftRegistration;
+    }
     return {
       ...draftRegistration,
       id: id,
