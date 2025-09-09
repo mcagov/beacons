de0ae689d4dee9fc69a22ecb9646964ddbf6a6b0
Will Fort <will.fort@madetech.com>
2025-09-09 09:07:01 +0100

    Removed changed to getDraftRegistration to aid in fixing check-your-answers

--- webapp/src/useCases/getDraftRegistration.ts
+++ webapp/src/useCases/getDraftRegistration.ts
@@ -3,10 +3,5 @@ import { IAppContainer } from "../lib/IAppContainer";
 
 export const getDraftRegistration =
   ({ draftRegistrationGateway }: IAppContainer) =>
-  async (id: string): Promise<DraftRegistration> => {
-    const draftRegistration = await draftRegistrationGateway.read(id);
-    return {
-      ...draftRegistration,
-      id: id,
-    };
-  };
+  async (id: string): Promise<DraftRegistration> =>
+    await draftRegistrationGateway.read(id);
