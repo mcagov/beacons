1529a1dd1139b02371d1f032bfcc4e357f7bb92d
YacquubAdan <yacquub.adan@madetech.com>
2025-12-02 16:23:09 +0000

    S3 Add Public Access (#1005)
    * add: S3 buckets for each env assets with CORS configuration
    
    * fix: name of env to from stage to staging in s3.tf
    
    * chore: include dry-run step to deploy to development.
    
    * Revert "chore: include dry-run step to deploy to development."
    
    This reverts commit d4a1df79d1184668daf6beac0159927580cfd5f3.
    
    * add: script to deploy assets to s3
    
    * add: deploy assets job
    
    * fix: removed caching element of aws s3 sync
    
    * chore: need to deploy image before infra changes.
    
    * chore: add public-access policy to get objects in s3
    
    ---------
    
    Co-authored-by: Ahmad Vegah <ahmadvegah@gmail.com>
    Co-authored-by: Ahmad Vegah <37585803+asvegah@users.noreply.github.com>

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -15,6 +15,38 @@ resource "aws_s3_bucket" "mca_beacons_b2c_assets" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "mca_beacons_b2c_assets" {
+  for_each = aws_s3_bucket.mca_beacons_b2c_assets
+
+  bucket = each.value.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "mca_beacons_b2c_assets_public_read" {
+  for_each = aws_s3_bucket.mca_beacons_b2c_assets
+
+  bucket = each.value.id
+
+  depends_on = [aws_s3_bucket_public_access_block.mca_beacons_b2c_assets]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${each.value.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket_cors_configuration" "mca_b2c_cors" {
   for_each = aws_s3_bucket.mca_beacons_b2c_assets
 
@@ -33,4 +65,3 @@ resource "aws_s3_bucket_cors_configuration" "mca_b2c_cors" {
     max_age_seconds = 3000
   }
 }
-
